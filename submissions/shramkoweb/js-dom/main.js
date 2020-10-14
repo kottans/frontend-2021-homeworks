4fa19e6c0ceb7a95334c49be58395eed478c93f5
serhii.shramko <shramko.web@yahoo.com>
2020-10-14 17:44:29 +0300

    refactor: remove imageURLs

--- submissions/shramkoweb/js-dom/main.js
+++ submissions/shramkoweb/js-dom/main.js
@@ -2,73 +2,41 @@ const frontendFrameworks = [
     {
         id: 0,
         name: 'React',
-    },
-    {
-        id: 1,
-        name: 'Vue',
-    },
-    {
-        id: 2,
-        name: 'Angular',
-    },
-    {
-        id: 3,
-        name: 'Django',
-
-    },
-    {
-        id: 4,
-        name: 'Ruby',
-
-    },
-    {
-        id: 5,
-        name: 'Flask',
-
-    },
-    {
-        id: 6,
-        name: 'Express',
-
-    },
-    {
-        id: 7,
-        name: 'JQuery',
-
-    },
-];
-
-const imageURLs = [
-    {
-        id: 0,
         url: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
     },
     {
         id: 1,
+        name: 'Vue',
         url: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Vue.js_Logo_2.svg',
     },
     {
         id: 2,
+        name: 'Angular',
         url: 'https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg',
     },
     {
         id: 3,
+        name: 'Django',
         url: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Django_logo.svg',
     },
     {
         id: 4,
+        name: 'Ruby',
         url: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Ruby_logo.png',
     },
     {
         id: 5,
+        name: 'Flask',
         url: 'https://upload.wikimedia.org/wikipedia/commons/3/3c/Flask_logo.svg',
     },
     {
         id: 6,
+        name: 'Express',
         url: 'https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png',
     },
     {
         id: 7,
+        name: 'JQuery',
         url: 'https://upload.wikimedia.org/wikipedia/commons/8/81/JQuery_logo_text.svg',
     },
 ];
@@ -98,7 +66,7 @@ const renderListItems = (items, activeID) => {
 };
 
 const renderContent = (activePageID) => {
-    const { url } = getItemByID(imageURLs, activePageID);
+    const { url } = getItemByID(frontendFrameworks, activePageID);
     const contentElement = document.querySelector('.content');
     const image = document.createElement('img');
     image.src = url;
@@ -134,5 +102,3 @@ const init = () => {
 };
 
 init();
-
-
