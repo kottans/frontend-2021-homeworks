c85527c6b9b1bdbda0221c441f1567c0736ed311
Chris Voitova <voitova@teamharbour.com>
2020-10-19 17:20:55 +0300

    rewrite code to classes, made refactor

--- submissions/chris-voitova/frogger-game/app.js
+++ submissions/chris-voitova/frogger-game/app.js
@@ -1,67 +1,87 @@
-//field
-const fieldWidth = 505;
-const borderCoordX = 400;
-const borderCoordY = 450;
-const waterCoord = 50;
-//enemies
-const enemyWidth = 101;
-const enemyHeight = 61;
-//player
-const playerWidth = 101;
-const playerHeight = 101;
-const playerFieldStep = 50;
+const field = {
+  width: 505,
+  borderX: 400,
+  borderY: 450,
+  waterCoord: 0,
+};
+
+const enemyProps = {
+  width: 98,
+  height: 56,
+};
+
+const playerProps = {
+  width: 70,
+  height: 86,
+  step: 50,
+};
+
+class Character {
+  constructor(positionX, positionY, width, height) {
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.width = width;
+    this.height = height;
+  }
+}
 
 // Enemies our player must avoid
-const Enemy = function (positionX, positionY, speed, width, height) {
+class Enemy extends Character {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
-  this.positionX = positionX;
-  this.positionY = positionY;
-  this.speed = speed;
-  this.width = width;
-  this.height = height;
+  constructor(
+    positionX,
+    positionY,
+    speed,
+    width = enemyProps.width,
+    height = enemyProps.height
+  ) {
+    super(positionX, positionY, width, height);
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = "images/enemy-bug.png";
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function (dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  this.positionX += this.speed * dt;
-  if (this.positionX > fieldWidth) {
-    this.positionX = 0;
+    this.speed = speed;
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = "images/enemy-bug.png";
   }
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
-};
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.positionX += this.speed * dt;
+    if (this.positionX > field.width) {
+      this.positionX = 0;
+    }
+  }
+  // Draw the enemy on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
-const Player = function (positionX, positionY, width, height, stepOnField) {
-  this.sprite = "images/char-boy.png";
-  this.positionX = positionX;
-  this.positionY = positionY;
-  this.width = width;
-  this.height = height;
-  this.initPosition = {
-    positionX: positionX,
-    positionY: positionY,
-  };
-  this.stepOnField = stepOnField;
-};
+class Player extends Character {
+  constructor(
+    positionX,
+    positionY,
+    width = playerProps.width,
+    height = playerProps.height,
+    stepOnField = playerProps.step
+  ) {
+    super(positionX, positionY, width, height);
 
-Player.prototype.checkCollisions = function (enemies) {
-  enemies.forEach((enemy) => {
+    this.sprite = "images/char-boy.png";
+    this.initPosition = {
+      positionX: positionX,
+      positionY: positionY,
+    };
+    this.stepOnField = stepOnField;
+  }
+  checkCollisions(enemy) {
     if (
       enemy.positionX < this.positionX + this.width &&
       enemy.positionX + enemy.width > this.positionX &&
@@ -71,63 +91,65 @@ Player.prototype.checkCollisions = function (enemies) {
       alert("you are lose!");
       this.goToInitPosition();
     }
-  });
-};
-
-Player.prototype.goToInitPosition = function () {
-  this.positionX = this.initPosition.positionX;
-  this.positionY = this.initPosition.positionY;
-};
-
-Player.prototype.update = function () {
-  this.checkCollisions(allEnemies);
-  if (this.positionY <= waterCoord) {
-    alert("you are win!");
-    this.goToInitPosition();
   }
-};
-
-Player.prototype.render = function () {
-  ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
-};
-
-Player.prototype.handleInput = function (key) {
-  switch (key) {
-    case "up":
-      this.positionY -= this.stepOnField;
-      break;
-    case "down":
-      this.positionY += this.stepOnField;
-      if (this.positionY > borderCoordY) {
-        this.positionY = borderCoordY;
-      }
-      break;
-    case "left":
-      this.positionX -= this.stepOnField;
-      if (this.positionX < 0) {
-        this.positionX = 0;
-      }
-      break;
-    case "right":
-      this.positionX += this.stepOnField;
-      if (this.positionX > borderCoordX) {
-        this.positionX = borderCoordX;
-      }
-      break;
+  goToInitPosition() {
+    this.positionX = this.initPosition.positionX;
+    this.positionY = this.initPosition.positionY;
   }
-};
+  update() {
+    if (this.positionY <= field.waterCoord) {
+      alert("you are win!");
+      this.goToInitPosition();
+    }
+  }
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.positionX, this.positionY);
+  }
+  handleInput(key) {
+    switch (key) {
+      case "up":
+        this.positionY -= this.stepOnField;
+        break;
+      case "down":
+        this.positionY += this.stepOnField;
+        if (this.positionY > field.borderY) {
+          this.positionY = field.borderY;
+        }
+        break;
+      case "left":
+        this.positionX -= this.stepOnField;
+        if (this.positionX < this.stepOnField) {
+          this.positionX = this.stepOnField;
+        }
+        break;
+      case "right":
+        this.positionX += this.stepOnField;
+        if (this.positionX > field.borderX) {
+          this.positionX = field.borderX;
+        }
+        break;
+    }
+  }
+}
 
 // Now instantiate your objects.
-
-const enemy1 = new Enemy(0, 280, 60, enemyWidth, enemyHeight);
-const enemy2 = new Enemy(0, 200, 100, enemyWidth, enemyHeight);
-const enemy3 = new Enemy(30, 130, 80, enemyWidth, enemyHeight);
-
 // Place all enemy objects in an array called allEnemies
-const allEnemies = [enemy1, enemy2, enemy3];
+const enemiesValues = [
+  [0, 280, 60],
+  [0, 200, 100],
+  [30, 130, 80],
+];
+const allEnemies = enemiesValues.map((value) => new Enemy(...value));
+
 // Place the player object in a variable called player
 
-const player = new Player(200, 450, playerWidth, playerHeight, playerFieldStep);
+const player = new Player(215, 450);
+
+function checkCollisions() {
+  allEnemies.forEach((enemy) => {
+    player.checkCollisions(enemy);
+  });
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
