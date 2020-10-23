0a3e4b9dde9115dc73577b88a94cc04261aee776
Chris Voitova <voitova@teamharbour.com>
2020-10-23 19:08:44 +0300

    changed formatting for file

--- submissions/chris-voitova/oop/index.js
+++ submissions/chris-voitova/oop/index.js
@@ -8,7 +8,6 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 
-
 // ======== OUTPUT ========
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -28,85 +27,84 @@
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
 
-  class Inhabitant {
-	constructor({ name, greating }) {
-	  this.name = name;
-	  this.greating = greating;
-	}
-	saying() {
-	  return this.greating;
-	}
-	toString() {
-	  let props = Object.entries(this);
-	  props = props.map(([key, value]) => {
-		 return value && key !== "greating" ? `${key}: ${value}` : null;
-	  });
-	  return `${props.join(" ")} ${this.saying()}`;
-	}
- }
- class Animal extends Inhabitant {
-	constructor({ name, gender, legs = 4, greating, friends }) {
-	  super({ name, greating });
-	  this.species = "animal";
-	  this.gender = gender;
-	  this.legs = legs;
-	  this.friends = friends;
-	}
- }
- 
- class Human extends Inhabitant {
-	constructor({ name, gender, legs = 2, hands = 2, greating, friends }) {
-	  super({ name, greating });
-	  this.species = "human";
-	  this.legs = legs;
-	  this.hands = hands;
-	  this.gender = gender;
-	  this.friends = friends;
-	}
- }
- 
- class Cat extends Animal {
-	constructor({ name, gender, greating, friends }) {
-	  super({ name, gender, greating, friends });
-	  this.species = "cat";
-	}
- }
- class Dog extends Animal {
-	constructor({ name, gender, greating, friends }) {
-	  super({ name, gender, greating, friends });
-	  this.species = "dog";
-	}
- }
- class CatWoman extends Cat {
-	constructor({ name, gender, greating, friends }) {
-	  super({ name, gender, greating, friends });
-	  this.species = "human";
-	  this.legs = 2;
-	  this.hands = 2;
-	}
- }
- 
- const cat = new Cat({ name: "Expert", gender: "male", greating: "meow!" }),
-		 dog = new Dog({
-			name: "Joker",
-			gender: "male",
-			greating: "woof-woof!",
-			friends: [cat.name],
-		 }),
-		 man = new Human({ name: "Mark", gender: "male", greating: "Hi!" }),
-		 woman = new Human({
-			name: "Alice",
-			gender: "female",
-			greating: "Hello!",
-			friends: [cat.name, dog.name, man.name],
-		 }),
-		 catWoman = new CatWoman({
-			name: "Wonder Alice",
-			gender: "female",
-			greating: "A am superhero!",
-		 });
- 
- [cat, dog, man, woman, catWoman].forEach((inhabitant) =>
-	print(inhabitant, "div")
- );
- 
+class Inhabitant {
+  constructor({ name, greating }) {
+    this.name = name;
+    this.greating = greating;
+  }
+  saying() {
+    return this.greating;
+  }
+  toString() {
+    let props = Object.entries(this);
+    props = props.map(([key, value]) => {
+      return value && key !== "greating" ? `${key}: ${value}` : null;
+    });
+    return `${props.join(" ")} ${this.saying()}`;
+  }
+}
+class Animal extends Inhabitant {
+  constructor({ name, gender, legs = 4, greating, friends }) {
+    super({ name, greating });
+    this.species = "animal";
+    this.gender = gender;
+    this.legs = legs;
+    this.friends = friends;
+  }
+}
+
+class Human extends Inhabitant {
+  constructor({ name, gender, legs = 2, hands = 2, greating, friends }) {
+    super({ name, greating });
+    this.species = "human";
+    this.legs = legs;
+    this.hands = hands;
+    this.gender = gender;
+    this.friends = friends;
+  }
+}
+
+class Cat extends Animal {
+  constructor({ name, gender, greating, friends }) {
+    super({ name, gender, greating, friends });
+    this.species = "cat";
+  }
+}
+class Dog extends Animal {
+  constructor({ name, gender, greating, friends }) {
+    super({ name, gender, greating, friends });
+    this.species = "dog";
+  }
+}
+class CatWoman extends Cat {
+  constructor({ name, gender, greating, friends }) {
+    super({ name, gender, greating, friends });
+    this.species = "human";
+    this.legs = 2;
+    this.hands = 2;
+  }
+}
+
+const cat = new Cat({ name: "Expert", gender: "male", greating: "meow!" }),
+      dog = new Dog({
+        name: "Joker",
+        gender: "male",
+        greating: "woof-woof!",
+        friends: [cat.name],
+      }),
+      man = new Human({ name: "Mark", gender: "male", greating: "Hi!" }),
+      woman = new Human({
+        name: "Alice",
+        gender: "female",
+        greating: "Hello!",
+        friends: [cat.name, dog.name, man.name],
+      }),
+      catWoman = new CatWoman({
+        name: "Wonder Alice",
+        gender: "female",
+        greating: "A am superhero!",
+      });
+
+[cat, dog, man, woman, catWoman].forEach((inhabitant) =>
+  print(inhabitant, "div")
+);
