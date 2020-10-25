e688ab5612ce356860a0b1834bea84f8015cb883
Viktor Kushnir <mustbefail@gmail.com>
2020-10-25 12:48:22 +0200

    Divided the function into two smaller parts getMoviesByGenre and render Chose the correct selectors for the function handler Replaced all tag selectors with class selectors in css Fixed bugs

--- submissions/mustbefail/dom-api-task/script.js
+++ submissions/mustbefail/dom-api-task/script.js
@@ -1,56 +1,62 @@
-const getMoviesByGenre = async (movieGenre) => {
-  const API_KEY = "6a4ca56d29d657ff19ba2cf05591a088";
+const init = () => {
   const imgBaseUrl = "http://image.tmdb.org/t/p/w300/";
-  const reqUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&with_genres=${movieGenre}`;
-  const responce = await fetch(reqUrl);
-  const data = await responce.json();
+  const API_KEY = "6a4ca56d29d657ff19ba2cf05591a088";
+
   const cardsContainer = document.querySelector(".cards-container");
-  cardsContainer.innerHTML = "";
+  const genreList = document.querySelectorAll("[data-genre]");
+  const menuChevron = document.querySelector(".menu-chevron");
 
-  data.results.forEach((el) => {
-    const movieCard = document.createElement("div");
-    movieCard.classList.add("movie-card");
-    const img = document.createElement("img");
-    const imgUrl = `${imgBaseUrl}${el.poster_path}`;
-    img.setAttribute("src", imgUrl);
-    movieCard.innerHTML = `
-      <img src="${imgUrl}" />
-      <div class="title">
-        <div class="card-header">
-          <h5>${el.title}</h5>
-          <h6>${el.vote_average}</h6>
+  const getMoviesByGenre = async (movieGenre) => {
+    const reqUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&with_genres=${movieGenre}`;
+    const response = await fetch(reqUrl);
+    const data = await response.json();
+    render(data);
+  };
+
+  const render = (data) => {
+    let cards = "";
+    data.results.forEach((el) => {
+      const imgUrl = `${imgBaseUrl}${el.poster_path}`;
+      cards += `<div class="movie-card">
+      <img class="card-img" src="${imgUrl}" />
+        <div class="title">
+          <div class="card-header">
+            <h5 class="movie-title">${el.title}</h5>
+            <h6 class="movie-rating">${el.vote_average}</h6>
+          </div>
+          <p class="card-description">
+          ${el.overview.slice(0, 150)}...
+          </p>
         </div>
-        <p class="card-description">
-        ${el.overview.slice(0, 150)}...
-        </p>
-      </div>
-    `;
-    cardsContainer.append(movieCard);
-  });
-  console.log(data);
+      </div>`;
+      cardsContainer.innerHTML = cards;
+    });
+  };
+
+  const handler = ({ target }) => {
+    const genreId = target.dataset.genre;
+    const currentActive = document.querySelector(".sidebar a.active");
+    const mainHeader = document.querySelector(".main-header");
+    mainHeader.textContent = `Movies in category ${target.textContent}`;
+    currentActive.classList.remove("active");
+    target.classList.add("active");
+    getMoviesByGenre(genreId);
+    toggleSidebar();
+  };
+
+  const toggleSidebar = () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("show");
+    menuChevron.classList.toggle("activated");
+  };
+
+  const addClickHandler = (el) => el.addEventListener("click", handler);
+
+  genreList.forEach(addClickHandler);
+
+  menuChevron.addEventListener("click", toggleSidebar);
+
+  getMoviesByGenre(28); //First initiation with Action genre id
 };
 
-const handler = (e) => {
-  const { target } = e;
-  const genreId = target.dataset.genre;
-  const currentActive = document.querySelector("a.active");
-  const mainHeader = document.querySelector("main h2");
-  mainHeader.textContent = `Movies in category ${target.textContent}`;
-  currentActive.classList.remove("active");
-  target.classList.add("active");
-  getMoviesByGenre(genreId);
-  showToggle();
-};
-
-const showToggle = () => {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.classList.toggle("show");
-};
-
-document
-  .querySelectorAll("[data-genre]")
-  .forEach((el) => el.addEventListener("click", handler));
-
-document.querySelector(".menu-chevron").addEventListener("click", showToggle);
-
-getMoviesByGenre(28);
+init();
