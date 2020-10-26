ea05ae09b48ff32b24057624f8b210c7eb5a3692
Viktor Kushnir <mustbefail@gmail.com>
2020-10-26 10:36:30 +0200

    Added window.onload, try/catch for fetch, code style fixes

--- submissions/mustbefail/dom-api-task/script.js
+++ submissions/mustbefail/dom-api-task/script.js
@@ -1,36 +1,22 @@
 const init = () => {
   const imgBaseUrl = "http://image.tmdb.org/t/p/w300/";
   const API_KEY = "6a4ca56d29d657ff19ba2cf05591a088";
+  const INIT_VAL = 28; //Action genre id
 
   const cardsContainer = document.querySelector(".cards-container");
   const genreList = document.querySelectorAll("[data-genre]");
   const menuChevron = document.querySelector(".menu-chevron");
-
+  
   const getMoviesByGenre = async (movieGenre) => {
     const reqUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_year=2020&with_genres=${movieGenre}`;
-    const response = await fetch(reqUrl);
-    const data = await response.json();
-    render(data);
-  };
-
-  const render = (data) => {
-    let cards = "";
-    data.results.forEach((el) => {
-      const imgUrl = `${imgBaseUrl}${el.poster_path}`;
-      cards += `<div class="movie-card">
-      <img class="card-img" src="${imgUrl}" />
-        <div class="title">
-          <div class="card-header">
-            <h5 class="movie-title">${el.title}</h5>
-            <h6 class="movie-rating">${el.vote_average}</h6>
-          </div>
-          <p class="card-description">
-          ${el.overview.slice(0, 150)}...
-          </p>
-        </div>
-      </div>`;
-      cardsContainer.innerHTML = cards;
-    });
+    try {
+      const response = await fetch(reqUrl);
+      const data = await response.json();
+      render(data);
+    }
+    catch(error) {
+      console.log(error);
+    }
   };
 
   const handler = ({ target }) => {
@@ -52,11 +38,31 @@ const init = () => {
 
   const addClickHandler = (el) => el.addEventListener("click", handler);
 
+  const render = (data) => {
+    let cards = "";
+    data.results.forEach((el) => {
+      const imgUrl = `${imgBaseUrl}${el.poster_path}`;
+      cards += `<div class="movie-card">
+      <img class="card-img" src="${imgUrl}" />
+        <div class="title">
+          <div class="card-header">
+            <h5 class="movie-title">${el.title}</h5>
+            <h6 class="movie-rating">${el.vote_average}</h6>
+          </div>
+          <p class="card-description">
+          ${el.overview.slice(0, 150)}...
+          </p>
+        </div>
+      </div>`;
+      cardsContainer.innerHTML = cards;
+    });
+  };
+
   genreList.forEach(addClickHandler);
 
   menuChevron.addEventListener("click", toggleSidebar);
 
-  getMoviesByGenre(28); //First initiation with Action genre id
+  getMoviesByGenre(INIT_VAL); //First initiation with Action genre id
 };
 
-init();
+window.onload = init;
