19db5d7214b844ec2e81c87876773db62b6ab80f
Khasanov <zkhasanovv@gmail.com>
2020-11-01 21:29:42 +0200

    Building a Tiny JS World (pre-OOP) and Object-Oriented JavaScript - done

---
+++ submissions/evgenii-del/tiny-js-world/index.js
@@ -0,0 +1,66 @@
+/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+   Complete the below for code reviewers' convenience:
+
+   Code repository: https://github.com/evgenii-del/a-tiny-JS-world
+   Web app: http://crooked-discovery.surge.sh/
+   */
+
+// ======== OBJECTS DEFINITIONS ========
+// Define your objects here
+
+const man = {
+    species: 'man',
+    name: 'John',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'I want to learn JS!'
+};
+
+const woman = {
+    species: 'woman',
+    name: 'Emma',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'I want to learn JS!'
+};
+
+const cat = {
+    species: 'cat',
+    name: 'Sam',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'meow-meow!'
+};
+
+const dog = {
+    species: 'dog',
+    name: 'Bob',
+    gender: 'male',
+    legs: 4,
+    hands: 0,
+    saying: 'woof-woof!'
+};
+
+[man, woman, cat, dog].forEach(item => print([item.species, item.name, item.gender, item.legs, item.hands, item.saying].join(';')));
+
+// ======== OUTPUT ========
+/* Use print(message) for output.
+   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+
+   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
+   However, please, REFRAIN from improving visuals at least until your code is reviewed
+   so code reviewers might focus on a single file that is index.js.
+   */
+
+/* Print examples:
+   print('ABC');
+   print('<strong>ABC</strong>');
+   print('<strong>ABC</strong>', 'div');
+
+   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
+   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
+   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
+   */
