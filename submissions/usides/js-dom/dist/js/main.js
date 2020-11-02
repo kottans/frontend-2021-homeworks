80ea5770253c5bf741dbfdde552f4c298b633bc2
usides <usides@gmail.com>
2020-11-02 09:57:47 +0400

    js updade

--- submissions/usides/js-dom/dist/js/main.js
+++ submissions/usides/js-dom/dist/js/main.js
@@ -13,8 +13,8 @@ const suspects = [
       '- Language: meowing <br>- Good at hiding <br>- Professional driver that very dangerous on the road - Pursue with extreme caution <br>- Strong sour cream addiction',
     charges:
       '- Suspected in multiple thefts of luxury vehicles around the world <br>- He spends all proceeds on sour cream and can kill for sour cream',
-    portret: 'crim_01',
-    files: ['crim_01_e01', 'crim_01_e02', 'crim_01_e03'],
+    portret: 'crim_01.jpg',
+    files: ['crim_01_e01.jpg', 'crim_01_e02.jpg', 'crim_01_e03.jpg'],
   },
   {
     number: 412,
@@ -30,8 +30,8 @@ const suspects = [
       '- Language: meowing <br>- strong metabolism - can eat all evidence without a form change <br> - master of disguise',
     charges:
       "- Suspected in mass burglaries and shopliftings <br>- Pretends to be a homeless cat and gets old ladies' trust <br>- Uses the stock in the fridge as the priority target for stealing",
-    portret: 'crim_02',
-    files: ['crim_02_e01', 'crim_02_e02'],
+    portret: 'crim_02.jpg',
+    files: ['crim_02_e01.jpg', 'crim_02_e02.jpg'],
   },
   {
     number: 404,
@@ -47,8 +47,8 @@ const suspects = [
       '- Language: meowing, bip-bip-bup-bup <br>- Professional hacker and techno maniac<br>- NFC and WiFi module inside',
     charges:
       '- Suspected in malware production<br>- Debit and Credit Card Frauds<br>- Identity Thefts<br>- Phishing scams<br>- Hacking into bank accounts<br>- Vacuum cleaner robots hacking',
-    portret: 'crim_03',
-    files: ['crim_03_e01', 'crim_03_e02', 'crim_03_e03'],
+    portret: 'crim_03.jpg',
+    files: ['crim_03_e01.jpg', 'crim_03_e02.jpg', 'crim_03_e03.jpg'],
   },
   {
     number: 123,
@@ -64,8 +64,8 @@ const suspects = [
       '- Language: meowing <br>- relaxed all the time <br>- not good at hiding',
     charges:
       '- Suspected in tax frauds <br>- Owner of Illegal flip flops market',
-    portret: 'crim_04',
-    files: ['crim_04_e01', 'crim_04_e02'],
+    portret: 'crim_04.jpg',
+    files: ['crim_04_e01.jpg', 'crim_04_e02.jpg'],
   },
   {
     number: 666,
@@ -81,8 +81,8 @@ const suspects = [
       '- Language: meowing, barking <br>- flip flops collector <br>- Charge with extreme caution',
     charges:
       '- Suspected in an assault with fangs and claws <br>- Terrorizes all dogs around ',
-    portret: 'crim_05',
-    files: ['crim_05_e01', 'crim_05_e02', 'crim_05_e03'],
+    portret: 'crim_05.jpg',
+    files: ['crim_05_e01.jpg', 'crim_05_e02.jpg', 'crim_05_e03.jpg'],
   },
   {
     number: 334,
@@ -98,8 +98,8 @@ const suspects = [
       '- Language: meowing <br>- calls himself as "sharp eye" but for some reason, other calls him as dog breed "shar-pei" <br>- master of disguise as turkey',
     charges:
       '- Suspected in mouses assassination with a rifle <br>- mouse killer for money - hitcat',
-    portret: 'crim_06',
-    files: ['crim_06_e01', 'crim_06_e02'],
+    portret: 'crim_06.jpg',
+    files: ['crim_06_e01.jpg', 'crim_06_e02.jpg'],
   },
 ];
 
@@ -107,11 +107,11 @@ const sideNavMenu = document.getElementById('side-nav-menu');
 const sideMenu = document.getElementById('side-nav');
 const menuBtn = document.getElementById('menu-btn');
 const nextBtn = document.getElementById('next-btn');
-let current;
+let current = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
   loadMenu();
-  printData(0);
+  printData(current);
 });
 
 menuBtn.addEventListener('click', function (e) {
@@ -167,7 +167,7 @@ function printData(perp) {
   }
   document
     .getElementById('crim-portret')
-    .setAttribute('src', `./img/${suspects[perp].portret}.jpg`);
+    .setAttribute('src', `./img/${suspects[perp].portret}`);
   document.getElementById('crim-label').textContent = labelCreate.call(
     suspects[perp],
   );
@@ -177,7 +177,7 @@ function printData(perp) {
     const newElem = document.createElement('div');
     newElem.innerHTML = `<img src="./img/${
       suspects[perp].files[i]
-    }.jpg" alt="Case photo"><p>File ${i + 1}</p>`;
+    }" alt="Case photo"><p>File ${i + 1}</p>`;
     newElem.classList.add('evidence-item');
     fragment.appendChild(newElem);
   }
@@ -189,7 +189,7 @@ function labelCreate() {
 }
 
 function makeActive(menuItem) {
-  let items = document.querySelectorAll('.menu-link');
+  const items = document.querySelectorAll('.menu-link');
   for (let i = 0; i < items.length; i++) {
     items[i].classList.remove('active');
     items[menuItem].classList.add('active');
