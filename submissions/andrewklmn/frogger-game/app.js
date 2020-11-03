88a4e8e9bf72a0045b9166336f579749a37b6d94
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-11-03 15:39:13 +0200

    Add: Player class

--- submissions/andrewklmn/frogger-game/app.js
+++ submissions/andrewklmn/frogger-game/app.js
@@ -24,12 +24,30 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function () {
+this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function() {
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const player = new Player();
+
 
 // This listens for key presses and sends the keys to your
+const allEnemies = [];
 
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
