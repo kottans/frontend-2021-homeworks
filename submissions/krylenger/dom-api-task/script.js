83ad8acad0c2027a02ef76672dea9a48c2522669
krylenger <krylenger@gmail.com>
2020-11-04 13:07:20 +0800

    changing label:checked toggle to button

--- submissions/krylenger/dom-api-task/script.js
+++ submissions/krylenger/dom-api-task/script.js
@@ -5,7 +5,7 @@ const database = [
     description: [
       "Usually designed as a jack of all trades, Adventure bikes tend to feature long travel suspension, a decent sized screen, high seat height, extensive luggage options, optional wire-spoked wheels and decent drop protection.",
       "They aim for maximum capability, range and the odd bit of very light off-road/dirt track adventuring, although taking a large heavy bike properly off-road will be very hard work. Some older definitions also referred to them as dual purpose, dual sport and on / off-road bikes.",
-      "The bikes tend to be so good at commuting, touring, sports and more recently looking good, that they are generally taking over from the other types of bike, particularly sports bikes with their aggressive and uncomfortable geometry."
+      "The bikes tend to be so good at commuting, touring, sports and more recently looking good, that they are generally taking over from the other types of bike, particularly sports bikes with their aggressive and uncomfortable geometry.",
     ],
     models: ["BMW R1250GS", "Honda Africa Twin", "BMW S1000XR"],
     avgPrice: "Average price: $15.000",
@@ -14,8 +14,8 @@ const database = [
     name: "NAKED",
     img: "./images/naked.png",
     description: [
-      "Roughly half of naked bikes tend to start off life as last season\'s range topping sports bikes, which then have their fairing removed to make them \"naked\". Other tweaks usually involve making the geometry less aggressive, flatter handlebars, no screen, single light cluster and some tidying up of wiring etc to showcase the engine.",
-      "The engine is sometimes from a previous generation sports bike to save cost. They are also referred to as roadsters, especially by BMW, or standard bikes while the highest power models are sometimes called muscle bikes, street fighters or hyper-nakeds."
+      'Roughly half of naked bikes tend to start off life as last season\'s range topping sports bikes, which then have their fairing removed to make them "naked". Other tweaks usually involve making the geometry less aggressive, flatter handlebars, no screen, single light cluster and some tidying up of wiring etc to showcase the engine.',
+      "The engine is sometimes from a previous generation sports bike to save cost. They are also referred to as roadsters, especially by BMW, or standard bikes while the highest power models are sometimes called muscle bikes, street fighters or hyper-nakeds.",
     ],
     models: [
       "Kawasaki Z H2",
@@ -30,7 +30,7 @@ const database = [
     description: [
       "Cruisers tend to have a very relaxed feet forward riding position with the body upright or leaning slightly back, a very low seat, large rear tyre, raked out fork and an engine focused on low down torque rather than top end power. They also tend to be really heavy and the power figure will often look at odds with the size of the engine, for instance only getting 165bhp out of a 2.5 litre engine in the Triumph Rocket 3 R.",
       "They are more about looking and sounding good at low speeds than getting from A to B as fast as possible and getting your knee down in the corners although they will also munch motorway miles with ease. The plentiful low down torque also allows a more relaxed approached to gear changes.",
-      "Ducati like to be a bit different and have worked out how to make a bike with cruiser styling perform like an outright sports bike with the XDiavel. In the USA cruisers are the most popular style of motorbike by some margin with Harley Davidson being a main player."
+      "Ducati like to be a bit different and have worked out how to make a bike with cruiser styling perform like an outright sports bike with the XDiavel. In the USA cruisers are the most popular style of motorbike by some margin with Harley Davidson being a main player.",
     ],
     models: [
       "Kawasaki Vulcan S",
@@ -44,8 +44,8 @@ const database = [
     img: "./images/touring.png",
     description: [
       "You can tour on almost any motorbike or scooter if you put your mind to it, however some are more suited to touring than others. Touring bikes are all about luggage capacity, range, comfort for two, all the toys you could want and sometimes even camping gear to boot.",
-      "They aren\'t about light weight performance or, like sports tourers, a compromise between sportiness and outright touring capability. Unfortunately there is a down side to all that capability; they tend to be some of the heaviest bikes on the road. Some are built specifically for the job, like the Honda Goldwing, while others are based around sports tourers or adventure bikes like the Yamaha Tracer 900 GT.",
-      "They often feature fixed panniers or a fancy system to remove the luggage quickly without ruining the looks of the bike. Cruiser derived tourers are sometimes referred to as \"baggers\" or \"fully dressed\"."
+      "They aren't about light weight performance or, like sports tourers, a compromise between sportiness and outright touring capability. Unfortunately there is a down side to all that capability; they tend to be some of the heaviest bikes on the road. Some are built specifically for the job, like the Honda Goldwing, while others are based around sports tourers or adventure bikes like the Yamaha Tracer 900 GT.",
+      'They often feature fixed panniers or a fancy system to remove the luggage quickly without ruining the looks of the bike. Cruiser derived tourers are sometimes referred to as "baggers" or "fully dressed".',
     ],
     models: ["Honda Goldwing", "BMW K1600GT", "Kawasaki Ninja 1000SX"],
     avgPrice: "Average price: $25.000",
@@ -56,7 +56,7 @@ const database = [
     description: [
       "Sports bikes have one goal in mind, which is going as fast as possible from A to B. They achieve this by minimising weight while at the same time maximising power, ideally around 1bhp per 1kg of weight. They also tend to have a low aggressive riding position, full aerodynamic fairing, state of the art technology and the best lean angle possible for cornering as low as possible.",
       "The most expensive bikes are usually closely related to the manufacturer's MotoGP race bike of the time.Sales have been suffering recently though because they aren't that comfortable, pillion friendly, economical or really that practical for everyday riding and if you can only have one bike an Adventure or Sport Touring model probably makes more sense, particularly if you won't be taking it anywhere near a track.",
-      "The middle capacities, around 600cc, have suffered the most though with the only real option still on sale being the gorgeous Yamaha R6."
+      "The middle capacities, around 600cc, have suffered the most though with the only real option still on sale being the gorgeous Yamaha R6.",
     ],
     models: ["BMW S1000RR", "Yamaha YZF-R1", "Kawasaki Ninja H2"],
     avgPrice: "Average price: $13.000",
@@ -66,8 +66,8 @@ const database = [
     img: "./images/supermoto.png",
     description: [
       "Supermoto bikes, sometimes called Supermotard, are designed for racing on tracks made up of tarmac, dirt and jump sections. ",
-      "To optimise the bikes for all three they tend to feature wire-spoke wheels, very long travel suspension, great ground clearance, very basic equipment and a noticeable high \"beak\" as the front fender.",
-      "They also tend to be simple single cylinder engines with large wheels and chunky tyres."
+      'To optimise the bikes for all three they tend to feature wire-spoke wheels, very long travel suspension, great ground clearance, very basic equipment and a noticeable high "beak" as the front fender.',
+      "They also tend to be simple single cylinder engines with large wheels and chunky tyres.",
     ],
     models: [
       "Husqvarna 701 Supermoto",
@@ -80,7 +80,7 @@ const database = [
     name: "OFFROAD",
     img: "./images/offroad.png",
     description: [
-      "The hallmarks of bikes specifically designed for off-road work tend to be very light weight, wire-spoked wheels, some form of protection under the engine in case of grounding, engine bars in case it drops, long travel suspension, hardly any screen in case you go over the top and usually \"knobbly\" tyres for improved grip.",
+      'The hallmarks of bikes specifically designed for off-road work tend to be very light weight, wire-spoked wheels, some form of protection under the engine in case of grounding, engine bars in case it drops, long travel suspension, hardly any screen in case you go over the top and usually "knobbly" tyres for improved grip.',
       "They are popular for green laning on public byways / un-tarmacked roads.",
     ],
     models: ["HONDA CRF450RX", "HUSQVARNA FX450", "KTM 450XC-F"],
@@ -92,54 +92,80 @@ const database = [
     description: [
       "Most of the retro models you can buy today started life as a sporty naked bike which has been modified to look more traditional, but without sacrificing all the modern tech and riding experience you would expect from a new bike.",
       "They usually feature a single round headlight, simpler rounded clocks, stitched seats, more subtle colour schemes, rounded exhausts, loads of optional shiny bits and often wire spoked wheels or cast wheels designed to look a little bit like spokes from a distance.",
-      "Closely related are modern cafe racers which tend to start life as retro naked bikes but with the addition of low bars, a headlight fairing and pillion seat hump."
+      "Closely related are modern cafe racers which tend to start life as retro naked bikes but with the addition of low bars, a headlight fairing and pillion seat hump.",
     ],
     models: ["Kawasaki Z900RS", "Triumph Speed Twin", "Moto Guzzi V7 III"],
     avgPrice: "Average price: $10.000",
   },
 ];
 
-const navUl = document.querySelector('.nav__ul');
-const navLink = document.querySelectorAll('.nav__link');
-const mainHeader = document.querySelector('.main__header');
-const mainImage = document.querySelector('.main__img');
-const mainDescription = document.querySelector('.main__description');
-const mainDescriptionText = document.querySelector('.main__description-text');
-const mainUlBikes = document.querySelector('.main__ul-bikes');
-const mainAvgPrice = document.querySelector('.main__avg-price');
+const navBtnToggle = document.querySelector(".nav-btn-toggle");
+const nav = document.querySelector(".nav");
+const navUl = document.querySelector(".nav__ul");
+const navLink = document.querySelectorAll(".nav__link");
+const main = document.querySelector(".main");
+const mainHeader = document.querySelector(".main__header");
+const mainImage = document.querySelector(".main__img");
+const mainDescription = document.querySelector(".main__description");
+const mainDescriptionText = document.querySelector(".main__description-text");
+const mainUlBikes = document.querySelector(".main__ul-bikes");
+const mainAvgPrice = document.querySelector(".main__avg-price");
+
+const handleNavBtnToggle = ({ target }) => {
+  if (navBtnToggle.classList.contains("nav-btn-toggle--nav-closed")) {
+    navBtnToggle.classList.remove("nav-btn-toggle--nav-closed");
+    navBtnToggle.classList.add("nav-btn-toggle--nav-opened");
+    nav.classList.add("nav--opened");
+    nav.classList.remove("nav--closed");
+    main.classList.remove("main--wide");
+    main.classList.add("main--short");
+    navUl.classList.remove("nav__ul--display-none");
+    navUl.classList.add("nav__ul--display-yes");
+  } else {
+    navBtnToggle.classList.remove("nav-btn-toggle--nav-opened");
+    navBtnToggle.classList.add("nav-btn-toggle--nav-closed");
+    nav.classList.remove("nav--opened");
+    nav.classList.add("nav--closed");
+    main.classList.remove("main--short");
+    main.classList.add("main--wide");
+    navUl.classList.remove("nav__ul--display-yes");
+    navUl.classList.add("nav__ul--display-none");
+  }
+};
 
 const handleNavigationClick = ({ target }) => {
-  console.log(target)
+  console.log(target);
   const sectionName = target.textContent;
-  const section = database.find(item => item.name === sectionName);
+  const section = database.find((item) => item.name === sectionName);
   mainHeader.textContent = section.name;
-  mainImage.setAttribute('src', section.img);
+  mainImage.setAttribute("src", section.img);
   mainAvgPrice.textContent = section.avgPrice;
 
   const fragmentBikes = document.createDocumentFragment();
   const fragmentDescription = document.createDocumentFragment();
-  
-  section.models.forEach(model => {
-    let bike = document.createElement('li');
+
+  section.models.forEach((model) => {
+    let bike = document.createElement("li");
     bike.textContent = model;
     fragmentBikes.appendChild(bike);
-  })
-  section.description.forEach(description => {
-    let desc = document.createElement('p');
+  });
+  section.description.forEach((description) => {
+    let desc = document.createElement("p");
     desc.textContent = description;
-    desc.setAttribute('class', 'main__description-p')
+    desc.setAttribute("class", "main__description-p");
     fragmentDescription.appendChild(desc);
-  })
-  navLink.forEach(link => {
+  });
+  navLink.forEach((link) => {
     link.classList.add("nav__link--lightgrey");
-  })
-  if (target.className === 'nav__link') {
+  });
+  if (target.className === "nav__link") {
     target.classList.add("nav__link--orange");
   }
-  mainUlBikes.innerHTML = '';
-  mainDescription.innerHTML = '';
+  mainUlBikes.innerHTML = "";
+  mainDescription.innerHTML = "";
   mainUlBikes.appendChild(fragmentBikes);
   mainDescription.appendChild(fragmentDescription);
-}
+};
 
-navUl.addEventListener('click', handleNavigationClick);
+navBtnToggle.addEventListener("click", handleNavBtnToggle);
+navUl.addEventListener("click", handleNavigationClick);
