64d383c95ce9702099af893bf616213c8f145dc2
Khasanov <zkhasanovv@gmail.com>
2020-11-04 17:19:19 +0200

    Refactoring

--- submissions/evgenii-del/dom/js/script.js
+++ submissions/evgenii-del/dom/js/script.js
@@ -1,27 +1,27 @@
 const cats = [
     {
         name: 'Пуфик',
-        icon: 'fas fa-cat',
+        icon: 'img/cat-solid.svg',
         url: 'https://images.pexels.com/photos/5200916/pexels-photo-5200916.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     },
     {
         name: 'Люся',
-        icon: 'fas fa-paw',
+        icon: 'img/fish-solid.svg',
         url: 'https://images.pexels.com/photos/1366996/pexels-photo-1366996.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     },
     {
         name: 'Трейси',
-        icon: 'fas fa-fish',
+        icon: 'img/heart-solid.svg',
         url: 'https://images.pexels.com/photos/1457595/pexels-photo-1457595.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     },
     {
         name: 'Люк',
-        icon: 'fas fa-heart',
+        icon: 'img/paw-solid.svg',
         url: 'https://images.pexels.com/photos/2870353/pexels-photo-2870353.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     },
     {
         name: 'Спарк',
-        icon: 'fas fa-sun',
+        icon: 'img/sun-solid.svg',
         url: 'https://images.pexels.com/photos/979247/pexels-photo-979247.jpeg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
     }
 ]
@@ -30,43 +30,47 @@ const main = document.querySelector('.js-main');
 const menu = document.querySelector('.js-menu');
 
 const renderCats = (position) => {
-    let cat = cats[position];
-    main.innerHTML = `
-        <img id="img" src=${cat.url} alt=${cat.name}>`;
+    const cat = cats[position];
+    main.innerHTML = `<img id="img" src=${cat.url} alt=${cat.name}>`;
 }
 
-const renderItem = (cat, index) => {
-    let block = document.createElement('li');
-    block.innerHTML = `
-        <a href="#" data-id="${index}">
+const renderItem = () => {
+    let fragment = document.createDocumentFragment();
+
+    cats.forEach((cat, index) => {
+        const block = document.createElement('li');
+        block.innerHTML = `
+        <a href="#" class="navbar__item" data-id="${index}">
             <span class="icon">
-                <i class="${cat.icon}"></i>
+            <img src="${cat.icon}" alt="${cat.name}">
             </span>
             <p class="text">${cat.name}</p>
         </a>
-    `;
-    menu.appendChild(block);
+        `;
+        fragment.appendChild(block);
+    })
+
+    menu.appendChild(fragment);
 }
 
 const removeActive = () => {
-    document.querySelectorAll('li').forEach((li) => {
-        li.querySelector('a').className = "";
-    })
+    const activeElement = document.querySelector('a.active');
+    activeElement.classList.remove('active');
 }
 
 window.onload = () => {
-    cats.forEach((item, index) => {
-        renderItem(item, index);
-    })
+    renderItem();
 
-    document.querySelectorAll('li').forEach((li) => {
-        li.addEventListener('click', ({target}) => {
+    document.querySelector('.navbar__items').addEventListener('click', (event) => {
+        const target = event.target;
+        if (event.target.tagName === 'A') {
             removeActive();
-            target.className = 'active';
-            renderCats(target.getAttribute("data-id"));
-        });
+            const data_id = target.getAttribute("data-id");
+            target.classList.add('active');
+            renderCats(data_id);
+        }
     })
 
     renderCats(0);
-    document.querySelector('a').className = 'active';
+    document.querySelector('.navbar__item').className = 'navbar__item active';
 };
