8f518da33814288657578a72597e1c00db4ad189
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-11-06 00:49:41 +0200

    Add: grid for cards

--- submissions/andrewklmn/memory-pair-game/app.js
+++ submissions/andrewklmn/memory-pair-game/app.js
@@ -1,9 +1,36 @@
-const container = document.querySelector(".container");
-
-const cards = [
-  
-];
-
 document.addEventListener('DOMContentLoaded', (event)=>{
-  container.innerHTML = "Hello world!"
+  
+  const container = document.querySelector(".container");
+  
+  const images = [
+    'image1.webp',
+    'image2.webp',
+    'image3.webp',
+    'image4.webp',
+    'image5.webp',
+    'image6.webp',
+    'image7.webp',
+    'image8.webp',
+    'image1.webp',
+    'image2.webp',
+    'image3.webp',
+    'image4.webp',
+    'image5.webp',
+    'image6.webp',
+    'image7.webp',
+    'image8.webp',
+  ];
+
+  images.sort(function() { return 0.5 - Math.random() });
+
+  images.forEach((name)=>{
+    const img = document.createElement("img");
+    img.src = `img/${name}`;
+    img.classList.add('card');
+    container.appendChild(img);
+  });
+
+  container.addEventListener('click',(e)=>{ 
+    e.target.classList.toggle('hide');
+  });
 });
