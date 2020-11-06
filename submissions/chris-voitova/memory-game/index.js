0da92eda6e3287d742677ab971598fea064305a1
Chris Voitova <voitova@teamharbour.com>
2020-11-06 15:28:25 +0200

    fixed field blocking

--- submissions/chris-voitova/memory-game/index.js
+++ submissions/chris-voitova/memory-game/index.js
@@ -11,6 +11,7 @@ const images = [
 const cards = [...images, ...images];
 const field = document.querySelector(".field");
 let cardsAmount = cards.length;
+let isBlockedField = false;
 
 const htmlTemplate = (img) => `
 <div class="card" data-name="${img}">
@@ -52,6 +53,7 @@ function checkTwoCards(field) {
     item.classList.contains("active") ? checkedCards.push(item) : null;
   });
   if (checkedCards.length === 2) {
+    isBlockedField = true;
     checkedCards.reduce((first, curr, index, arr) => {
       if (first.dataset.name === curr.dataset.name) {
         cardsAmount -= 2;
@@ -60,14 +62,18 @@ function checkTwoCards(field) {
             arr.forEach((item) => {
               item.classList.remove("active");
               item.classList.add("hidden");
-            }),
+            }, (isBlockedField = false)),
           1000
         );
         checkedCards = [];
       } else {
         arr.forEach((item) => item.classList.remove("active")),
           setTimeout(
-            () => arr.forEach((item) => item.classList.remove("flipped")),
+            () =>
+              arr.forEach(
+                (item) => item.classList.remove("flipped"),
+                (isBlockedField = false)
+              ),
             1000
           );
         checkedCards = [];
@@ -86,7 +92,7 @@ createCardsFromArr(cards);
 
 field.addEventListener("click", function (e) {
   const target = e.target;
-  if (target.classList.contains("card")) {
+  if (target.classList.contains("card") && !isBlockedField) {
     toggleClassName(target, ["active", "flipped"]);
     checkTwoCards(field);
   }
