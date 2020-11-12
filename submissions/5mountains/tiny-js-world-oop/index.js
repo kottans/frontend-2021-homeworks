f8a98b797812d747608b901e9915560e3ab6c9cd
5Mountains <V.Piatyhor@gmail.com>
2020-11-12 20:17:54 +0200

    JS OOP approach improvements

--- submissions/5mountains/tiny-js-world-oop/index.js
+++ submissions/5mountains/tiny-js-world-oop/index.js
@@ -8,67 +8,82 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 class Resident {
-  constructor(species, name, gender, saying, friends) {
+  constructor(species, name, gender) {
     this.species = species;
     this.name = name;
     this.gender = gender;
-    this.saying = saying;
-    this.friends = friends;
   }
-  introducing() {
+  toString() {
     return `Wonderful creature - <b>${
       this.species
     }</b>, whose name is <b>${
       this.name
-    }</b>! Usually the greeting is <b>${
-      this.saying
-    }</b></i> and friends of this inhabitant are <b>${
-      this.friends.join(', ')
-    }</b>. By the wat, this member is owner of`;
+    }</b> gender is <b>${
+      this.gender
+    }</b>!`;
   }
 }
 
 class Human extends Resident {
-  constructor(name, gender, saying, friends) {
-    super('human', name, gender, saying, friends);
+  constructor(name, gender, saying, friends = 'no one') {
+    super('human', name, gender);
     this.legs = 2;
     this.hands = 2;
+    this.saying = saying;
+    this.friends = friends;
   }
-  introducing() {
+  toString() {
     return (
-      super.introducing() +
-      ` <b>${this.legs} legs</b> and <b>${this.hands} hands</b>!`
+      super.toString() + `</b> Usually the greeting is <b>${
+        this.saying
+      }</b> The friends of this inhabitant are <b>${
+        this.friends.join(', ')
+      }</b>. By the wat, this member is owner of <b>${
+        this.legs
+      } legs</b> and <b>${
+        this.hands
+      } hands</b>!`
     );
   }
 }
 
 class Animal extends Resident {
-  constructor(species, name, gender, saying, friends) {
-    super(species, name, gender, saying, friends);
+  constructor(species, name, gender, saying, friends = 'no one') {
+    super(species, name, gender);
     this.paws = 4;
+    this.saying = saying;
+    this.friends = friends;
   }
-  introducing() {
-    return super.introducing() + ` <b>${this.paws} paws</b>!`;
+  toString() {
+    return super.toString() + `</b> Usually the greeting is <b>${
+      this.saying
+    }</b> The friends of this inhabitant are <b>${
+      this.friends.join(', ')
+    }</b>. By the wat, this member is owner of <b>${
+     this.paws
+    } paws</b>!`;
   }
 }
 
 class Dog extends Animal {
-  constructor(name, gender) {
-    super('dog', name, gender, 'woof-woof!');
+  constructor(name, gender, saying, friends) {
+    super('dog', name, gender, saying, friends);
+    this.saying =  'woof-woof!';
     this.friends = ['Tom', 'Jenny'];
   }
 }
 
 class Cat extends Animal {
-  constructor(name, gender) {
-    super('cat', name, gender, 'meow!');
-    this.friends = ['Tom', 'Jenny', 'Selina'];
+  constructor(name, gender, saying, friends) {
+    super('cat', name, gender, saying, friends);
+    this.saying = 'meow!';
+    this.friends =  ['Tom', 'Jenny', 'Selina'];
   }
 }
 
 class CatWoman extends Human {
-  constructor(name, gender) {
-    super(name, gender);
+  constructor(name, gender, saying) {
+    super(name, gender, saying);
     this.friends = ['Tom', 'Felix'];
     this.saying = new Cat().saying;
   }
@@ -81,19 +96,14 @@ Message can contain HTML markup. You may also tweak index.html and/or styles.css
 However, please, REFRAIN from improving visuals at least until your code is reviewed
 so code reviewers might focus on a single file that is index.js.
 */
-const man = new Human('Tom', 'male', 'Hello Jenny!', [
-    'Tom',
-    'Jenny',
-    'Rex',
-    'Selina',
-  ]),
+const man = new Human('Tom', 'male', 'Hello Jenny!', ['Tom','Jenny','Rex', 'Selina']),
   women = new Human('Jenny', 'female', 'Hello Tom!', ['Tom', 'Rex', 'Felix']),
   dog = new Dog('Rex', 'male'),
   cat = new Cat('Felix', 'male'),
   catWoman = new CatWoman('Selina', 'female'),
   residents = [man, women, cat, dog, catWoman];
 
-residents.map((resident) => print(resident.introducing(), 'p'));
+residents.forEach((resident) => print((resident), 'p'));
 /* Print examples:
 print('ABC');
 print('<strong>ABC</strong>');
