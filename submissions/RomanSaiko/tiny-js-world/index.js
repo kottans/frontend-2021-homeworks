80719fea89c75091bbd6c035149a5da963823207
RomanSaiko <rsaiko1995@gmail.com>
2020-11-16 21:16:03 +0200

    + fixes according to review comments;

--- submissions/RomanSaiko/tiny-js-world/index.js
+++ submissions/RomanSaiko/tiny-js-world/index.js
@@ -50,8 +50,10 @@ const catWoman = {
 
 const inhabitants = [dog, cat, man, woman, catWoman]
 
-console.log(inhabitants);
-
-for(let i = 0; i < inhabitants.length; i++) {
-    print(`${inhabitants[i].species}; ${inhabitants[i].name}; ${inhabitants[i].gender}; ${inhabitants[i].legs}; ${inhabitants[i].hands}; ${inhabitants[i].saying}; ${inhabitants[i].friends.map(item => item)}.`);
+const introduceInhabitants = ({species, name, gender, legs, hands, saying, friends}) => {
+    return `${species}; ${name}; ${gender}; ${legs}; ${hands}; ${saying}; ${friends.join(', ')}.`
 }
+
+inhabitants.forEach((item) => {
+    print(introduceInhabitants(item))
+})
