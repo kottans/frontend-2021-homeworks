313fc301832472143be33de80577398139018527
Yevhen Tkachenko <joy_123@ukr.net>
2020-11-17 19:27:43 +0100

    Create a list ob my burger-menu' content. It is an array of objects

---
+++ submissions/t-jay-777/dom-api-task/js/script.js
@@ -0,0 +1,68 @@
+//create an array of objects of my dom menu content
+const contentList = [
+    {
+        id: 000,
+        name: 'Big Mac @ McDonald’s',
+        url: 'img/000-Big-Mac.jpg',
+        text: 'Call us crazy, but how can we possibly leave out a burger that sells 550 million times each year in America alone?\
+        That means that every second, there are 17 people across the country stuffing their face with the McDonald\’s icon.\
+        It might not be topped with exotic truffles of served alongside a sparkly bottle of rear 1950s Champagne, but it sells.\
+        And with those kinds of astronomical numbers, it must be doing something right.\
+        It’s layers of meat and bread, plus shredded lettuce, pickles and McDonald’s special Big Mac sauce,\
+        all served on a sesame bun - and apparently, the world can’t get enough of it.',
+    },
+    {
+        id: 001,
+        name: 'FleurBurger @ Fleur',
+        url: 'img/001-FleurBurger.jpg',
+        text: '',
+    },
+    {
+        id: 002,
+        name: 'The Butchers Club Burger @ The Butchers Club',
+        url: 'img/002-0AThe-Butchers-Club-Burger.jpg',
+        text: '',
+    },
+    {
+        id: 003,
+        name: 'The Smokey Burger @ Burger Liquor',
+        url: 'img/003-The-Smokey-Burger-at-Burger-Liquor.jpg',
+        text: '',
+    },
+    {
+        id: 004,
+        name: 'The Ozersky Burger',
+        url: 'img/004-The-Ozersky-Burger.jpg',
+        text: '',
+    },
+    {
+        id: 005,
+        name: 'The Burger @ Dirty Bones',
+        url: 'img/005-dirty-bones-burger-london.jpg',
+        text: '',
+    },
+    {
+        id: 006,
+        name: 'American Burger @ Gordon Ramsay Burger',
+        url: 'img/006-gordon-ramsay-american-burger-las-vegas.jpg',
+        text: '',
+    },
+    {
+        id: 007,
+        name: 'The Jackie-O @ Fat Bobs Bar & Grill',
+        url: 'img/007-fat-bobs-burger.jpg',
+        text: '',
+    },
+    {
+        id: 008,
+        name: 'The Burger @ Raoul\'s',
+        url: 'img/008-The-Burger0ARaouls.jpg',
+        text: '',
+    },
+    {
+        id: 009,
+        name: 'Bleecker Black Burger @ Bleecker',
+        url: 'img/009-Bleecker-Black-Burger.jpg',
+        text: '',
+    },
+];
