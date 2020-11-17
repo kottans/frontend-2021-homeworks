39248cb15b5fb4b8307883bba6cc5c5c1fcec288
Dima <t0perxxl@gmail.com>
2020-11-17 13:01:27 +0200

    Fixed my task, added classes

--- submissions/xT0PERx/OOP exercise/index.js
+++ submissions/xT0PERx/OOP exercise/index.js
@@ -1,37 +1,93 @@
 class Mammals {
-  constructor(species, name, gender, legs, hands, saying) {
-    this.species = species;
-    this.name = name;
+  constructor(props) {
+    this.name = props.name;
+    this.saying = props.saying;
+  }
+  say() {
+    return this.saying;
+  }
+  printInfo() {
+    print(
+      [
+        this.species,
+        this.name,
+        this.gender,
+        this.hands,
+        this.legs,
+        this.say(),
+      ].join('; ')
+    );
+  }
+}
+
+class Animal extends Mammals {
+  constructor({ name, gender, legs = 4, saying }) {
+    super({ name, saying });
+    this.species = 'animal';
+    this.gender = gender;
+    this.legs = legs;
+  }
+}
+
+class Human extends Mammals {
+  constructor({ name, gender, legs = 2, hands = 2, saying }) {
+    super({ name, saying });
+    this.species = 'human';
     this.gender = gender;
     this.legs = legs;
     this.hands = hands;
-    this.saying = saying;
-  }
-  printInfo() {
-    print([
-      this.species,
-      this.name,
-      this.gender,
-      this.legs,
-      this.hands,
-      this.saying,
-    ]);
   }
 }
-const man = new Mammals('man', 'Bob', 'male', 2, 2, `Hi-i am a man`);
-const woman = new Mammals('woman', 'Lara', 'female', 2, 2, `Hi-i am a woman`);
-const dog = new Mammals('dog', 'Bobik', 'male', 4, 0, `Woof-woof`);
-const cat = new Mammals('cat', 'Zarina', 'female', 4, 0, `Meow-Meow`);
-const werewolf = new Mammals(
-  'werewolf',
-  'Dorian',
-  'male',
-  2,
-  2,
-  `Lingering howl...`
-);
-const enhabitants = [man, woman, dog, cat, werewolf];
 
-enhabitants.forEach((el) => {
+class Cat extends Animal {
+  constructor({ name, gender, saying }) {
+    super({ name, gender, saying });
+    this.species = 'cat';
+  }
+}
+class Dog extends Animal {
+  constructor({ name, gender, saying }) {
+    super({ name, gender, saying });
+    this.species = 'dog';
+  }
+}
+class Werewolf extends Human {
+  constructor({ name, gender, saying }) {
+    super({ name, gender, saying });
+    this.species = 'werewolf';
+    this.legs = 2;
+    this.hands = 2;
+  }
+}
+
+const man = new Human({
+    name: 'Bob',
+    gender: 'male',
+    saying: 'Hi!',
+  }),
+  woman = new Human({
+    name: 'Lara',
+    gender: 'female',
+    saying: 'Hello!',
+  }),
+  dog = new Dog({
+    name: 'Bobik',
+    gender: 'male',
+    saying: 'Woof-Woof!',
+  }),
+  cat = new Cat({
+    name: 'Zarina',
+    gender: 'female',
+    saying: `Meow-Meow`,
+  }),
+  werewolf = new Werewolf({
+    name: 'Dorian',
+    gender: 'male',
+    saying: `Lingering howl...`,
+  });
+
+const inhabitants = [man, woman, dog, cat, werewolf];
+
+inhabitants.forEach((el) => {
   el.printInfo();
 });
