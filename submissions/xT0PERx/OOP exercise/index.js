60efed5dff85b9f57f9840d994f82b7185d6fe54
Dima <t0perxxl@gmail.com>
2020-11-19 09:48:28 +0200

    Fixed my js code

--- submissions/xT0PERx/OOP exercise/index.js
+++ submissions/xT0PERx/OOP exercise/index.js
@@ -1,93 +1,84 @@
-class Mammals {
-  constructor(props) {
-    this.name = props.name;
-    this.saying = props.saying;
+class Mammal {
+  constructor(name, gender, saying) {
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
   }
-  say() {
-    return this.saying;
-  }
-  printInfo() {
-    print(
-      [
-        this.species,
-        this.name,
-        this.gender,
-        this.hands,
-        this.legs,
-        this.say(),
-      ].join('; ')
-    );
+  toString() {
+    return `${this.name}; ${this.gender}; ${this.saying}`;
   }
 }
 
-class Animal extends Mammals {
-  constructor({ name, gender, legs = 4, saying }) {
-    super({ name, saying });
+class Animal extends Mammal {
+  constructor(name, gender, saying, legs = 4) {
+    super(name, gender, saying);
     this.species = 'animal';
-    this.gender = gender;
     this.legs = legs;
   }
+  toString() {
+    return `${super.toString()}; ${this.species}; ${this.legs}`;
+  }
 }
 
-class Human extends Mammals {
-  constructor({ name, gender, legs = 2, hands = 2, saying }) {
-    super({ name, saying });
+class Human extends Mammal {
+  constructor(name, gender, saying, friend, legs = 2, hands = 2) {
+    super(name, gender, saying);
     this.species = 'human';
-    this.gender = gender;
+    this.friend = friend;
     this.legs = legs;
     this.hands = hands;
+  }
+  toString() {
+    return `${super.toString()}; ${this.species}; ${this.legs}; ${
+      this.hands
+    }; ${this.friend}`;
   }
 }
 
 class Cat extends Animal {
-  constructor({ name, gender, saying }) {
-    super({ name, gender, saying });
+  constructor(name, gender, saying, friend) {
+    super(name, gender, saying);
     this.species = 'cat';
+    this.friend = friend;
+  }
+  toString() {
+    return `${super.toString()}; ${this.friend}`;
   }
 }
 class Dog extends Animal {
-  constructor({ name, gender, saying }) {
-    super({ name, gender, saying });
+  constructor(name, gender, saying, friend) {
+    super(name, gender, saying);
     this.species = 'dog';
+    this.friend = friend;
+  }
+  toString() {
+    return `${super.toString()}; ${this.friend}`;
   }
 }
 class Werewolf extends Human {
-  constructor({ name, gender, saying }) {
-    super({ name, gender, saying });
+  constructor(name, gender, saying, friend, transforms) {
+    super(name, gender, saying);
     this.species = 'werewolf';
-    this.legs = 2;
-    this.hands = 2;
+    this.friend = friend;
+    this.transforms = transforms;
+  }
+  toString() {
+    return `${super.toString()}; ${this.transforms}`;
   }
 }
 
-const man = new Human({
-    name: 'Bob',
-    gender: 'male',
-    saying: 'Hi!',
-  }),
-  woman = new Human({
-    name: 'Lara',
-    gender: 'female',
-    saying: 'Hello!',
-  }),
-  dog = new Dog({
-    name: 'Bobik',
-    gender: 'male',
-    saying: 'Woof-Woof!',
-  }),
-  cat = new Cat({
-    name: 'Zarina',
-    gender: 'female',
-    saying: `Meow-Meow`,
-  }),
-  werewolf = new Werewolf({
-    name: 'Dorian',
-    gender: 'male',
-    saying: `Lingering howl...`,
-  });
+const man = new Human('Bob', 'male', 'Hi!', 'Bobik');
+const woman = new Human('Lara', 'female', 'Hello!', 'Zarina');
+const dog = new Dog('Bobik', 'male', 'Woof-Woof', 'Bob');
+const cat = new Cat('Zarina', 'female', `Meow-Meow`, 'Lara');
+const werewolf = new Werewolf(
+  'Dorian',
+  'male',
+  `Lingering howl!`,
+  'Bobik',
+  'full moon'
+);
 
 const inhabitants = [man, woman, dog, cat, werewolf];
 
-inhabitants.forEach((el) => {
-  el.printInfo();
-});
+inhabitants.forEach((inhabitant) => print(inhabitant));
