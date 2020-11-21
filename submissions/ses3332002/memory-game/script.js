d66c520cc3e48a486483a860dd41f233a2c8c603
Сергей Гаврилов <ses3332002@gmail.com>
2020-11-21 17:59:08 +0200

    requested changes applied

--- submissions/ses3332002/memory-game/script.js
+++ submissions/ses3332002/memory-game/script.js
@@ -7,20 +7,28 @@
   const startButton = document.querySelector(".start_button");
   const gameArea = document.querySelector(".game_area");
   const switchableControls = document.querySelectorAll(".switchable");
+  const popupHailWindow = document.querySelector(".popup_hail");
+  const okButton = document.querySelector(".popup_hail__button");
+  
   let numbersArray;
   let cardThemeSelected;
   let cardSelected;
   let cardsArray;
   let matchedCounter;
-  
+  const gamefieldSize1 = 12;
+  const gamefieldSize2 = 16;
+  const gamefieldSize3 = 20;
+  const animationDelay = 400;
+    
   startButton.addEventListener("click", startGame);
+  okButton.addEventListener("click", resetWindow);
   doc.classList.add("theme1");
   
   themeButtons.addEventListener("click", function({target}) {
     if (target.tagName != "BUTTON") {
       return;
     };
-    doc.classList.remove(doc.classList[0]);
+    doc.className = "";
     doc.classList.add(target.dataset.theme);
   });
   
@@ -38,50 +46,58 @@
     });
   };
   
+  function checkOrientationAndSize() {
+    if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
+      if (sizeSelector.value == gamefieldSize1) {
+        gameArea.classList.add("game_area-3columns");
+      } else {
+        gameArea.classList.add("game_area-4columns");
+      };
+    } else {
+      if (sizeSelector.value <= gamefieldSize2) {
+        gameArea.classList.add("game_area-4columns");
+      } else if (sizeSelector.value == gamefieldSize3){
+        gameArea.classList.add("game_area-5columns");
+      } else {
+        gameArea.classList.add("game_area-6columns");
+      };
+    };
+  };
+  
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    };
+  };
+  
   function prepareGameField() {
     numbersArray = [];
     for (let i=0; i < sizeSelector.value/2; i++) {
       numbersArray[i] = i;
     };
     numbersArray = numbersArray.concat(numbersArray);
-
-    function shuffle(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      };
-    };
     
     shuffle(numbersArray);
+    checkOrientationAndSize();
     
-    if (document.documentElement.clientHeight > document.documentElement.clientWidth) {
-      if (sizeSelector.value == 12) {
-        gameArea.classList.add("game_area-3columns");
-      } else {
-        gameArea.classList.add("game_area-4columns");
-      };
-    } else {
-      if (sizeSelector.value <= 16) {
-        gameArea.classList.add("game_area-4columns");
-      } else if (sizeSelector.value == 20){
-        gameArea.classList.add("game_area-5columns");
-      } else {
-        gameArea.classList.add("game_area-6columns");
-      };
-    };
-    let fragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
     for (let i=0; i < sizeSelector.value; i++) {
       let card = document.createElement("button");
       card.classList.add("card");
       card.innerHTML = `<div class="card__front" style="background-image: url(img/${cardThemeSelected.value}_bg.jpg);"></div><div class="card__back" style="background-image: url(img/${cardThemeSelected.value}_${numbersArray[i] + 1}.jpg);"></div>`;
       fragment.append(card);
-      card.addEventListener("click", checkCard);
     };
     gameArea.append(fragment);
+    gameArea.addEventListener("click", checkCard);
     cardsArray = Array.from(document.querySelectorAll(".card"));
   };
   
   function checkCard({target}) {
+    if (target.className != "card") {
+      return;
+    };
+    
     flipCard.call(target);
     setTimeout(function() {
       if (!cardSelected) {
@@ -101,40 +117,36 @@
         cardSelected = "";      
         flipCard.call(target);
       };
-    }, 400);
-    
-    function vanishCard() {
-      this.classList.add("card-vanish");
-      this.setAttribute("tabindex", "-1");
-    };
-    
-    function flipCard() {
-      this.firstElementChild.classList.toggle("card__front-flip");
-      this.lastElementChild.classList.toggle("card__back-flip");
-    };
-    
-    function lockCard() {
-      this.style.pointerEvents = "none";
-      this.setAttribute("disabled", "true");
-    };
-    
-    function unlockCard() {
-      this.style.pointerEvents = "auto";
-      this.removeAttribute("disabled");
-    };
+    }, animationDelay);
+  };
+  
+  function vanishCard() {
+    this.classList.add("card-vanish");
+    this.setAttribute("tabindex", "-1");
+  };
+  
+  function flipCard() {
+    this.firstElementChild.classList.toggle("card__front-flip");
+    this.lastElementChild.classList.toggle("card__back-flip");
+  };
+  
+  function lockCard() {
+    this.classList.add("card-locked");
+    this.setAttribute("disabled", "true");
+  };
+  
+  function unlockCard() {
+    this.classList.remove("card-locked");
+    this.removeAttribute("disabled");
   };
 
   function hailWinner() {
-    const popupHailWindow = document.querySelector(".popup_hail");
     popupHailWindow.classList.toggle("popup_hail-hide");
-    const okButton = document.querySelector(".popup_hail__button");
-    okButton.addEventListener("click", resetWindow);
-    
-    function resetWindow() {
-      okButton.removeEventListener("click", resetWindow);
-      popupHailWindow.classList.toggle("popup_hail-hide");
-      gameArea.classList.remove(gameArea.classList[1]);
-      gameArea.innerHTML = "";
-      switchControls();
-    };
+  };
+  
+  function resetWindow() {
+    popupHailWindow.classList.toggle("popup_hail-hide");
+    gameArea.className = "game_area";
+    gameArea.innerHTML = "";
+    switchControls();
   };
