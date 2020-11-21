49c712d6c47d21845a276e3b1d3bd729c5bff35d
webdevtoday <coderwd@gmail.com>
2020-11-21 12:50:03 +0200

    Changed render variable name

--- submissions/webdevtoday/task_js_dom/js/Controller.js
+++ submissions/webdevtoday/task_js_dom/js/Controller.js
@@ -3,12 +3,12 @@ import {Render} from './Render.js';
 export class Controller {
     constructor(root) {
         this.root = root;
-        this.render = new Render( this.root );
+        this.renderInstance = new Render( this.root );
     }
 
     get(controllerName) {
         if (!controllerName) {
-            this.render.render( `<h1>404 Page Not Found</h1>` );
+            this.renderInstance.render( `<h1>404 Page Not Found</h1>` );
             return false;
         }
         controllerName = controllerName[0].toUpperCase() + controllerName.slice(1).toLowerCase();
@@ -16,12 +16,12 @@ export class Controller {
             .then(controller => {
                 console.log('then');
                 const content = new controller.default();
-                this.render.render(content.get());
+                this.renderInstance.render(content.get());
             })
             .catch(err => {
                 console.log('catch');
                 console.log(err);
-                this.render.render( `<h1>404 Page Not Found</h1>` );
+                this.renderInstance.render( `<h1>404 Page Not Found</h1>` );
             });
     }
 }
