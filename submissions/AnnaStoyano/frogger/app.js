b1ccd3ac6de651ecfa3d892af30828cb09452d5d
Anna Stoyanova <annstoyanova99@gmail.com>
2020-11-25 22:27:39 +0200

    add constant position & use non-global variables

--- submissions/AnnaStoyano/frogger/app.js
+++ submissions/AnnaStoyano/frogger/app.js
@@ -17,20 +17,6 @@ Entity.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.position.x, this.position.y);
 }
 
-Entity.prototype.increaseLevel = function () {
-    this.level++;
-}
-
-Entity.prototype.checkWallCollision = function (wallX, wallY, newX, newY) {
-    if (this.position.x >= wallX) {
-        this.position.x = newX;
-    } else if (this.position.y >= wallY) {
-        this.position.y = newY;
-    }
-}
-
-/*Enemy*/
-
 const Enemy = function (x, y, speed, sprite, player) {
     Entity.call(this, x, y, speed, sprite);
     this.player = player;
@@ -39,30 +25,34 @@ const Enemy = function (x, y, speed, sprite, player) {
 Enemy.prototype = Object.create(Entity.prototype);
 Enemy.prototype.update = function (dt) {
     this.position.x += this.speed * dt * (this.level * 0.8);
-    this.checkWallCollision(canvas.width, canvas.height, -90, 0);
-    this.checkLose();
+    if (this.position.x >= canvas.width) {
+        this.position.x = -90;
+    }
+    let lose = this.checkCollision();
+    if (lose) {
+        this.player.enemies.forEach(enemy => enemy.level = 1);
+    }
+    this.increaseLevel();
 }
-
 Enemy.prototype.checkCollision = function () {
     let positionX = Math.floor(this.position.x);
-    if (positionX - 50 < player.position.x && player.position.x < positionX + 50) {
-        if (this.position.y - 55 < player.position.y && player.position.y < this.position.y + 20) {
+    if (positionX - 50 < this.player.position.x && this.player.position.x < positionX + 50) {
+        if (this.position.y - 55 < this.player.position.y && this.player.position.y < this.position.y + 20) {
             alert(`You lose!\nYour level is ${this.player.level}`);
+            this.player.restartPosition();
+            this.player.level = 1;
             return true;
         }
     }
     return false;
 }
-Enemy.prototype.checkLose = function () {
-    let lose = this.checkCollision();
-    if (lose) {
-        player.restartPosition();
-        player.level = 1;
-        player.enemies.forEach(enemy => enemy.level = 1);
+
+Enemy.prototype.increaseLevel = function () {
+    if (this.player.level != this.level) {
+        this.level++;
     }
 }
 
-/*Player*/
 
 const Player = function (x, y, speed, sprite) {
     Entity.call(this, x, y, speed, sprite);
@@ -75,12 +65,10 @@ Player.prototype.update = function () {
     this.checkWin();
     this.checkWallCollision();
 }
-
 Player.prototype.checkWin = function () {
     if (this.position.y < -10) {
         alert(`You win!\nYour level is ${this.level}`);
-        this.increaseLevel();
-        this.enemies.forEach(enemy => enemy.increaseLevel());
+        this.level++;
         this.restartPosition();
     }
 }
@@ -90,9 +78,12 @@ Player.prototype.restartPosition = function () {
 }
 
 Player.prototype.checkWallCollision = function () {
-    Entity.prototype.checkWallCollision.bind(this)(canvas.width - 80, canvas.height - 5, canvas.width - 85, canvas.height - 5);
     if (this.position.x < -30) {
         this.position.x = -20;
+    } else if (this.position.x > canvas.width - 80) {
+        this.position.x = canvas.width - 85;
+    } else if (this.position.y > canvas.height - 5) {
+        this.position.y = canvas.height - 5;
     }
 }
 
@@ -113,11 +104,32 @@ Player.prototype.handleInput = function (key) {
     }
 }
 
-const player = new Player(200, 404, 7, 'images/char-boy.png');
+const START_POSITION_PLAYER = {
+    X: 200,
+    Y: 404
+};
+const START_POSITION_ENEMY_1 = {
+    X: 0,
+    Y: 120
+};
+const START_POSITION_ENEMY_2 = {
+    X: 15,
+    Y: 220
+};
+const START_POSITION_ENEMY_3 = {
+    X: 30,
+    Y: 50
+};
+const PLAYER_SPEED = 7;
+const ENEMY_1_SPEED = 20;
+const ENEMY_2_SPEED = 60;
+const ENEMY_3_SPEED = 30;
 
-const enemy1 = new Enemy(30, 120, 60, 'images/enemy-bug.png', player);
-const enemy2 = new Enemy(15, 220, 30, 'images/enemy-bug.png', player);
-const enemy3 = new Enemy(0, 50, 20, 'images/enemy-bug.png', player);
+
+const player = new Player(START_POSITION_PLAYER.X, START_POSITION_PLAYER.Y, PLAYER_SPEED, 'images/char-boy.png');
+const enemy1 = new Enemy(START_POSITION_ENEMY_1.X, START_POSITION_ENEMY_1.Y, ENEMY_1_SPEED, 'images/enemy-bug.png', player);
+const enemy2 = new Enemy(START_POSITION_ENEMY_2.X, START_POSITION_ENEMY_2.Y, ENEMY_2_SPEED, 'images/enemy-bug.png', player);
+const enemy3 = new Enemy(START_POSITION_ENEMY_3.X, START_POSITION_ENEMY_3.Y, ENEMY_3_SPEED, 'images/enemy-bug.png', player);
 const allEnemies = [enemy1, enemy2, enemy3];
 
 player.enemies = allEnemies;
