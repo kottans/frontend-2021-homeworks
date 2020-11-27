d8ce63c0bfccd79751e647ab63e324269f809c3b
Andriy Klymeniuk <andrew.klmn@gmail.com>
2020-11-27 16:28:09 +0200

    Add: random users fetcher

--- submissions/andrewklmn/friends-app/app.js
+++ submissions/andrewklmn/friends-app/app.js
@@ -1,1 +1,25 @@
-console.log('App started');
+const state = {
+  friends: [],
+  filters: [], 
+  x: 10,
+}
+
+const drawFriendsList = (list)=> {
+  // TODO call DOM updater with new set of friends
+  console.log(list);
+}
+
+
+const initApp = (state) => {
+  fetch('https://randomuser.me/api/?results=5')
+    .then(response => response.json())
+    .then(json => drawFriendsList(json.results));
+
+  // Add filters driver
+
+};
+
+document.addEventListener('DOMContentLoaded',(event)=>{
+  initApp(state);
+
+})
