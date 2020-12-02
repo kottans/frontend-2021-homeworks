78ed62a9f38feb160660a02b36708d306e1ffdc0
ustymchyk <ustymchyk.nazar@gmail.com>
2020-12-02 11:28:36 +0200

    reset engine

--- submissions/ustymchyk/js-oop/app.js
+++ submissions/ustymchyk/js-oop/app.js
@@ -3,98 +3,144 @@ const Helper = {
     cellHeight: 83,
     cellOffsetTop: 50,
     cellOffsetBottom: 40,
-    getCanvasHeight(rows) {
-        return this.cellOffsetTop + this.cellOffsetBottom + rows * this.cellHeight;
-    },
-    getCanvasWidth(cols) {
-        return cols * this.cellWidth;
-    },
     get cellFullHeight() {
         return this.cellHeight + this.cellOffsetTop + this.cellOffsetBottom;
     }
 };
 ///////
-// Hero
+// Player
 ////// 
-const Hero = function () {
-    this.sprite = 'images/char-boy.png';
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
 
-    this.x = 0;
-    this.y = 0;
-    this.nextX = 0;
-    this.nextY = 0;
-    this.centerX = 0;
-    this.centerY = 0;
-}
-
-Hero.prototype.init = function () {
-    const canvasWidth = ctx.canvas.width;
-    const canvasHeight = ctx.canvas.height;
-
-    this.x = canvasWidth % 2 ? (canvasWidth - Helper.cellWidth) / 2 : canvasWidth / 2;
-    this.y = canvasHeight - Helper.cellHeight - Helper.cellOffsetTop - Helper.cellOffsetBottom - 25;
-    this.nextX = this.x;
-    this.nextY = this.y;
-
-    this.setCenter();
-}
-
-Hero.prototype.setCenter = function () {
-    this.centerX = this.x + Helper.cellWidth / 2;
-    this.centerY = this.y + Helper.cellOffsetTop + Helper.cellHeight / 2;
-}
-
-Hero.prototype.update = function () {
-    if (this.y !== this.nextY) {
-        this.y += this.stepY;
+        this.x = 0;
+        this.y = 0;
+        this.nextX = 0;
+        this.nextY = 0;
+        this.centerX = 0;
+        this.centerY = 0;
     }
-    if (this.x !== this.nextX) {
-        this.x += this.stepX;
+    init() {
+        const canvasWidth = ctx.canvas.width;
+        const canvasHeight = ctx.canvas.height;
+
+        this.x = canvasWidth % 2 ? (canvasWidth - Helper.cellWidth) / 2 : canvasWidth / 2;
+        this.y = canvasHeight - Helper.cellHeight - Helper.cellOffsetTop - Helper.cellOffsetBottom - 45;
+        this.nextX = this.x;
+        this.nextY = this.y;
+
+        this.setCenter();
     }
+    setCenter() {
+        this.centerX = this.x + Helper.cellWidth / 2;
+        this.centerY = this.y + Helper.cellOffsetTop + Helper.cellHeight / 2;
+    }
+    update() {
+        if (this.y !== this.nextY) {
+            this.y += this.stepY;
+        }
+        if (this.x !== this.nextX) {
+            this.x += this.stepX;
+        }
 
-    this.setCenter();
-}
+        this.setCenter();
+        this.checkWin();
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    checkWin() {
+        if (this.y === -27) {
+            alert('You won!');
 
-Hero.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+            this.init();
+        }
+    }
+    handleUp() {
+        const nextY = this.nextY - Helper.cellHeight;
 
-Hero.prototype.handleUp = function () {
-    const nextY = this.nextY - Helper.cellHeight;
+        if (nextY >= 0 - Helper.cellOffsetTop) {
+            this.nextY = nextY;
+            this.stepY = (nextY - this.y) / 8;
+        }
+    }
+    handleDown() {
+        const nextY = this.nextY + Helper.cellHeight;
 
-    if (nextY >= 0 - Helper.cellOffsetTop) {
-        this.nextY = nextY;
-        this.stepY = (nextY - this.y) / 8;
+        if (nextY <= ctx.canvas.height - Helper.cellFullHeight) {
+            this.nextY = nextY;
+            this.stepY = (nextY - this.y) / 8;
+        }
+    }
+    handleRight() {
+        const nextX = this.nextX + Helper.cellWidth;
+
+        if (nextX < ctx.canvas.width) {
+            this.nextX = nextX;
+            this.stepX = (nextX - this.x) / 8;
+        }
+    }
+    handleLeft() {
+        const nextX = this.nextX - Helper.cellWidth;
+        if (nextX >= 0) {
+            this.nextX = nextX;
+            this.stepX = (nextX - this.x) / 8;
+        }
     }
 }
 
-Hero.prototype.handleDown = function () {
-    const nextY = this.nextY + Helper.cellHeight;
+////////
+// Enemy
+///////
+class Enemy {
+    constructor(row, player) {
+        this.sprite = 'images/enemy-bug.png';
+        this.row = row;
+        this.player = player;
+    }
+    init() {
+        this.setSpeed();
+        this.setOffset();
+        this.y = this.row * Helper.cellHeight - 25;
+    }
+    update(dt) {
+        const canvasWidth = ctx.canvas.width;
 
-    if (nextY <= ctx.canvas.height - Helper.cellFullHeight) {
-        this.nextY = nextY;
-        this.stepY = (nextY - this.y) / 8;
+        if (this.x < canvasWidth) {
+            this.x += dt * this.speed;
+        } else {
+            this.x = -Helper.cellWidth;
+            this.setSpeed();
+        }
+
+        this.setCenter();
+        this.checkCollision();
+    }
+    setSpeed() {
+        this.speed = Math.random() * 300 + 200;
+    }
+    setOffset() {
+        this.x = -Helper.cellHeight - Math.random() * Helper.cellWidth * 5;
+    }
+    setCenter() {
+        this.centerX = this.x + Helper.cellWidth / 2;
+        this.centerY = this.y + Helper.cellOffsetTop + Helper.cellHeight / 2;
+    }
+    checkCollision() {
+        const playerCenterX = this.player.centerX;
+        const playerCenterY = this.player.centerY;
+
+        if (Math.abs(playerCenterX - this.centerX) < 65 && Math.abs(playerCenterY - this.centerY) < 55) {
+            alert('You lost!');
+
+            this.player.init();
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
-
-Hero.prototype.handleRight = function () {
-    const nextX = this.nextX + Helper.cellWidth;
-
-    if (nextX < ctx.canvas.width) {
-        this.nextX = nextX;
-        this.stepX = (nextX - this.x) / 8;
-    }
-}
-
-Hero.prototype.handleLeft = function () {
-    const nextX = this.nextX - Helper.cellWidth;
-    if (nextX >= 0) {
-        this.nextX = nextX;
-        this.stepX = (nextX - this.x) / 8;
-    }
-}
-
-const hero = new Hero();
 
 document.addEventListener('keyup', function (e) {
     const allowedKeys = {
@@ -107,57 +153,22 @@ document.addEventListener('keyup', function (e) {
     const methodName = allowedKeys[e.key];
 
     if (methodName) {
-        hero[methodName]()
+        player[methodName]()
     }
 });
 
-////////
-// Enemy
-///////
-const Enemy = function (row) {
-    this.sprite = 'images/enemy-bug.png';
-    this.row = row;
-};
+const player = new Player();
+const allEnemies = [];
 
-Enemy.prototype.init = function () {
-    this.setSpeed();
-    this.setOffset();
-    this.y = this.row * Helper.cellHeight - 25;
+for (let i = 0; i < 3; i++) {
+    const enemy = new Enemy(i + 1, player);
+    allEnemies.push(enemy);
 }
 
-Enemy.prototype.update = function (dt) {
-    const canvasWidth = ctx.canvas.width;
 
-    if (this.x < canvasWidth) {
-        this.x += dt * this.speed;
-    } else {
-        this.x = -Helper.cellWidth;
-        this.setSpeed();
-    }
-
-    this.setCenter();
-};
-
-Enemy.prototype.setSpeed = function () {
-    this.speed = Math.random() * 300 + 200;
-}
-
-Enemy.prototype.setOffset = function () {
-    this.x = -Helper.cellHeight - Math.random() * Helper.cellWidth * 5;
-}
-
-Enemy.prototype.setCenter = function () {
-    this.centerX = this.x + Helper.cellWidth / 2;
-    this.centerY = this.y + Helper.cellOffsetTop + Helper.cellHeight / 2;
-}
-
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-const allEnemies = [...Array(5).keys()].map(num => new Enemy(num + 1));
-
-/////////////
-// Start game
-////////////
-Engine(this);
+(function init() {
+    setTimeout(() => {
+        player.init();
+        allEnemies.forEach(enemy => enemy.init());
+    }, 0);
+})();
