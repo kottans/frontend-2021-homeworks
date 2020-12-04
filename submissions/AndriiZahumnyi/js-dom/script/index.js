da0a2afaf09c302a7ad3c3da1a3783e017ae5876
AndriiZahumnyi <41964713+AndriiZahumnyi@users.noreply.github.com>
2020-12-04 08:34:28 +0200

    Update index.js

--- submissions/AndriiZahumnyi/js-dom/script/index.js
+++ submissions/AndriiZahumnyi/js-dom/script/index.js
@@ -11,24 +11,15 @@ const asideMenuList = document.getElementById("aside-menu-list");
 
 const lengthOfArrAside = arrAside.length;
 
-function respondToTheClick(evt) {
 
-  const asideButton = arrAside.find(element => document.querySelector(element) === evt.target);
-  const indexOfAsideButton = arrAside.indexOf(asideButton);
-  const sectionContent = document.querySelector(arrContent[indexOfAsideButton]);
+function respondToTheClick({target}) {
 
-  for (let i = 0; i < lengthOfArrAside; i++) {
-    
-    const localSectionContent = document.querySelector(arrContent[i]);
-
-    localSectionContent.classList.add("section-none");
-    localSectionContent.classList.remove("section-block");
-
-  }
-
-  sectionContent.classList.add("section-block");
-  sectionContent.classList.remove("section-none");
-
+  const matchedAttribute = target.dataset.menu;
+  const activeSectionItem = document.querySelector('.section.active');
+  activeSectionItem.classList.remove('active');
+  const matchedSection = document.querySelector(`.section[data-content='${matchedAttribute}']`);
+  matchedSection.classList.add('active');
+  
 }
 
 hamburgerToggle.addEventListener('click', () => flexMenu.classList.toggle('active'));
