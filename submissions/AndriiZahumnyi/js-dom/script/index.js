1709d710d677e246e8a204e55e745aa56be93e0b
AndriiZahumnyi <41964713+AndriiZahumnyi@users.noreply.github.com>
2020-12-04 10:10:54 +0200

    Update index.js

--- submissions/AndriiZahumnyi/js-dom/script/index.js
+++ submissions/AndriiZahumnyi/js-dom/script/index.js
@@ -3,14 +3,7 @@ const hamburgerToggle = document.querySelector('#menu-toggle');
 
 const flexMenu = document.querySelector('.flex-menu');
 
-const arrAside = [".aside-html", ".aside-css", ".aside-bootstrap", ".aside-js", ".aside-jquery", ".aside-git"];
-
-const arrContent = [".html-section", ".css-section", ".bootstrap-section", ".js-section", ".jquery-section", ".git-section"];
-
 const asideMenuList = document.getElementById("aside-menu-list");
-
-const lengthOfArrAside = arrAside.length;
-
 
 function respondToTheClick({target}) {
 
@@ -22,6 +15,6 @@ function respondToTheClick({target}) {
   
 }
 
-hamburgerToggle.addEventListener('click', () => flexMenu.classList.toggle('active'));
+hamburgerToggle.addEventListener('click', () => flexMenu.classList.toggle('activeToggle'));
 
 asideMenuList.addEventListener('click', respondToTheClick);
