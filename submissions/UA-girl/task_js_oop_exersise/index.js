9308334da9ba46cc3e2cd30828b6d2641ceaf191
UA_Girl <n_kholodkova@ukr.net>
2020-12-05 23:25:27 +0200

    Fixed

--- submissions/UA-girl/task_js_oop_exersise/index.js
+++ submissions/UA-girl/task_js_oop_exersise/index.js
@@ -8,53 +8,48 @@
 // ======== OBJECTS DEFINITIONS ========
 
 class Creature {
-    constructor(species, name, age, gender, say) {
+    constructor(species, name, age, gender, legs, say, friends = []) {
         this.species = species;
         this.name = name;
         this.age = age;
         this.gender = gender;
         this.say = say;
-    }
-
-    getDescription() {
-        return `${this.say}! My name is ${this.name} and I am ${this.age} years old ${this.gender} ${this.species}. `}
-}
-
-class Person extends Creature {
-    constructor(species, name, age, gender, say, friends = []) {
-        super(species, name, age, gender, say);
-        this.hands = 2;
-        this.legs = 2;
+        this.legs = legs
         this.friends = friends.length > 0 ? friends : [];
     }
 
-    getDescription() {
-        let message = super.getDescription();
-        message += `I have ${this.legs} legs and ${this.hands} hands. My friends are ${this.friends.join(', ')}.`;
-        return message
+    toString() {
+        return `${this.say}! My name is ${this.name} and I am ${this.age} years old ${this.gender} ${this.species}.`
+    }
+}
+
+class Person extends Creature {
+    constructor(species, name, age, gender, legs, say, friends) {
+        super(species, name, age, gender, legs, say, friends);
+        this.hands = 2;
+    }
+
+    toString() {
+        return super.toString() + ` I have ${this.legs} legs and ${this.hands} hands. My friends are ${this.friends.join(', ')}.`
     }
 }
 
 class Animal extends Creature {
-    constructor(species, name, age, gender, say, friends = []) {
-        super(species, name, age, gender, say);
-        this.legs = 4;
-        this.friends = friends.length > 0 ? friends : [];
+    constructor(species, name, age, gender, legs, say, friends) {
+        super(species, name, age, gender, legs, say, friends);
     }
 
-    getDescription() {
-        let message = super.getDescription();
-        message += `I have ${this.legs} legs. My friends are ${this.friends.join(', ')}.`;
-        return message
+    toString() {
+        return super.toString() + ` I have ${this.legs} legs. My friends are ${this.friends.join(', ')}.`;
     }
 }
 
-const woman = new Person(species='human', name='Alice', age = 25, gender = 'female', say = 'Hello, dear!', friends = ['Adam', 'Stephany', 'Iren']);
-const man = new Person(species='human', name='Adam', age = 35, gender = 'male', say = 'Hey!', friends = ['Alice', 'Stephany', 'Jeorge', 'Jack']);
-const dog = new Animal(species='dog', name='Margo', age = 7, gender = 'female', say = 'Buf-buf!', friends = ['Jack', 'Pashtet', 'Cesar']);
-const cat = new Animal(species='cat', name='Murzik', age = 4, gender = 'male', say = 'Meow-meow!', friends = ['Margo', 'Murka']);
+const woman = new Person('human', 'Alice', 25, 'female', 2, 'Hello, dear!', ['Adam', 'Stephany', 'Iren']);
+const man = new Person('human', 'Adam', 35, 'male', 2, 'Hey!', ['Alice', 'Stephany', 'Jeorge', 'Jack']);
+const dog = new Animal('dog', 'Margo', 7, 'female', 4, 'Buf-buf!', ['Jack', 'Pashtet', 'Cesar']);
+const cat = new Animal('cat', 'Murzik', 4, 'male', 4, 'Meow-meow!', ['Margo', 'Murka']);
 
 // ======== OUTPUT ========
 
 const inhabitats = [man, woman, dog, cat];
-inhabitats.forEach(item => print(item.getDescription(), 'div'));
+inhabitats.forEach(item => print(item.toString(), 'div'));
