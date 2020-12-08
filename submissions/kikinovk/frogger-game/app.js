ce96169564ebd1b0587c31403bfbda821e045f30
Kost Kikinov <kikinov.k@gmail.com>
2020-12-08 13:46:16 +0200

    fixed the code according to the checklist

--- submissions/kikinovk/frogger-game/app.js
+++ submissions/kikinovk/frogger-game/app.js
@@ -1,103 +1,167 @@
-// Enemies our player must avoid
-var Enemy = function(x,y) {
-  // Variables applied to each of our instances go here,
-  // we've provided one for you to get started
+const ENEMY_START_POS_X = -100;
+const ENEMY_END_POS_X = 500;
+const ENEMY_BLOCK_HEIGHT = 85;
+const ENEMY_POS_INDENT = 60;
 
-  // The image/sprite for our enemies, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';    
-  this.x = x;
-  this.y = y;
-  this.v = Math.random()*500;
+const PLAYER_START_POS_X = 200;
+const PLAYER_START_POS_Y = 400;
+const PLAYER_END_POS_Y = -15;
+const PLAYER_MIN_POS_X = 0;
+const PLAYER_MAX_POS_X = 400;
+const PLAYER_MIN_POS_Y = -15;
+const PLAYER_MAX_POS_Y = 400;
+const PLAYER_STEP_X = 100;
+const PLAYER_STEP_Y = 83;
+
+const SPEED_COEFF = 500;
+
+const CLASH_X = 80;
+const CLASH_Y = 15;
+
+
+const PLAYER_CONF = {
+    initPosition: {
+        x: PLAYER_START_POS_X,
+        y: PLAYER_START_POS_Y
+    },
+    sprite: 'images/char-boy.png'
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+const ENEMY_CONF = [
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 0
+        },
+        sprite: 'images/enemy-bug.png'
+    },
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 0
+        },
+        sprite: 'images/enemy-bug.png'
+    },
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 1
+        },
+        sprite: 'images/enemy-bug.png'
+    },
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 1
+        },
+        sprite: 'images/enemy-bug.png'
+    },
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 2
+        },
+        sprite: 'images/enemy-bug.png'
+    },
+    {
+        initPosition: {
+            x: ENEMY_START_POS_X,
+            y: ENEMY_POS_INDENT + ENEMY_BLOCK_HEIGHT * 2
+        },
+        sprite: 'images/enemy-bug.png',
+        speed: 150
+    },
+];
+
+
+const Character = function(x,y, sprite) {
+    this.x = x;
+    this.y = y;
+    this.sprite = sprite;
+};
+
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+const Enemy = function(x, y, sprite, speed) {
+    Character.call(this, x, y, sprite);
+    this.speed = speed ? speed : this.randomSpeed(SPEED_COEFF);
+
+};
+
+Enemy.prototype = Object.create(Character.prototype);
+
+Enemy.prototype.randomSpeed = function(coeff){
+    return Math.random()*coeff;
+};
+
 Enemy.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-  this.x += dt * this.v;
-  if (this.x >= 500) {
-      this.x = -100;
-      this.v = Math.random()*500;
-  }
+    this.x += dt * this.speed;
+    if (this.x >= ENEMY_END_POS_X) {
+        this.x = ENEMY_START_POS_X;
+        this.speed = this.randomSpeed(SPEED_COEFF);
+    }
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+const Player = function(x, y, sprite) {
+    Character.call(this, x, y, sprite);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-var Player = function(x,y) {
-  this.sprite = 'images/char-boy.png';
-  this.x = x;
-  this.y = y;
+Player.prototype = Object.create( Character.prototype);
+
+Player.prototype.reset = function() {
+    this.x = PLAYER_START_POS_X;
+    this.y = PLAYER_START_POS_Y;
+};
+
+Player.prototype.checkCollision = function(enemy) {
+    if ( Math.abs(this.y - enemy.y) < CLASH_Y &&  Math.abs(this.x - enemy.x) < CLASH_X) {
+        this.reset();
+    };
 };
 
 Player.prototype.update = function(dt) {
-  allEnemies.forEach(enemy => {
-      if ( Math.abs(this.y - enemy.y) < 15 &&  Math.abs(this.x - enemy.x) < 80) {
-          this.x = 200;
-          this.y = 400;
-      }
-  });
-  if (this.y === -15) {
-      alert('YOU WIN!!');
-      this.x = 200;
-      this.y = 400;
-  }
-};
-
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    if (this.y === PLAYER_END_POS_Y) {
+        alert('YOU WIN!!');
+        this.reset();
+    }
 };
 
 Player.prototype.handleInput = function(move) {
-  var stepX = 100;
-  var stepY = 83;
-  var minX = 0;
-  var maxX = 400;
-  var minY = -15;
-  var maxY = 400;
-  var addStep = function(startPosition, min, max, step){
-      var newPosition = startPosition + step;
-      return newPosition >= max ? max : newPosition  <= min ? min : newPosition
-  }
-  switch(move) {
-      case 'left':
-          this.x = addStep(this.x, minX, maxX, -stepX);
-          break;
-      case 'up':
-          this.y = addStep(this.y, minY, maxY, -stepY);
-          break;
-      case 'right':
-          this.x = addStep(this.x, minX, maxX, stepX);
-          break;
-      case 'down':
-          this.y = addStep(this.y, minY, maxY, stepY);
-          break;
-  }
+
+    let addStep = function(startPosition, min, max, step){
+        var newPosition = startPosition + step;
+        return newPosition >= max ? max : newPosition  <= min ? min : newPosition
+    };
+    switch(move) {
+        case 'left':
+            this.x = addStep(this.x, PLAYER_MIN_POS_X, PLAYER_MAX_POS_X, -PLAYER_STEP_X);
+            break;
+        case 'up':
+            this.y = addStep(this.y, PLAYER_MIN_POS_Y, PLAYER_MAX_POS_Y, -PLAYER_STEP_Y);
+            break;
+        case 'right':
+            this.x = addStep(this.x, PLAYER_MIN_POS_X, PLAYER_MAX_POS_X, PLAYER_STEP_X);
+            break;
+        case 'down':
+            this.y = addStep(this.y, PLAYER_MIN_POS_Y, PLAYER_MAX_POS_Y, PLAYER_STEP_Y);
+            break;
+    }
 };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-var allEnemies = [new Enemy(-100, 60), new Enemy(-100, 145), new Enemy(-100, 225),new Enemy(-100, 60), new Enemy(-100, 145), new Enemy(-100, 225)];
-// Place the player object in a variable called player
-var player = new Player(200, 400);
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+const allEnemies = ENEMY_CONF.map(enemy => new Enemy(enemy.initPosition.x, enemy.initPosition.y, enemy.sprite));
+
+const player = new Player(PLAYER_CONF.initPosition.x, PLAYER_CONF.initPosition.y, PLAYER_CONF.sprite, PLAYER_CONF.speed);
+
 document.addEventListener('keyup', function(e) {
-  var allowedKeys = {
-      37: 'left',
-      38: 'up',
-      39: 'right',
-      40: 'down'
-  };
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
 
-  player.handleInput(allowedKeys[e.keyCode]);
+    player.handleInput(allowedKeys[e.keyCode]);
 });
