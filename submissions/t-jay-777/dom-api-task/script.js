1aeb7e01d94aa910aac3b6baf2ee1c6b7f804e7e
Yevhen Tkachenko <joy_123@ukr.net>
2020-12-14 21:32:00 +0100

    Add a url for images

--- submissions/t-jay-777/dom-api-task/script.js
+++ submissions/t-jay-777/dom-api-task/script.js
@@ -3,7 +3,7 @@ const contentList = [
     {
         id: 000,
         name: 'Big Mac @ McDonald’s',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/000-Big-Mac.jpg',
+        url: 'https://i.postimg.cc/1XYr1vVg/000-Big-Mac.jpg',
         text: 'Call us crazy, but how can we possibly leave out a burger that sells 550 million times each year in America alone?\
         That means that every second, there are 17 people across the country stuffing their face with the McDonald\’s icon.\
         It might not be topped with exotic truffles of served alongside a sparkly bottle of rear 1950s Champagne, but it sells.\
@@ -14,7 +14,7 @@ const contentList = [
     {
         id: 001,
         name: 'FleurBurger @ Fleur',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/001-FleurBurger.jpg',
+        url: 'https://i.postimg.cc/Fs8VPnQ8/001-Fleur-Burger.jpg',
         text: 'Arguably, a quick burger feed at Shake Shack or In-N-Out will do the trick,\
         and with a bill of under $10, it’s hard to say no. Over at the Fleur restaurant in Las Vegas, however, $10 won\’t\
         even get you a napkin. The FleurBurger, which is considered to be one of the world’s best burgers,\
@@ -26,7 +26,7 @@ const contentList = [
     {
         id: 002,
         name: 'The Butchers Club Burger @ The Butchers Club',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/002-0AThe-Butchers-Club-Burger.jpg',
+        url: 'https://i.postimg.cc/XJsw9s82/002-0-AThe-Butchers-Club-Burger.jpg',
         text: 'It might come as a bit of a surprise to find one of the world’s best burgers (a dish which is typically American)\
         in an Asian locale, however, when we consider the number of internationals in the city, as well as the fact that its executive\
         chef is from Canada, it starts to make a little more sense. He even said himself that “hamburgers are something you grow\
@@ -37,7 +37,7 @@ const contentList = [
     {
         id: 003,
         name: 'The Smokey Burger @ Burger Liquor',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/003-The-Smokey-Burger-at-Burger-Liquor.jpg',
+        url: 'https://i.postimg.cc/rmV1dBS0/003-The-Smokey-Burger-at-Burger-Liquor.jpg',
         text: 'Yes, everything’s bigger in Texas, but for our next stop, we’re heading down to Wellington, New Zealand, where,\
         apparently, everything’s more delicious. Over at Burger Liquor, the owners wanted to dish up top-of-the-range burgers and\
         drinks, without customers having to wait half an hour, and they’ve executed their vision to perfection.\
@@ -48,7 +48,7 @@ const contentList = [
     {
         id: 004,
         name: 'The Ozersky Burger',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/004-The-Ozersky-Burger.jpg',
+        url: 'https://i.postimg.cc/VNcBwrvF/004-The-Ozersky-Burger.jpg',
         text: 'When you’ve been rated as the best burger in Dallas, a city with a booming foodie scene,\
         then you know you’ve done something rather extraordinary. That’s exactly the case with The Ozersky Burger at Knife Dallas,\
         named after burger aficionado Josh Ozersky (who preferred his meals to be very simplistic).\
@@ -59,7 +59,7 @@ const contentList = [
     {
         id: 005,
         name: 'The Burger @ Dirty Bones',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/005-dirty-bones-burger-london.jpg',
+        url: 'https://i.postimg.cc/Cx8H9Hh1/005-dirty-bones-burger-london.jpg',
         text: 'With six ounces of aged beef alongside black treacle bacon, gooey gorgonzola sauce, pickled jalapeños, baby\
         spinach and a slathering of garlic aioli, ‘The Burger’ at Dirty Bones in Kensington, London is sure to satisfy burger enthusiasts,\
         be them from the area or from afar. It’s constantly rated amongst the most impressive burgers, not only citywide but across the continent.\
@@ -69,7 +69,7 @@ const contentList = [
     {
         id: 006,
         name: 'American Burger @ Gordon Ramsay Burger',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/006-gordon-ramsay-american-burger-las-vegas.jpg',
+        url: 'https://i.postimg.cc/wBc5jqsY/006-gordon-ramsay-american-burger-las-vegas.jpg',
         text: 'With six ounces of aged beef alongside black treacle bacon, gooey gorgonzola sauce, pickled jalapeños,\
         baby spinach and a slathering of garlic aioli, ‘The Burger’ at Dirty Bones in Kensington, London is sure to satisfy burger enthusiasts,\
         be them from the area or from afar. It’s constantly rated amongst the most impressive burgers, not only citywide but across the continent.\
@@ -79,7 +79,7 @@ const contentList = [
     {
         id: 007,
         name: 'The Jackie-O @ Fat Bobs Bar & Grill',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/007-fat-bobs-burger.jpg',
+        url: 'https://i.postimg.cc/J0tjxjYR/007-fat-bobs-burger.jpg',
         text: 'For the next leg of our juicy burger journey around the world, we’re heading all the way to the Land Down Under,\
         specifically the southeastern suburbs of Melbourne. In the industrial Moorabbin area, burger enthusiasts are shocked to find one of\
         the country’s best nestled in a place you wouldn’t ordinarily expect. The aesthetic takes inspiration from the classic 1950s\
@@ -90,7 +90,7 @@ const contentList = [
     {
         id: 008,
         name: 'The Burger @ Raoul\'s',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/008-The-Burger0ARaouls.jpg',
+        url: 'https://i.postimg.cc/1XTwj4bw/008-The-Burger0-ARaouls.jpg',
         text: 'Nestled on Prince St in Soho, Lower Manhattan, New York, Raoul\'s offers a unique Parisian atmosphere\
         with one of the world’s most devourable burgers. With such a revered reputation, Raoul’s needs to limit their burger\
         production, otherwise they’d never be able to meet the demand - the restaurant only makes 12 burgers a night, so samplings are competitive.\
@@ -101,7 +101,7 @@ const contentList = [
     {
         id: 009,
         name: 'Bleecker Black Burger @ Bleecker',
-        url: 'https://github.com/t-jay-777/dom.github.io/blob/main/img/009-Bleecker-Black-Burger.jpg',
+        url: 'https://i.postimg.cc/VsHq6H5v/009-Bleecker-Black-Burger.jpg',
         text: 'Burger lovers will find this juicy plate of goodness over at Bleecker in London, which is aptly named\
         after the owner Zan Kaufman’s favorite street in Manhattan. The restaurant has received the astounding accolade of the city\'s best\
         burger not once, but twice, voted by Time Out London.\
