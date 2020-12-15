7d0ee14720fb31aaf738cb3988b747b48174cf83
Sergey Gavrilov <ses3332002@gmail.com>
2020-12-15 20:28:46 +0200

    friends app improved

--- submissions/ses3332002/friends-app/app.js
+++ submissions/ses3332002/friends-app/app.js
@@ -2,27 +2,33 @@
 const hamburgerMenu = document.querySelector(".hamburger_menu");
 const mainArea = document.querySelector("main");
 const optionForm = document.forms.sorting_n_filtering;
+const asIsOption = document.querySelector("#as_is");
+const allOption = document.querySelector("#all");
+const ageFromOption = document.querySelector("#age_from");
+const ageToOption = document.querySelector("#age_to");
+const searchNameOption = document.querySelector(".filter_name");
+const requestString = "https://randomuser.me/api/?nat=gb,us,fr&results=60&inc=gender,name,location,email,picture,dob";
 let receivedData = [];
 let dataToRender = [];
 let filterCriterionSex = () => true;
 let filterCriterionAge = () => true;
 let filterCriterionName = () => true;
-let sortCriterion = () => true;
-as_is.checked = true;
-all.checked = true;
-age_from.value = "";
-age_to.value = "";
-search_name.value = "";
+let sortCriterion = () => false;
+asIsOption.checked = "true";
+allOption.checked = "true";
+ageFromOption.value = "";
+ageToOption.value = "";
+searchNameOption.value = "";
 hamburgerMenu.addEventListener("click", () => hamburgerMenu.classList.toggle("hamburger_menu-opened"));
 optionForm.addEventListener("input", changeData);
 
-fetch("https://randomuser.me/api/?nat=gb,us,fr&results=60&inc=gender,name,location,email,picture,dob")
+fetch(requestString)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
     receivedData = data.results;
-    dataToRender = receivedData.slice().filter(filterCriterionSex).filter(filterCriterionAge).filter(filterCriterionName).sort(sortCriterion);
+    dataToRender = receivedData.slice();
     render();
   });
 
@@ -31,11 +37,14 @@ function render() {
   const contentToRender = document.createDocumentFragment();
   dataToRender.forEach((item) => {
     let card = document.createElement("div");
-    let lastNumberOfAge = item.dob.age.toString().slice(-1);
+    function sexString(age) {
+      const lastNumberOfAge = age.toString().slice(-1);
+      return (lastNumberOfAge == "1") ? "год" : (lastNumberOfAge == "2")||(lastNumberOfAge == "3")||(lastNumberOfAge == "4") ? "года" : "лет";
+    };
     card.classList.add("friend_card");
     card.innerHTML = `<img src=${item.picture.large} alt="photo" class="friend_card__photo">\
     <div class="friend_card__text friend_card__name">${item.name.first} ${item.name.last}</div>\
-    <div class="friend_card__text friend_card__age">${item.dob.age} ${(lastNumberOfAge == "1") ? "год" : (lastNumberOfAge == "2")||(lastNumberOfAge == "3")||(lastNumberOfAge == "4") ? "года" : "лет"}</div>\
+    <div class="friend_card__text friend_card__age">${item.dob.age} ${sexString(item.dob.age)}</div>\
     <div class="friend_card__text friend_card__sex">${item.gender == "male" ? "мужчина" : "женщина"}</div>\
     <div class="friend_card__text friend_card__address">${item.location.country}, ${item.location.city}</div>\
     <div class="friend_card__text friend_card__email">${item.email}</div>`;
@@ -48,28 +57,39 @@ function changeData({target}) {
   if (target.tagName != "INPUT") {
     return;
   };
-  if (target.id == "as_is") {
-    sortCriterion = () => true;
-  } else if (target.id == "nameAZ") {
-    sortCriterion = (itemA, itemB) => itemA.name.first > itemB.name.first ? 1 : -1;
-  } else if (target.id == "nameZA") {
-    sortCriterion = (itemA, itemB) => itemA.name.first < itemB.name.first ? 1 : -1;
-  } else if (target.id == "age19") {
-    sortCriterion = (itemA, itemB) => itemA.dob.age > itemB.dob.age ? 1 : -1;
-  } else if (target.id == "age91") {
-    sortCriterion = (itemA, itemB) => itemA.dob.age < itemB.dob.age ? 1 : -1;
-  } else if (target.id == "all") {
-    filterCriterionSex = () => true;
-  } else if (target.id == "man") {
-    filterCriterionSex = (item) => item.gender == "male";
-  } else if (target.id == "woman") {
-    filterCriterionSex = (item) => item.gender == "female";
-  } else if ((target.id == "age_from")||(target.id == "age_to")) {
-    if ((age_from.value)&&(age_to.value)) {
-      filterCriterionAge = (item) => (item.dob.age >= age_from.value)&&(item.dob.age <= age_to.value);
-    };
-  } else if (target.id == "search_name") {
-    filterCriterionName = (item) => (item.name.first.toLowerCase().includes(search_name.value.toLowerCase()))||(item.name.last.toLowerCase().includes(search_name.value.toLowerCase()));
+  switch (target.id) {
+    case "as_is":
+      sortCriterion = () => false;
+      break;
+    case "nameAZ":
+      sortCriterion = (itemA, itemB) => itemA.name.first > itemB.name.first ? 1 : -1;
+      break;
+    case "nameZA":
+      sortCriterion = (itemA, itemB) => itemA.name.first < itemB.name.first ? 1 : -1;
+      break;
+    case "age19":
+      sortCriterion = (itemA, itemB) => itemA.dob.age > itemB.dob.age ? 1 : -1;
+      break;
+    case "age91":
+      sortCriterion = (itemA, itemB) => itemA.dob.age < itemB.dob.age ? 1 : -1;
+      break;
+    case "all":
+      filterCriterionSex = () => true;
+      break;
+    case "man":
+      filterCriterionSex = (item) => item.gender == "male";
+      break;
+    case "woman":
+      filterCriterionSex = (item) => item.gender == "female";
+      break;
+    case "age_from":
+    case "age_to":
+      if ((ageFromOption.value)&&(ageToOption.value)) {
+        filterCriterionAge = (item) => (item.dob.age >= ageFromOption.value)&&(item.dob.age <= ageToOption.value);
+      };
+      break;
+    case "search_name":
+      filterCriterionName = (item) => (item.name.first.toLowerCase().includes(searchNameOption.value.toLowerCase()))||(item.name.last.toLowerCase().includes(searchNameOption.value.toLowerCase()));
   };
   dataToRender = receivedData.slice().filter(filterCriterionSex).filter(filterCriterionAge).filter(filterCriterionName).sort(sortCriterion);
   render();
