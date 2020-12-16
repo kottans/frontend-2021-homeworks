c9b81dc5d7ce53369547ff062a378e7be0e5c016
Olha Rybchynska <olha.rybchynska@gmail.com>
2020-12-17 00:14:40 +0200

    add post-oop tiny-js-world

---
+++ submissions/ORybka/post-a-tiny-js-world/index.js
@@ -0,0 +1,61 @@
+/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+   Complete the below for code reviewers' convenience:
+
+   Code repository: https://github.com/ORybka/a-tiny-JS-world
+   Web app: https://orybka.github.io/a-tiny-JS-world/
+   */
+
+// ======== OBJECTS DEFINITIONS ========
+class Inhabitant {
+  constructor(species, name, gender, legs, hands, saying, friends) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.legs = legs;
+    this.hands = hands;
+    this.saying = saying;
+    this.friends = friends;
+  }
+
+  callFriends() {
+    return this.friends.join(', ');
+  }
+
+  getValues() {
+    return [this.species, `<strong>${this.name}</strong>`, this.gender, this.legs, this.hands, `<em>${this.saying}</em>`, this.callFriends()];
+  }
+
+  printValues() {
+    return this.getValues().join('; ');
+  }
+}
+
+class Human extends Inhabitant {
+  constructor(name, gender, saying, friends) {
+    super('human', name, gender, 2, 2, saying, friends);
+  }
+}
+
+const man = new Human('John', 'male', "Hey! What's up?", ['Mary', 'Marley', 'Tisha']);
+const woman = new Human('Mary', 'female', 'Hey there!', ['John', 'Marley', 'Tisha']);
+
+class Animal extends Inhabitant {
+  constructor(species, name, gender, saying, friends) {
+    super(species, name, gender, 4, 0, saying, friends);
+  }
+}
+
+const dog = new Animal('dog', 'Marley', 'male', 'woof-woof!', ['John', 'Mary', 'Selina']);
+const cat = new Animal('cat', 'Tisha', 'male', 'meow-purr!', []);
+
+class CatWoman extends Inhabitant {
+  constructor(name, gender, friends) {
+    super('catWoman', name, gender, 2, 2, cat.saying, friends);
+  }
+}
+
+const catWoman = new CatWoman('Selina', 'female', ['Tisha']);
+
+// ======== OUTPUT ========
+
+[man, woman, dog, cat, catWoman].forEach((el) => print(el.printValues(), 'div'));
