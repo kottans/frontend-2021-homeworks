a59dcd080f41c617f3c8a44ab95d2e67e0b1da12
Chyvikov-Anton <chivikovanton@gmail.com>
2020-12-17 16:09:34 +0200

    Added solution after code review #1

--- submissions/Ant-C-tech/frogger-game/app.js
+++ submissions/Ant-C-tech/frogger-game/app.js
@@ -12,6 +12,7 @@ const ENEMY_START_MAX = FIELD_LEFT_BORDER - 100;
 const ENEMY_FINISH = FIELD_RIGHT_BORDER + 200;
 const ENEMY_MIN = 3;
 const ENEMY_MAX = 9;
+const NUMBER_OF_ENEMIES = _getRandomIntInclusive(ENEMY_MIN, ENEMY_MAX);
 
 const PLAYER_START_X = 200;
 const PLAYER_START_Y = 400;
@@ -20,55 +21,50 @@ const PLAYER_WIDTH = 50;
 const BODY = document.querySelector('body');
 
 
-const Enemy = function (x, y) {
+const Enemy = function(x, y) {
     this.x = x;
     this.y = y;
     this.speedValue = _getRandomIntInclusive(MIN_ENEMY_SPEED, MAX_ENEMY_SPEED);
     this.sprite = 'images/enemy-bug.png';
 };
 
-Enemy.prototype.update = function (dt) {
+Enemy.prototype.update = function(dt) {
+    this.move(dt);
+    this.checkFinish();
+};
+
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Enemy.prototype.move = function(dt) {
     this.x = this.x + this.speedValue * dt;
+};
+
+Enemy.prototype.checkFinish = function() {
     if (this.x > ENEMY_FINISH) {
         this.x = _getRandomIntInclusive(ENEMY_START_MIN, ENEMY_START_MAX);
         this.speedValue = _getRandomIntInclusive(MIN_ENEMY_SPEED, MAX_ENEMY_SPEED);
     };
 };
 
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
-const Player = function (x, y) {
+const Player = function(x, y, allEnemies) {
     this.x = x;
     this.y = y;
+    this.allEnemies = allEnemies
     this.sprite = "images/char-boy.png";
 };
 
-Player.prototype.update = function (dt) {
-    for (const bug of allEnemies) {
-        if (bug.y === this.y) {
-            if (bug.x <= this.x + PLAYER_WIDTH && bug.x >= this.x - PLAYER_WIDTH) {
-                this.x = PLAYER_START_X;
-                this.y = PLAYER_START_Y;
-
-                _addMessage('Sorry! You lose!');
-            };
-        };
-    };
-    if (this.y < FIELD_TOP_BORDER) {
-        this.x = PLAYER_START_X;
-        this.y = PLAYER_START_Y;
-
-        _addMessage('Congratulations! You won!');
-    };
+Player.prototype.update = function() {
+    this.checkCollision();
 };
 
-Player.prototype.render = function () {
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-Player.prototype.handleInput = function (key) {
+Player.prototype.handleInput = function(key) {
     this.lastX = this.x;
     this.lastY = this.y;
 
@@ -95,27 +91,46 @@ Player.prototype.handleInput = function (key) {
     if (this.y > PLAYER_START_Y) {
         this.y = this.lastY;
     };
+
+    this.checkWin();
+};
+
+Player.prototype.checkCollision = function() {
+    for (const bug of this.allEnemies) {
+        if (bug.y === this.y) {
+            if (bug.x <= this.x + PLAYER_WIDTH && bug.x >= this.x - PLAYER_WIDTH) {
+                this.x = PLAYER_START_X;
+                this.y = PLAYER_START_Y;
+
+                _addMessage('Sorry! You lose!');
+            };
+        };
+    };
+};
+
+Player.prototype.checkWin = function() {
+    if (this.y < FIELD_TOP_BORDER) {
+        this.x = PLAYER_START_X;
+        this.y = PLAYER_START_Y;
+
+        _addMessage('Congratulations! You won!');
+    };
 };
 
 
 let allEnemies = [];
-for (let index = 0; index < _getRandomIntInclusive(ENEMY_MIN, ENEMY_MAX); index++) {
-    let bugY;
-    if (index < 3) {
-        bugY = (FIELD_TOP_BORDER + index * FIELD_CELL_HEIGHT);
-    } else {
-        bugY = (FIELD_TOP_BORDER + (index % 3) * FIELD_CELL_HEIGHT);
-    };
+for (let index = 0; index < NUMBER_OF_ENEMIES; index++) {
+    const bugY = (FIELD_TOP_BORDER + (index % 3) * FIELD_CELL_HEIGHT);
     const bug = new Enemy(_getRandomIntInclusive(ENEMY_START_MIN, ENEMY_START_MAX), bugY);
     allEnemies.push(bug);
 };
 
-const player = new Player(PLAYER_START_X, PLAYER_START_Y);
+const player = new Player(PLAYER_START_X, PLAYER_START_Y, allEnemies);
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function(e) {
     e.preventDefault();
 });
-document.addEventListener('keyup', function (e) {
+document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
