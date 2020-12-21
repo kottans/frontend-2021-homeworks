70256fc68fefce5a6cc29bfd04bf57e747ce2189
UA_Girl <n_kholodkova@ukr.net>
2020-12-21 07:59:39 +0200

    Fixed sorting function, variables names

--- submissions/UA-girl/task_friends_app/js/app.js
+++ submissions/UA-girl/task_friends_app/js/app.js
@@ -1,5 +1,7 @@
 const URL_FETCH = 'https://randomuser.me/api/?results=100&nat=GB';
 const CARDS_PER_PAGE = 10;
+const MAX_AGE = 100;
+const BREAK_POINT_WIDTH = 467;
 const filterSet = document.querySelector('.container-filters');
 const pages = document.querySelector('.pages');
 let cards = [];
@@ -28,7 +30,7 @@ class Person {
             this.card.classList.add('female');
         }
         let cardContent = `<picture>
-<source media="(min-width: 467px)" srcset="${this.picture.large}">
+<source media="(min-width: ${BREAK_POINT_WIDTH}px)" srcset="${this.picture.large}">
 <img src="${this.picture.medium}" alt="${this.name.first}'s photo" class="card-image">
 </picture>
 <figcaption class="card-description">
@@ -41,23 +43,24 @@ ${this.name.first} ${this.name.last}
 </div>
 <div class="card-info card-info__contacts">
 <h4 class="contacts-title">Contact information:</h4>
-<p class="email">${this.email}</p>
-<p class="phone">${this.phone}, ${this.cell}</p>
+<a class="email" href="mailto: ${this.email}">${this.email}</a>
+<a class="phone" href="tel: ${this.phone}">${this.phone},</a>
+<a class="phone" href="tel: ${this.cell}">${this.cell}</a>
 </div>
 <p class="registration"><span class="card-subtitle">Registered </span>${this.registered.age} years ago</p>
 </figcaption>`;
         this.card.innerHTML = cardContent;
     }
 
-    ageCompare(other) {
+    compareAge(other) {
         return this.age - other.age;
     }
 
-    nameCompare(other) {
+    compareName(other) {
         return this.name.first.toLowerCase().localeCompare(other.name.first.toLowerCase());
     }
 
-    dateRegistrationCompare(other) {
+    compareDateRegistration(other) {
         return Date.parse(this.registered.date) - Date.parse(other.registered.date);
     }
 }
@@ -66,28 +69,13 @@ class App {
     constructor(container) {
         this.container = container;
         this.filterStates = {
-            gender: {
-                toggled: false,
-                gender: null,
-            },
-            age: {
-                toggled: false,
-                age: null,
-            },
+            gender: null,
+            age: null
         };
         this.sortingStates = {
-            name: {
-                toggled: false,
-                direction: null,
-            },
-            registration: {
-                toggled: false,
-                direction: null
-            },
-            age: {
-                toggled: false,
-                direction: null,
-            }
+            name: null,
+            registration: null,
+            age: null
         };
         this.cards = [];
         this.currentPage = 0;
@@ -97,134 +85,92 @@ class App {
         this.currentPage = 0;
         this.cards = cards.slice();
         for (let [filter, state] of Object.entries(this.filterStates)) {
-            if (state.toggled) {
+            if (state) {
                 switch (filter) {
                     case 'gender':
-                        this.genderFilter(state.gender, false);
+                        this.genderFilter(state, false);
                         break;
                     case 'age':
-                        this.ageFilter(state.age || 100);
+                        this.ageFilter(state || MAX_AGE);
                         break;
                 }
             }
         }
         for (let [filter, state] of Object.entries(this.sortingStates)) {
-            if (state.toggled) {
-                this.sortCards(state.direction, filter);
+            if (state) {
+                this.sortCards(state, filter);
             }
         }
     }
 
     resetFilterState(updateGender = false) {
-        this.filterStates.age = {
-            toggled: false,
-            age: null,
-        };
+        this.filterStates.age = null;
         if (updateGender) {
-            this.filterStates.gender = {
-                toggled: false,
-                gender: null,
-            }
+            this.filterStates.gender = null
         }
     }
 
     resetSortingState() {
         this.sortingStates = {
-            name: {
-                toggled: false,
-                direction: null,
-            },
-            registration: {
-                toggled: false,
-                direction: null
-            },
-            age: {
-                toggled: false,
-                direction: null,
-            }
+            name: null,
+            registration: null,
+            age: null
         };
     }
 
-    genderFilter(gender, wasToggled=true) {
+    genderFilter(gender, wasToggled = true) {
+        this.filterStates.gender = gender;
         if (wasToggled) {
-            this.filterStates.gender.gender = gender;
             this.restoreFromFilter();
         } else {
-            this.filterStates.gender.toggled = true;
-            this.filterStates.gender.gender = gender;
             this.cards = this.cards.filter(person => person.gender === gender);
         }
     }
 
     ageFilter(maxAge) {
-        this.filterStates.age.toggled = true;
         document.querySelector('.label-range').innerHTML = `Max age: ${maxAge}`;
-        if(this.filterStates.age.age < +maxAge) {
-            this.filterStates.age.age = +maxAge;
+        if (this.filterStates.age < +maxAge) {
+            this.filterStates.age = +maxAge;
             this.restoreFromFilter();
         } else {
-            this.filterStates.age.age = +maxAge;
-            this.cards = this.cards.filter(person => person.age <= + maxAge);
+            this.filterStates.age = +maxAge;
+            this.cards = this.cards.filter(person => person.age <= +maxAge);
         }
     }
 
     sortCards(value, filterName) {
+        this.resetSortingState();
         switch (filterName) {
             case 'age':
-                this.sortByAge(value);
+                this.cards.sort((person1, person2) => person1.compareAge(person2));
                 break;
             case 'name':
-                this.sortByName(value);
+                this.cards.sort((person1, person2) => person1.compareName(person2));
                 break;
             case 'registration':
-                this.sortByRegistrationDate(value)
+                this.cards.sort((person1, person2) => person1.compareDateRegistration(person2));
+        }
+        this.sortingStates[filterName] = value;
+        if (value === 'down') {
+            this.cards.reverse()
         }
     }
 
     nameFilter(name) {
         name = name.trim().toLowerCase();
         this.restoreFromFilter();
-        this.cards = this.cards.filter(person => person.name.first.toLowerCase().startsWith(name));
-    }
-
-    sortByAge(isFromLowest) {
-        this.resetSortingState();
-        this.cards.sort((person1, person2) => person1.ageCompare(person2));
-        this.sortingStates.age.toggled = true;
-        this.sortingStates.age.direction = isFromLowest;
-        if (isFromLowest === 'down') {
-            this.cards.reverse()
-        }
-    }
-
-    sortByName(isFromLowest) {
-        this.resetSortingState();
-        this.cards.sort((person1, person2) => person1.nameCompare(person2));
-        this.sortingStates.name.toggled = true;
-        this.sortingStates.name.direction = isFromLowest;
-        if (isFromLowest === 'down') {
-            this.cards.reverse()
-        }
-    }
-
-    sortByRegistrationDate(isFromLowest) {
-        this.resetSortingState();
-        this.cards.sort((person1, person2) => person1.dateRegistrationCompare(person2));
-        this.sortingStates.registration.toggled = true;
-        this.sortingStates.registration.direction = isFromLowest;
-        if (isFromLowest === 'down') {
-            this.cards.reverse()
-        }
+        this.cards = this.cards.filter(person => person.name.first.toLowerCase()
+            .startsWith(name) || person.name.last.toLowerCase().startsWith(name));
     }
 
     disableEnablePageToggler() {
-        const [backPage, forwPage] = document.querySelectorAll('.button-page');
-        if ((this.currentPage + 1) * CARDS_PER_PAGE >= this.cards.length)  {
-            forwPage.classList.add('disabled');
-            forwPage.setAttribute('disabled', true);
+        const [backPage, forwardPage] = document.querySelectorAll('.button-page');
+        if ((this.currentPage + 1) * CARDS_PER_PAGE >= this.cards.length) {
+            forwardPage.classList.add('disabled');
+            forwardPage.setAttribute('disabled', true);
         } else {
-            forwPage.removeAttribute('disabled');
-            forwPage.classList.remove('disabled');
+            forwardPage.removeAttribute('disabled');
+            forwardPage.classList.remove('disabled');
         }
         if (this.currentPage === 0) {
             backPage.classList.add('disabled');
@@ -241,8 +187,10 @@ class App {
         this.cards = cards.slice();
         this.resetFilterState(true);
         this.resetSortingState();
-        document.querySelector('.label-range').innerHTML = `Max age: 100`;
-        document.querySelector('.range').value = 100;
+        document.querySelector('.label-range').innerHTML = `Max age: ${MAX_AGE}`;
+        document.querySelector('.range').value = MAX_AGE;
+        document.querySelector('.search').value = '';
+        document.querySelectorAll('.radio').forEach(input => input.checked = false)
     }
 
     displayPeople() {
@@ -260,7 +208,7 @@ class App {
         this.disableEnablePageToggler();
     }
 
-    addListenersFilters() {
+    toggleFilters() {
         const app = this;
         filterSet.addEventListener('input', function (event) {
             const target = event.target.closest('input');
@@ -277,7 +225,7 @@ class App {
                     app.sortCards(value, target.dataset.name);
                     break;
                 case 'gender':
-                    const wasToggled = app.filterStates.gender.toggled;
+                    const wasToggled = !!app.filterStates.gender;
                     app.genderFilter(value, wasToggled);
                     break;
                 case 'range':
@@ -289,8 +237,8 @@ class App {
         });
 
         filterSet.addEventListener('click', function ({target}) {
-            target = target.closest('input.button-reset');
-            if (!target) {
+            const resetAppButton = target.closest('input.button-reset');
+            if (!resetAppButton) {
                 return
             }
             app.currentPage = 0;
@@ -299,16 +247,16 @@ class App {
         })
     }
 
-    addEventListenersPages() {
+    togglePages() {
         const app = this;
-        pages.addEventListener('click', function({target}) {
-            const t = target.closest('button.button-page');
-            if (!t) {
+        pages.addEventListener('click', function ({target}) {
+            const pageToggler = target.closest('button.button-page');
+            if (!pageToggler) {
                 return
             }
-            if (t.classList.contains('button-page__back')){
+            if (pageToggler.classList.contains('button-page__back')) {
                 app.currentPage = app.currentPage - 1;
-            } else if(t.classList.contains('button-page__forward')){
+            } else if (pageToggler.classList.contains('button-page__forward')) {
                 app.currentPage = app.currentPage + 1;
             }
             app.disableEnablePageToggler();
@@ -324,28 +272,26 @@ function startApp(peopleData) {
     app.cards = cards.slice();
     removeOverlay();
     app.displayPeople();
-    app.addListenersFilters();
-    app.addEventListenersPages();
+    app.toggleFilters();
+    app.togglePages();
 }
 
 function removeOverlay() {
-    const o = document.querySelector('.overlay');
-    o.classList.add('fade');
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('fade');
 }
 
 function createPeopleList(data) {
     data.forEach(function (person) {
         const {name, dob: {age}, gender, location: {country, city}, email, phone, cell, picture, registered} = person;
-        let newPerson = new Person(name, age, gender, country, city, email, phone, cell, picture, registered);
+        const newPerson = new Person(name, age, gender, country, city, email, phone, cell, picture, registered);
         newPerson.cardTemplate();
         cards.push(newPerson);
     });
 }
 
 fetch(URL_FETCH)
-    .then(response => {
-        return response.json();
-    })
+    .then(response => response.json())
     .catch(err => {
         document.querySelector('.smile').classList.add('hidden');
         const overlay = document.querySelector('.overlay');
