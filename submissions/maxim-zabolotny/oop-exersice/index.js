7830a6830f79eba88aa0361e80d85c53cafb9439
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-12-22 13:16:28 +0200

    used as a separator ; add an empty line at the end

--- submissions/maxim-zabolotny/oop-exersice/index.js
+++ submissions/maxim-zabolotny/oop-exersice/index.js
@@ -1,79 +1,79 @@
 class Creature {
-    constructor(name, gender, species, legs, saying) {
-      this.name = name;
-      this.gender = gender;
-      this.species = species;
-      this.legs = legs;
-      this.saying = saying;
-      this.friends = [];
-    }
-  
-    addFriends(...newFriends) {
-      newFriends.forEach(({ name }) => this.friends.push(name));
-      return this.friends;
-    }
-  
-    toString() {
-      let template = [
-        `name: ${this.name}; `,
-        `gender: ${this.gender}; `,
-        `species: ${this.species}; `,
-        `saying: ${this.saying}; `,
-        `legs: ${this.legs}; `,
-      ];
-  
-      if (this.friends.length > 0) {
-        template.push(`friends: ${this.friends}; `);
-      }
-      return template.join('');
-    }
+  constructor(name, gender, species, legs, saying) {
+    this.name = name;
+    this.gender = gender;
+    this.species = species;
+    this.legs = legs;
+    this.saying = saying;
+    this.friends = [];
   }
-  
-  class Dog extends Creature {
-    constructor(name, gender) {
-      super(name, gender, 'dog', 4, 'woof!');
-    }
+
+  addFriends(...newFriends) {
+    newFriends.forEach(({ name }) => this.friends.push(name));
+    return this.friends;
   }
-  
-  class Cat extends Creature {
-    constructor(name, gender) {
-      super(name, gender, 'cat', 4, 'mew-mew-mew-mew');
+
+  toString() {
+    let template = [
+      `name: ${this.name}`,
+      `gender: ${this.gender}`,
+      `species: ${this.species}`,
+      `saying: ${this.saying}`,
+      `legs: ${this.legs}`,
+    ]
+
+    if (this.friends.length > 0) {
+      template.push(`friends: ${this.friends}; `);
     }
+    return template.join('; ');
   }
-  
-  class Human extends Creature {
-    constructor(name, gender, saying) {
-      super(name, gender, 'homo sapiens', 2, saying);
-      this.hands = 2;
-    }
-  
-    toString() {
-      return [super.toString(), `hands: ${this.hands}`].join('');
-    }
+}
+
+class Dog extends Creature {
+  constructor(name, gender) {
+    super(name, gender, 'dog', 4, 'woof!');
   }
-  
-  class Woman extends Human {
-    constructor(name) {
-      super(name, 'female', 'I am a woman.');
-    }
+}
+
+class Cat extends Creature {
+  constructor(name, gender) {
+    super(name, gender, 'cat', 4, 'mew-mew-mew-mew');
   }
-  
-  class Man extends Human {
-    constructor(name) {
-      super(name, 'male', 'I am a man.');
-    }
+}
+
+class Human extends Creature {
+  constructor(name, gender, saying) {
+    super(name, gender, 'homo sapiens', 2, saying);
+    this.hands = 2;
   }
-  
-  const dog = new Dog('Alex', 'male');
-  const cat = new Cat('Dora', 'female');
-  const woman = new Woman('Anna');
-  const man = new Man('Elon');
-  man.addFriends(cat, woman);
-  woman.addFriends(man, dog);
-  dog.addFriends(cat, woman, man);
-  
-  const creatures = [dog, cat, woman, man];
-  
-  creatures.forEach((creature) => {
-    print(creature);
-  });
+
+  toString() {
+    return [super.toString(), `hands: ${this.hands}`].join('');
+  }
+}
+
+class Woman extends Human {
+  constructor(name) {
+    super(name, 'female', 'I am a woman.');
+  }
+}
+
+class Man extends Human {
+  constructor(name) {
+    super(name, 'male', 'I am a man.');
+  }
+}
+
+const dog = new Dog('Alex', 'male');
+const cat = new Cat('Dora', 'female');
+const woman = new Woman('Anna');
+const man = new Man('Elon');
+man.addFriends(cat, woman);
+woman.addFriends(man, dog);
+dog.addFriends(cat, woman, man);
+
+const creatures = [dog, cat, woman, man];
+
+creatures.forEach((creature) => {
+  print(creature);
+});
