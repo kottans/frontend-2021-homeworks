d241c9c1c4ad11f54df1a23f4a42b6c41c1f6365
Anastasiia <nastia.shrai@gmail.com>
2020-12-22 18:14:39 +0200

    feat: add frogger game

---
+++ submissions/Shrai-dev/frogger-game/app.js
@@ -0,0 +1,106 @@
+// Enemies our player must avoid
+const Enemy = function (x, y, speed) {
+  // Variables applied to each of our instances go here,
+  // we've provided one for you to get started
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = "images/enemy-bug.png";
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function (dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+  this.x += this.speed * dt;
+
+  if (this.x > 510) {
+    this.x = -50;
+    this.speed = Math.floor(Math.random() * 190) + 150;
+  }
+
+  if (
+    player.x < this.x + 80 &&
+    player.x + 80 > this.x &&
+    player.y < this.y + 60 &&
+    player.y + 60 > this.y
+  ) {
+    player.x = 202;
+    player.y = 404;
+  }
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Now write your own player class
+
+const Player = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.sprite = "images/char-boy.png";
+};
+
+// This class requires an update(), render() and
+// a handleInput() method.
+
+Player.prototype.update = function (dt) {
+  
+};
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function (keyPress) {
+    if (keyPress == 'left' && this.x > 0) {
+        this.x -= 102;
+    };
+    if (keyPress == 'right' && this.x < 404) {
+        this.x += 102;
+    };
+    if (keyPress == 'up' && this.y > 0) {
+        this.y -= 83;
+    };
+    if (keyPress == 'down' && this.y < 404) {
+        this.y += 83;
+    };
+    if (this.y < -10) {
+      setTimeout ( () => {
+          this.x = 202;
+          this.y = 404;
+      }, 800);
+  };
+};
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+
+const allEnemies = [];
+const enemyLocation = [63, 147, 230];
+enemyLocation.forEach(function (locationY) {
+  enemy = new Enemy(0, locationY, 150);
+  allEnemies.push(enemy);
+});
+// Place the player object in a variable called player
+
+const player = new Player(202, 404);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener("keyup", function (e) {
+  const allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
+});
