a022cb2fb99fbe57ebe8c12a8b0a5be29cba2d83
dmitry <akulinind@gmail.com>
2020-12-24 17:01:24 +0300

    refactor js

--- submissions/akulahere/DOM model/src/scripts/main.js
+++ submissions/akulahere/DOM model/src/scripts/main.js
@@ -4,12 +4,12 @@ const galleryList = document.querySelector('.gallery_list');
 const bigImage = document.querySelector('.gallery_large-img');
 const title = document.querySelector('figcaption');
 
-title.textContent = "Day and night"
+title.textContent = 'Day and night';
 
 galleryList.addEventListener('click', (event) => {
   event.preventDefault();
-  bigImage.src = event.target.closest('.list-item_link').href;
-  console.log(event.target.closest('.list-item_link').href);
-  console.log(event.target.closest('.list-item_link').title);
-  title.textContent = event.target.closest('.list-item_link').title;
+  if (event.target.classList.contains('gallery_img')) {
+    bigImage.src = event.target.closest('.list-item_link').href;
+    title.textContent = event.target.closest('.list-item_link').title;
+  }
 });
