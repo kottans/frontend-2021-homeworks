51c9cab693180ac091e9ab10abfb209821ed59ca
Anastasiia <nastia.shrai@gmail.com>
2020-12-24 20:16:07 +0200

    fix: fix after code review

--- submissions/Shrai-dev/oop-exercise/index.js
+++ submissions/Shrai-dev/oop-exercise/index.js
@@ -8,54 +8,90 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 class Inhabitant {
-   constructor (species, name, gender, age, saying, friends) {
-      this.species = species;
-      this.name = name;
-      this.gender = gender;
-      this.age = age;
-      this.saying = saying;
-      this.friends = friends;
-   }
-   toString() {
-      return `I am a ${this.species}. My name is ${this.name}. I am ${this.age} years old. My friends are ${this.friends.join(', ')}. I say: ${this.saying}`
-   }
-
+  constructor(species, name, gender, age, saying, friends) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.saying = saying;
+    this.friends = friends;
+  }
+  toString() {
+    return `I am a ${this.species}. My gender is ${this.gender}. My name is ${this.name}. I am ${this.age} years old. My friends are ${this.friends.join(", ")}. I say: ${this.saying}`;
+  }
 }
 
 class Animal extends Inhabitant {
    constructor(species, name, gender, age, paws, saying, friends) {
-      super(species, name, gender, age, saying, friends);
-      this.paws = paws;
+     super(species, name, gender, age, saying, friends);
+     this.paws = paws;
    }
    toString() {
-      return super.toString() + ` I have ${this.paws} paws.`
+     return super.toString() + ` I have ${this.paws} paws.`;
    }
+}
+
+class Cat extends Animal {
+  constructor(name, gender, age, friends) {
+    super("cat", name, gender, age, 4, "meow!", friends);
+  }
+  toString() {
+    return super.toString();
+  }
+}
+
+class Dog extends Animal {
+   constructor(name, gender, age, friends) {
+      super('dog', name, gender, age, 4, "bark-bark!", friends);
+   }
+toString() {
+   return super.toString();
+}
 }
 
 class Human extends Inhabitant {
-   constructor (species, name, gender, age, legs, hands, saying, friends) {
-      super(species, name, gender, age, saying, friends);
-      this.legs = legs;
-      this.hands = hands;
+  constructor(species, name, gender, age, hands, legs, saying, friends) {
+    super(species, name, gender, age, saying, friends);
+    this.legs = legs;
+    this.hands = hands;
+  }
+  toString() {
+    return (
+      super.toString() + ` I have ${this.legs} legs and ${this.hands} hands.`
+    );
+  }
+}
+
+class Woman extends Human {
+   constructor(name, age, saying, friends) {
+      super("human", name, "female", age, 2, 2, saying, friends);
+   }
+toString() {
+   return super.toString();
+}
+}
+
+class Man extends Human {
+   constructor(name, age, saying, friends) {
+     super("human", name, "male", age, 2, 2, saying, friends);
    }
    toString() {
-      return super.toString() + ` I have ${this.legs} legs and ${this.hands} hands.`
+     return super.toString();
    }
-
-}
+ }
 
 // ======== OUTPUT ========
 
-const dog = new Animal ('dog', 'Buddy', 'male', 3, 4, 'bark-bark!', ['Elsa', 'Chip', 'Max']);
-const cat = new Animal ('cat', 'Chloe', 'female', 2, 4, 'meow!', ['Leo', 'Simon', 'Kitty']);
-const man = new Human ('human', 'Brian', 'male', 26, 2, 2, 'Everyone thinks of changing the world, but no one thinks of changing himself!', ['Ethan', 'James', 'Ryan']);
-const woman = new Human ('human', 'Olivia', 'female', 24, 2, 2, 'The simplest way to be happy is to do good!', ['Maria', 'Kelly', 'Jennifer']);
+const dog = new Dog("Buddy", "male", 3, ["Elsa", "Chip", "Max"]);
+const cat = new Cat("Chloe", "female", 2, ["Leo", "Simon", "Kitty"]);
+const man = new Man ('Brian', 26, 'Everyone thinks of changing the world, but no one thinks of changing himself!', ['Ethan', 'James', 'Ryan']);
+const woman = new Woman ('Olivia', 24, 'The simplest way to be happy is to do good!', ['Maria', 'Kelly', 'Jennifer']);
 
-const inhabitants = [dog, cat, man, woman];
+const inhabitants = [dog, cat, woman, man];
 
-inhabitants.forEach(elem => {
-   print(elem, 'p');
-}) 
+inhabitants.forEach((elem) => {
+  print(elem, "p");
+});
 
 /* Use print(message) for output.
    Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
@@ -74,5 +110,3 @@ inhabitants.forEach(elem => {
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
    print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
    */
-
-
