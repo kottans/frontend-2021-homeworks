4991c0c8a116d5d116545cb9a2add1df54289bb0
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2020-12-25 10:56:32 +0200

    delete unnecessary files. implement the end of the game

--- submissions/maxim-zabolotny/pair-game/script.js
+++ submissions/maxim-zabolotny/pair-game/script.js
@@ -1,56 +1,56 @@
 const cardsArray = [{
-    'name': 'shell',
-    'img': 'img/blueshell.png',
-  },
-  {
-    'name': 'star',
-    'img': 'img/star.png',
-  },
-  {
-    'name': 'bobomb',
-    'img': 'img/bobomb.png',
-  },
-  {
-    'name': 'mario',
-    'img': 'img/mario.png',
-  },
-  {
-    'name': 'luigi',
-    'img': 'img/luigi.png',
-  },
-  {
-    'name': 'peach',
-    'img': 'img/peach.png',
-  },
-  {
-    'name': '1up',
-    'img': 'img/1up.png',
-  },
-  {
-    'name': 'mushroom',
-    'img': 'img/mushroom.png',
-  },
-  {
-    'name': 'thwomp',
-    'img': 'img/thwomp.png',
-  },
-  {
-    'name': 'bulletbill',
-    'img': 'img/bulletbill.png',
-  },
-  {
-    'name': 'coin',
-    'img': 'img/coin.png',
-  },
-  {
-    'name': 'goomba',
-    'img': 'img/goomba.png',
-  },
+  'name': 'shell',
+  'img': 'img/blueshell.png',
+},
+{
+  'name': 'star',
+  'img': 'img/star.png',
+},
+{
+  'name': 'bobomb',
+  'img': 'img/bobomb.png',
+},
+{
+  'name': 'mario',
+  'img': 'img/mario.png',
+},
+{
+  'name': 'luigi',
+  'img': 'img/luigi.png',
+},
+{
+  'name': 'peach',
+  'img': 'img/peach.png',
+},
+{
+  'name': '1up',
+  'img': 'img/1up.png',
+},
+{
+  'name': 'mushroom',
+  'img': 'img/mushroom.png',
+},
+{
+  'name': 'thwomp',
+  'img': 'img/thwomp.png',
+},
+{
+  'name': 'bulletbill',
+  'img': 'img/bulletbill.png',
+},
+{
+  'name': 'coin',
+  'img': 'img/coin.png',
+},
+{
+  'name': 'goomba',
+  'img': 'img/goomba.png',
+},
 ];
 
 const gameGrid = cardsArray
-  .concat(cardsArray)
-  .sort(() => 0.5 - Math.random());
+.concat(cardsArray)
+.sort(() => 0.5 - Math.random());
 
 let firstGuess = '';
 let secondGuess = '';
@@ -64,75 +64,87 @@ grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
 gameGrid.forEach(item => {
-  const { name, img } = item;
+const { name, img } = item;
 
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.dataset.name = name;
+const card = document.createElement('div');
+card.classList.add('card');
+card.dataset.name = name;
 
-  const front = document.createElement('div');
-  front.classList.add('front');
+const front = document.createElement('div');
+front.classList.add('front');
 
-  const back = document.createElement('div');
-  back.classList.add('back');
-  back.style.backgroundImage = `url(${img})`;
+const back = document.createElement('div');
+back.classList.add('back');
+back.style.backgroundImage = `url(${img})`;
 
-  grid.appendChild(card);
-  card.appendChild(front);
-  card.appendChild(back);
+grid.appendChild(card);
+card.appendChild(front);
+card.appendChild(back);
 });
 
 const match = () => {
-  const selected = document.querySelectorAll('.selected');
-  selected.forEach(card => {
-    card.classList.add('match');
-  });
+const selected = document.querySelectorAll('.selected');
+selected.forEach(card => {
+  card.classList.add('match');
+});
 };
 
 const resetGuesses = () => {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-  previousTarget = null;
+firstGuess = '';
+secondGuess = '';
+count = 0;
+previousTarget = null;
 
-  var selected = document.querySelectorAll('.selected');
-  selected.forEach(card => {
-    card.classList.remove('selected');
-  });
+var selected = document.querySelectorAll('.selected');
+selected.forEach(card => {
+  card.classList.remove('selected');
+});
+if(isFinish().every(el => el == 1)) {
+  alert('You Win!!!')
+  location.reload()
+}
 };
 
 grid.addEventListener('click', event => {
 
-  const clicked = event.target;
+const clicked = event.target;
+if (
+  clicked.nodeName === 'SECTION' ||
+  clicked === previousTarget ||
+  clicked.parentNode.classList.contains('selected') ||
+  clicked.parentNode.classList.contains('match')
+) {
+  return;
+}
 
-  if (
-    clicked.nodeName === 'SECTION' ||
-    clicked === previousTarget ||
-    clicked.parentNode.classList.contains('selected') ||
-    clicked.parentNode.classList.contains('match')
-  ) {
-    return;
+if (count < 2) {
+  count++;
+  if (count === 1) {
+    firstGuess = clicked.parentNode.dataset.name;
+    console.log(firstGuess);
+    clicked.parentNode.classList.add('selected');
+  } else {
+    secondGuess = clicked.parentNode.dataset.name;
+    console.log(secondGuess);
+    clicked.parentNode.classList.add('selected');
   }
 
-  if (count < 2) {
-    count++;
-    if (count === 1) {
-      firstGuess = clicked.parentNode.dataset.name;
-      console.log(firstGuess);
-      clicked.parentNode.classList.add('selected');
-    } else {
-      secondGuess = clicked.parentNode.dataset.name;
-      console.log(secondGuess);
-      clicked.parentNode.classList.add('selected');
+  if (firstGuess && secondGuess) {
+    if (firstGuess === secondGuess) {
+      setTimeout(match, delay);
     }
-
-    if (firstGuess && secondGuess) {
-      if (firstGuess === secondGuess) {
-        setTimeout(match, delay);
-      }
-      setTimeout(resetGuesses, delay);
-    }
-    previousTarget = clicked;
+    setTimeout(resetGuesses, delay);
   }
+  previousTarget = clicked;
+}
 
 });
+
+
+function isFinish() {
+let val = []
+let cards = document.querySelectorAll('.card')
+cards.forEach(el => val = [...val, el.classList.contains('match')])
+return val
+}
+console.log(isFinish().every(el => el == 1))
