d4e4aef4d3fab932fd292ab4649f6776c6c27aee
Lu-sure <66886680+Lu-sure@users.noreply.github.com>
2020-12-28 17:50:09 +0200

    Debug according kottans' comments

--- submissions/Lu-sure/frogger/app.js
+++ submissions/Lu-sure/frogger/app.js
@@ -1,101 +1,132 @@
-const FIELD_WIDTH = 101;
-const FIELD_HEIGHT = 83;
-const SHIFT = 18;
+const BLOCK_WIDTH = 101;
+const BLOCK_HEIGHT = 83;
 
-var Enemy = function (y, speed) {
-    this.sprite = 'images/enemy-bug.png';
-    this.x = -FIELD_WIDTH;
+const FIELD_WIDTH = 5 * BLOCK_WIDTH;
+const FIELD_HEIGHT = 6 * BLOCK_HEIGHT;
+
+const HEIGHT_SHIFT = 18;
+
+const MOVE_AREA = {width: {start: 0 , end: FIELD_WIDTH - BLOCK_WIDTH}, height: {start: FIELD_HEIGHT - BLOCK_HEIGHT - HEIGHT_SHIFT, end: 0}};
+
+const ENEMY_CONFIG = {
+    sprite: 'images/enemy-bug.png',
+    startPointX: - BLOCK_WIDTH,
+    linesToGo: {first: BLOCK_HEIGHT - HEIGHT_SHIFT, second: 2* BLOCK_HEIGHT - HEIGHT_SHIFT, third: 3* BLOCK_HEIGHT - HEIGHT_SHIFT},
+    minSpeed: 100,
+    maxSpeed: 450
+};
+
+const PLAYER_CONFIG = {
+    sprite: 'images/char-boy.png',
+    startPos: {x: 2 * BLOCK_WIDTH, y: MOVE_AREA.height.start},
+    winPositionY: BLOCK_HEIGHT-HEIGHT_SHIFT,
+    thickness: 60
+};
+
+const DELAY_OF_COLLISION = 50;
+const DELAY_OF_WIN = 200;
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+const Hero = function(sprite) {
+    this.sprite = sprite;
+};
+
+Hero.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+const Enemy = function (y, speed, sprite) {
+    Hero.call(this, sprite);
+    this.x = ENEMY_CONFIG.startPointX;
     this.y = y;
     this.speed = speed;
 };
 
+Enemy.prototype = Object.create(Hero.prototype);
+Enemy.prototype.constructor = Enemy;
+
 Enemy.prototype.update = function (dt) {
     this.x += this.speed * dt;
-    if (this.x > 5 * FIELD_WIDTH) {
-        this.x = -FIELD_WIDTH;
-    };
-    collision();
+    if (this.x > MOVE_AREA.width.end)
+        this.x = ENEMY_CONFIG.startPointX;
+    checkCollision();
 };
 
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-var Player = function () {
-    this.sprite = 'images/char-boy.png';
+const Player = function (sprite) {
+    Hero.call(this, sprite);
     this.goToStart();
     this.move = true;
 };
 
-Player.prototype.update = function () { };
+Player.prototype = Object.create(Hero.prototype);
+Player.prototype.constructor = Player;
 
-Player.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+Player.prototype.update = function () { };
 
 Player.prototype.handleInput = function (direction) {
     switch (direction) {
         case 'left':
-            if (this.x > 0) {
-                this.x -= FIELD_WIDTH;
+            if (this.x > MOVE_AREA.width.start) {
+                this.x -= BLOCK_WIDTH;
             }
-            collision();
             break;
         case 'right':
-            if (this.x < 4 * FIELD_WIDTH) {
-                this.x += FIELD_WIDTH;
+            if (this.x < MOVE_AREA.width.end) {
+                this.x += BLOCK_WIDTH;
             }
-            collision();
             break;
         case 'up':
-            if (this.y > 0) {
-                this.y -= FIELD_HEIGHT;
+            if (this.y > MOVE_AREA.height.end) {
+                this.y -= BLOCK_HEIGHT;
             }
-            collision();
-            if (this.y < FIELD_HEIGHT - SHIFT) {
+            if (this.y < PLAYER_CONFIG.winPositionY) {
                 this.move = false;
-                setTimeout(() => this.goToStart(), 400);
+                setTimeout(() => this.goToStart(), DELAY_OF_WIN);
             }
             break;
         case 'down':
-            if (this.y < 5 * FIELD_HEIGHT - SHIFT) {
-                this.y += FIELD_HEIGHT;
+            if (this.y < MOVE_AREA.height.start) {
+                this.y += BLOCK_HEIGHT;
             }
-            collision();
             break;
     }
+    checkCollision();
 };
 
 Player.prototype.goToStart = function () {
-    this.x = 2 * FIELD_WIDTH;
-    this.y = 5 * FIELD_HEIGHT - SHIFT;
+    this.x = PLAYER_CONFIG.startPos.x;
+    this.y = PLAYER_CONFIG.startPos.y;
     this.move = true;
 };
 
-var allEnemies = [
-    new Enemy(FIELD_HEIGHT - SHIFT, 200),
-    new Enemy(2 * FIELD_HEIGHT - SHIFT, 240),
-    new Enemy(3 * FIELD_HEIGHT - SHIFT, 180),
+const allEnemies = [
+    new Enemy(ENEMY_CONFIG.linesToGo.first, getRandomInteger(ENEMY_CONFIG.minSpeed, ENEMY_CONFIG.maxSpeed), ENEMY_CONFIG.sprite),
+    new Enemy(ENEMY_CONFIG.linesToGo.second, getRandomInteger(ENEMY_CONFIG.minSpeed, ENEMY_CONFIG.maxSpeed), ENEMY_CONFIG.sprite),
+    new Enemy(ENEMY_CONFIG.linesToGo.third, getRandomInteger(ENEMY_CONFIG.minSpeed, ENEMY_CONFIG.maxSpeed), ENEMY_CONFIG.sprite),
 ];
-var player = new Player();
 
-var collision = function () {
+const player = new Player(PLAYER_CONFIG.sprite);
+
+const checkCollision = function () {
     for (let i = 0; i < allEnemies.length; i++) {
-        if (Math.abs(player.x - allEnemies[i].x) < FIELD_WIDTH - 18 && player.y == allEnemies[i].y) {
+        if (Math.abs(player.x - allEnemies[i].x) < PLAYER_CONFIG.thickness && player.y === allEnemies[i].y) {
             player.move = false;
-            setTimeout(() => player.goToStart(), 200);
+            setTimeout(() => player.goToStart(), DELAY_OF_COLLISION);
         };
     };
 };
 
 document.addEventListener('keyup', function (e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-    if (player.move == false) {
+    if (player.move === false) {
         return;
     }
     player.handleInput(allowedKeys[e.keyCode]);
