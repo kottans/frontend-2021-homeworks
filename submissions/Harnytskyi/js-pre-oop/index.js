97af5a56f46282907d89d11673f0fb268403c097
Harnytskyi <andrij.garnytskiy@gmail.com>
2020-12-28 14:29:56 +0200

    updated a tiny js world

--- submissions/Harnytskyi/js-pre-oop/index.js
+++ submissions/Harnytskyi/js-pre-oop/index.js
@@ -6,67 +6,64 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-class Inhabitant {
-    constructor(species, name, gender, legs, hands, saying) {
-        this.species = species;
-        this.name = name;
-        this.gender = gender;
-        this.legs = legs;
-        this.hands = hands;
-        this.saying = saying;
-    };
-    toString() {
-        const infoMessage = [
-            this.saying,
-            'I am a',
-            this.species + '.',
-            'My name is',
-            this.name + '.',
-            'My gender is',
-            this.gender + '.',
-            `I have`,
-            this.legs,
-            this.definitionExtremities()
-        ];
-        return infoMessage.join(' ');
-    }
-    definitionExtremities() {
-        if (this.species == 'human')
-            return 'legs and ' + this.hands + ' hands.'
-        else
-            return 'paws.'
-    }
-}
-class Human extends Inhabitant {
-    constructor(name, gender, saying) {
-        super('human', name, gender, 2, 2, saying);
-    }
-}
-class Man extends Human {
-    constructor(name, saying) {
-        super(name, 'male', saying);
-    }
-}
-class Woman extends Human {
-    constructor(name, saying) {
-        super(name, 'female', saying);
-    }
-}
-class Dog extends Inhabitant {
-    constructor(name, gender) {
-        super('dog', name, gender, 4, 0, 'Gav');
-    }
-}
-class Cat extends Inhabitant {
-    constructor(name, gender) {
-        super('cat', name, gender, 4, 0, 'Meow');
-    }
+const man = {
+    species: 'human',
+    name: 'John',
+    gender: 'male',
+    legs: 2,
+    hands: 2,
+    saying: 'Hello, everybody!'
+};
+
+const woman = {
+    species: 'human',
+    name: 'Jessica',
+    gender: 'female',
+    legs: 2,
+    hands: 2,
+    saying: 'Good morning, gays!'
 }
 
-const man = new Man('John', 'Hello, everybody!');
-const woman = new Woman('Jessica', 'Good morning, gays!');
-const cat = new Cat('Tom', 'male');
-const dog = new Dog('Jerry', 'female');
+const cat = {
+    species: 'cat',
+    name: 'Tom',
+    gender: 'male',
+    legs: 4,
+    saying: 'Meow!'
+};
+
+const dog = {
+    species: 'dog',
+    name: 'Jerry',
+    gender: 'female',
+    legs: 4,
+    saying: 'Gav!'
+};
+
+const inhabitants = [man, woman, cat, dog];
+
+function message(obj) {
+    const infoMessage = [
+        obj.saying,
+        'I am a',
+        obj.species + '.',
+        'My name is',
+        obj.name + '.',
+        'My gender is',
+        obj.gender + '.',
+        `I have`,
+
+        definitionExtremities(obj)
+    ];
+    return infoMessage.join(' ');
+};
+
+function definitionExtremities(obj) {
+    if (obj.species == 'human')
+        return obj.legs + ' legs and ' + obj.hands + ' hands.';
+    else
+        return obj.legs + ' paws.';
+};
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -77,6 +74,4 @@ const dog = new Dog('Jerry', 'female');
    so code reviewers might focus on a single file that is index.js.
    */
 
-const inhabitants = [man, woman, cat, dog];
-inhabitants.forEach(key => print(key));
-
+inhabitants.forEach(key => print(message(key)));
