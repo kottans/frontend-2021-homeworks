7755797e8acce1b2febc0e8daf9d6f972b47cbf9
Roophee <alex.babaew@gmail.com>
2020-12-29 05:36:37 +0200

    refactor code, fixed one card close, fixed typos

--- submissions/Roophee/pair-game/index.js
+++ submissions/Roophee/pair-game/index.js
@@ -1,25 +1,26 @@
 "use strict";
 
-const heousArray = ['luke', 'vader', 'solo', 'ob1', 'palpatine', 'leia', 'r2d2-c3po', 'chewbacca', 'bad1', 'bad2', 'bad3', 'bad4', 'good1', 'good3', 'good4', 'robot1', 'yoda'],
+const heroesArray = ['luke', 'vader', 'solo', 'ob1', 'palpatine', 'leia', 'r2d2-c3po', 'chewbacca', 'bad1', 'bad2', 'bad3', 'bad4', 'good1', 'good3', 'good4', 'robot1', 'yoda'],
     indexInUse = [],
     imagesCountForTwo = {},
     prePair = [],
     pairs = [],
     gameBg = document.querySelector('.game'),
     cardPlace = document.querySelector('.game__card-place'),
-    audioItem = document.querySelector("audio")
+    audioItem = document.querySelector('audio'),
+    audioButton = document.querySelector('.audio__button'),
+    quantityOfUniqueCards = 17,
+    quantityOfPairsOnTheScreen = 6;
+
 
 // TAKE RANDOM CARDS FROM POOL OF CARDS
-
 const cardsForPlay = function () {
     while (true) {
-        let randomIndex = Math.floor(Math.random() * 17);
-        if (indexInUse.length === 6) {
+        let randomIndex = Math.floor(Math.random() * quantityOfUniqueCards);
+        if (indexInUse.length === quantityOfPairsOnTheScreen) {
             break
-        } else {
-            if (indexInUse.indexOf(randomIndex) < 0) {
-                indexInUse.push(randomIndex);
-            }
+        } else if (indexInUse.indexOf(randomIndex) < 0) {
+            indexInUse.push(randomIndex);
         }
     }
 }
@@ -27,7 +28,6 @@ const cardsForPlay = function () {
 cardsForPlay();
 
 //COUNT HOW MANY SAME IMAGES ADD IN GAME
-
 const imageCounter = function (indx) {
     if (imagesCountForTwo.hasOwnProperty(indx) && imagesCountForTwo[indx] >= 2) {
         return -2;
@@ -40,131 +40,131 @@ const imageCounter = function (indx) {
     }
 }
 
-// TAKE RANDOM IMAGE FROM POR OF IMAGES FOR THIS GAME
-
+// TAKE RANDOM IMAGE FROM POOL OF IMAGES FOR THIS GAME
 const getRandomImage = function () {
     while (true) {
-        let randomIndex = Math.floor(Math.random() * 6);
+        let randomIndex = Math.floor(Math.random() * quantityOfPairsOnTheScreen);
         if (imageCounter(randomIndex) >= -1) {
             return randomIndex;
         }
     }
 }
 
-//MAKE ANIMATION ON PAGE DOWNLOAD
-
-gameBg.style.top = `${-gameBg.clientHeight - gameBg.offsetTop}px`;
-document.addEventListener('DOMContentLoaded', function (e) {
-    setTimeout(function () {
+    //MAKE ANIMATION ON PAGE DOWNLOAD
+const animateOnStart = function(){
+    gameBg.style.top = `${-gameBg.clientHeight - gameBg.offsetTop}px`;
+    cardPlace.style.bottom = `${-document.documentElement.clientHeight * 2}px`;
+    setTimeout(function (){
         gameBg.style.top = '0px';
-    }, 500)
-})
-
-cardPlace.style.bottom = `${-document.documentElement.clientHeight * 2}px`;
-document.addEventListener('DOMContentLoaded', function (e) {
-    setTimeout(function () {
         cardPlace.style.bottom = '0px';
-    }, 500)
-})
+    },500)
+}
+
+document.addEventListener('DOMContentLoaded', animateOnStart);
+
 
 // ADD CARDS ON DOCUMENT
-
 let addCardsOnPage = function () {
+    let cardsContainerOnDynamicGeneration = document.createDocumentFragment();
     for (let i = 0; i < 12; i++) {
-        let cardItem = document.createElement('div');
+        let cardNameInsertOnDynamicGeneration = heroesArray[indexInUse[getRandomImage()]],
+            idForCardOnCheckIsTheSameOrNot = i+1,
+            cardItem = document.createElement('div');
         cardItem.classList.add('game__card-holder');
-        cardItem.value = i
-        cardItem.innerHTML = `<div class="game__card" >
-                                <div class=" cards__front">
-                                    <img src="img/${heousArray[indexInUse[getRandomImage()]]}.jpg" alt="" value="${i}" class="card__front">
-                                </div>
-                                <div class="cards__back">
-                                    <img src="img/cardback.jpg" alt="cardback" class="card__back">
-                                </div>
-                            </div>`;
-        cardPlace.appendChild(cardItem);
+        cardItem.setAttribute('data-id', idForCardOnCheckIsTheSameOrNot.toString());
+        cardItem.innerHTML = `<div class="game__card-item">
+                            <div class="game__card-front">
+                                <img src="img/${cardNameInsertOnDynamicGeneration}.jpg" alt="${cardNameInsertOnDynamicGeneration}"  class="game__card-img">
+                            </div>
+                            <div class="game__card-back">
+                                <img src="img/cardback.jpg" alt="cardback" class="game__card-cardback">
+                            </div>
+                        </div>`;
+        cardsContainerOnDynamicGeneration.appendChild(cardItem);
     }
+    cardPlace.appendChild(cardsContainerOnDynamicGeneration);
 }
 
 addCardsOnPage();
 
-// CHEK PLAYER COLLECTED ALL PAIRS OR NOT
-
-const gameIsOver = function () {
-    setTimeout(function () {
-        if (pairs.length === 6) {
-            let playAgain = window.confirm("Congratulations. You have matched all pairs and won the game. Want to play again?");
-            if (playAgain) {
-                document.querySelector('.game__card-place').innerHTML="";
-                for (const prop of Object.keys(imagesCountForTwo)) {
-                    delete imagesCountForTwo[prop];
-                };
-                indexInUse.length = 0;
-                prePair.length = 0;
-                pairs.length = 0;
-                cardsForPlay();
-                addCardsOnPage();
+let clearAllOperationObjectsAndCreateNewGame = function() {
+    if (pairs.length === quantityOfPairsOnTheScreen) {
+        let playAgain = window.confirm('Congratulations. You have matched all pairs and won the game. Want to play again?');
+                if (playAgain) {
+                    document.querySelector('.game__card-place').innerHTML = "";
+                    for (const prop of Object.keys(imagesCountForTwo)) {
+                        delete imagesCountForTwo[prop];
+                        };
+                    indexInUse.length = 0;
+                    prePair.length = 0;
+                    pairs.length = 0;
+                    cardsForPlay();
+                    addCardsOnPage();
+                }
             }
-        }
+}
+
+// CHECK PLAYER COLLECTED ALL PAIRS OR NOT
+const checkGameIsOverOrNot = function () {
+    setTimeout(function () {
+            clearAllOperationObjectsAndCreateNewGame();
     }, 2000)
 }
 
+let hidePair = function(){
+    prePair[0][1].classList.add('hide');
+    prePair[1][1].classList.add('hide');
+    prePair.splice(0, 2);
+}
+
+let clearPair = function() {
+    prePair[0][1].classList.remove('active');
+    prePair[1][1].classList.remove('active');
+    prePair.splice(0, 2);
+}
+
 // CHECK IS TWO CARDS PAIR OR NOT
-let pairOrNot = function () {
-    if (prePair.length === 2 && prePair[0][0] == prePair[1][0] && prePair[0][2] !== prePair[1][2]) {
+let checkPairOrNot = function () {
+    if (prePair.length === 2 && prePair[0][0] === prePair[1][0] && prePair[0][2] !== prePair[1][2]) {
         pairs.push('1');
         setTimeout(function () {
-            prePair[0][1].classList.add('hide');
-            prePair[1][1].classList.add('hide');
-            prePair.splice(0, 2);
-        }, 800);
-        gameIsOver();
-
-    } else if (prePair.length === 2 && prePair[0][0] == prePair[1][0] && prePair[0][2] == prePair[1][2]) {
-        setTimeout(function () {
-            prePair[0][1].classList.remove('active');
-            prePair[1][1].classList.remove('active');
-            prePair.splice(0, 2);
-        }, 800);
+            hidePair();
+        }, 500);
+        checkGameIsOverOrNot();
 
     } else if (prePair.length === 2 && prePair[0][0] !== prePair[1][0]) {
         setTimeout(function () {
-            prePair[0][1].classList.remove('active');
-            prePair[1][1].classList.remove('active');
-            prePair.splice(0, 2);
+            clearPair();
         }, 800);
+}}
 
-    } else if (prePair.length < 2) {
-    }
+let createCardInfoAndAddItInCheckPlace = (cardBack, cardHolderElement) => {
+    let cardNameHolder = cardBack.closest('.game__card-holder').querySelector('.game__card-img').src.split('/');
+    let cardName = cardNameHolder[cardNameHolder.length - 1].split(".")[0];
+    prePair.push([cardName, cardHolderElement, cardHolderElement.getAttribute('data-id')]);
 }
 
 //  CLICK ON CARD EVENT
-
-document.addEventListener('click', function (e) {
-    if (e.target.parentElement.parentElement.className === "game__card") {
-        let parent = e.target.parentElement.parentElement.parentElement;
-        if(prePair.length<2) {
-        parent.classList.add("active");
-        let cardNameHolder = e.target.parentElement.parentElement.parentElement.querySelector(".card__front").src.split("/");
-        let cardName = cardNameHolder[cardNameHolder.length - 1].split(".")[0];
-        prePair.push([cardName, parent, parent.value]);
-        pairOrNot();
+cardPlace.addEventListener('click', function (e) {
+    let cardBack = e.target;
+    if (cardBack.classList.contains('game__card-cardback')) {
+        let cardHolderElement = cardBack.closest('.game__card-holder');
+        if (prePair.length < 2) {
+            cardHolderElement.classList.add('active');
+            createCardInfoAndAddItInCheckPlace(cardBack,cardHolderElement);
+        checkPairOrNot();
         }
     }
 })
 
-
 // MUTE AUDIO
-
-document.addEventListener('click', function(e) {
-    if(e.target.classList.contains("audio__play")) {
+audioButton.addEventListener('click', function (e) {
         audioItem.volume = 0.4;
-        if(!audioItem.paused){
-            e.target.classList.remove('audio__pause');
+        if (!audioItem.paused) {
+            e.target.classList.remove('pause');
             audioItem.pause();
         } else {
-            e.target.classList.add('audio__pause');
+            e.target.classList.add('pause');
             audioItem.play();
         }
-    }
 })
