8716ab09fd6a9a2d3009d0004185887f3e135350
dmitry <akulinind@gmail.com>
2020-12-29 13:55:28 +0300

    implement post oop

--- submissions/akulahere/a-tiny-js-world/index.js
+++ submissions/akulahere/a-tiny-js-world/index.js
@@ -1,39 +1,65 @@
-const dog = {
-  species: 'dog',
-  name: 'Toby',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'woof-woof!'
-};
-const cat = {
-  species: 'cat',
-  name: 'Sheldon',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'mrrr!'
-};
-const man = {
-  species: 'human',
-  name: 'Dmitry',
-  gender: 'male',
-  legs: 2,
-  hands: 2,
-  saying: 'Hello!'
-};
-const woman = {
-  species: 'human',
-  name: 'Lero',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: 'Bye!'
-};
+class Inhabitant {
+  constructor(species, name, gender, legs, hands, saying) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.legs = legs;
+    this.hands = hands;
+    this.saying = saying;
+    this.friends = Array.isArray(this.friends) ? this.friends : 'No friends' ;
+  }
 
-const inhabitantsArray = [dog, cat, man, woman];
-const properties = ['species', 'name', 'gender', 'legs', 'hands', 'saying']
+  makeFriend(friend) {
+    this.friends = this.friends === 'No friends' ? [] : this.friends;
+    this.friends.push(friend.name);
+  }
+
+  makeMessage() {
+    const properties = [];
+    for (const property of Object.keys(this)) {
+      properties.push(property.toString());
+    }
+    return properties.map(property => `${capitalizeString(property)}: ${this[property]}`).join(' ')
+  }
+}
+
+class Dog extends Inhabitant {
+  constructor(name, gender) {
+    super('dog', name, gender, 4, 0, 'woof-woof!');
+  }
+}
+
+class Cat extends Inhabitant {
+  constructor(name, gender) {
+    super('cat', name, gender, 4, 0, 'mrrr!');
+  }
+}
+
+class CatWoman extends Cat {
+  constructor(name) {
+    super(name, 'female');
+    this.species = 'CatWoman';
+    this.legs = 2;
+    this.hands = 2;
+  }
+}
+
+class Human extends Inhabitant {
+  constructor(name, gender, saying) {
+    super('human', name, gender, 2, 2, saying);
+  }
+}
+
+const man = new Human('Dmitry', 'male', 'Hello!');
+const woman = new Human('Lero', 'female', 'Bye!');
+const dog = new Dog('Toby', 'male');
+const cat = new Cat('Sheldon', 'male');
+const catWoman = new CatWoman('Mary');
+dog.makeFriend(cat);
+dog.makeFriend(man);
+man.makeFriend(cat);
+const inhabitantsArray = [dog, cat, man, woman, catWoman];
+
 const capitalizeString = (string) => string.charAt(0).toUpperCase() + string.slice(1);
 
-const makeMessage = (inhabitant) => properties.map(property => `${capitalizeString(property)}: ${inhabitant[property]}.`).join(' ');
-inhabitantsArray.forEach(inhabitant => print(makeMessage(inhabitant)));
+inhabitantsArray.forEach(inhabitant => print(inhabitant.makeMessage()));
