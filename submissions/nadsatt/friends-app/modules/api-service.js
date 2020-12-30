3251c940a8ee1a480e6425d967b8dc4fc06411ee
Yuliia <yabybka@gmail.com>
2020-12-30 15:45:33 +0200

    fixed uninformative property name

--- submissions/nadsatt/friends-app/modules/api-service.js
+++ submissions/nadsatt/friends-app/modules/api-service.js
@@ -7,12 +7,12 @@ export class ApiService {
         this.page = 1;
         this.manResults = 40;
         this.womanResults = 60;
-        this.inc = ['gender', 'name', 'email', 'dob', 'location', 'registered'];
+        this.propertiesToFetch = ['gender', 'name', 'email', 'dob', 'location', 'registered'];
     }
 
     async getUsers(retries = 3){
-        let malesResponse = await fetch(`${this.endpoint}?gender=male&page=${this.page}&results=${this.manResults}&inc=${this.inc.join(',')}`);
-        let femalesResponse = await fetch(`${this.endpoint}?gender=female&page=${this.page}&results=${this.womanResults}&inc=${this.inc.join(',')}`);
+        let malesResponse = await fetch(`${this.endpoint}?gender=male&page=${this.page}&results=${this.manResults}&inc=${this.propertiesToFetch.join(',')}`);
+        let femalesResponse = await fetch(`${this.endpoint}?gender=female&page=${this.page}&results=${this.womanResults}&inc=${this.propertiesToFetch.join(',')}`);
 
         if(malesResponse.status === 200 && femalesResponse.status === 200){
             const males = await malesResponse.json();
