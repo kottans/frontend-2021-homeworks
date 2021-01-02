4bb72e0115a98c19827ea050bc342081708b682a
Kasio <kasio87@ukr.net>
2021-01-02 23:32:18 +0200

    Make some fixes after review

--- submissions/kasionio/oop-exercise/index.js
+++ submissions/kasionio/oop-exercise/index.js
@@ -8,56 +8,56 @@
 // ======== OBJECTS DEFINITIONS ========
 // Define your objects here
 class Inhabitant {
-  constructor(species, name, gender, legs, hands, saying) {
-  this.species = species,
-  this.name = name,
-  this.gender = gender,
-  this.legs = legs,
-  this.hands = hands,
-  this.saying = saying
+  constructor(species, name, gender, legs, saying) {
+      this.species = species;
+      this.name = name;
+      this.gender = gender;
+      this.legs = legs;
+      this.saying = saying;
   }
 }
 
 class Human extends Inhabitant {
-  constructor(name, gender, saying) {
-    super('human', name, gender, 2, 2, saying);
-    }
+  constructor(name, gender, saying, hands) {
+      super('human', name, gender, 2, saying);
+      this.hands = 2;
+  }
 }
 
 class Man extends Human {
   constructor(name, saying) {
-    super(name, 'male', saying);
-    }
+      super(name, 'male', saying);
+  }
 }
 
 class Woman extends Human {
   constructor(name, saying) {
-    super(name, 'female', saying);
-    }
+      super(name, 'female', saying);
+  }
 }
 
 class Animal extends Inhabitant {
   constructor(species, name, gender, saying) {
-    super(species, name, gender, 4, 0, saying);
-    }
+      super(species, name, gender, 4, saying);
+  }
 }
 
 class Dog extends Animal {
   constructor(name, gender) {
-    super('dog', name, gender, 'woof-woof');
-    }
+      super('dog', name, gender, 'woof-woof');
+  }
 }
 
 class Cat extends Animal {
   constructor(name, gender) {
-    super('cat', name, gender, 'meow');
-    }
+      super('cat', name, gender, 'meow');
+  }
 }
 
 class CatWoman extends Inhabitant {
   constructor(name) {
-    super('catWoman', name, 'female', 2, 2, cat.saying);
-    }
+      super('catWoman', name, 'female', 2, cat.saying);
+  }
 }
 
 let man = new Man('Arnold', 'Hasta la vista, baby!');
@@ -66,5 +66,11 @@ let cat = new Cat('Bobby', 'male');
 let dog = new Dog('Toby', 'male');
 let catWoman = new CatWoman('Betty');
 
-const inhabitants = [man, woman, catWoman, cat, dog]; 
-inhabitants.forEach(item => print(Object.values(item)));
+const inhabitants = [man, woman, catWoman, cat, dog];
+const properties = ['species', 'name', 'gender', 'legs', 'hands', 'saying'];
+
+const message = (inhabitants) => inhabitants.map(inhabitant => properties.map(prop => inhabitant[prop] ? inhabitant[prop] : 'undefined')
+  .join('; '))
+  .join('\n');
+
+print(message(inhabitants));
