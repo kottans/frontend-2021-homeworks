87d451397558e3b9f641310e74524708b3d3d827
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2021-01-04 00:30:52 +0200

    stats(script): Move general purpose functions to utils

--- stats/make-stats.js
+++ stats/make-stats.js
@@ -4,13 +4,8 @@
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 const writeFile = promisify(require('fs').writeFile);
-const {
-  prLabels,
-  prStates,
-  parsingRegex,
-  url,
-  statsFileName,
-} = require('./config');
+const { prLabels, prStates, parsingRegex, url, statsFileName } = require('./config');
+const { mergeObjects, recombine } = require('./utils');
 
 exec("gh --version")
   .catch(e => {
@@ -195,26 +190,4 @@ function makeComparator(primaryKeyNumericDescending, secondaryKeyAlphaAscendingC
     }
     return bPk - aPk;
   }
-}
-
-function mergeObjects(primaryObject, secondaryObject) {
-  const mergedObject = {...primaryObject};
-  Object.keys(secondaryObject)
-    .forEach(key => {
-      mergedObject[key] = {
-        ...secondaryObject[key],
-        ...primaryObject[key],
-      };
-    });
-  return mergedObject;
-}
-
-/**
- * Recombines elements from input arrays into an array of tuples
- * @param {Array} arr1
- * @param {Array} arr2
- * @returns {[*,*][]}
- */
-function recombine(arr1, arr2) {
-  return arr1.reduce((acc, e1) => [...acc, ...arr2.map(e2 => [e1, e2])], []);
 }
