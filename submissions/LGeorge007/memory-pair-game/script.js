59f52f6dd7ce3af99fd3db84a52a504f0ff4c970
George <losevgeorge007@gmail.com>
2021-01-03 17:26:05 +0200

    no array filled by hand, boolean intead 0&1, using ID, using {target}, no inner function

--- submissions/LGeorge007/memory-pair-game/script.js
+++ submissions/LGeorge007/memory-pair-game/script.js
@@ -1,65 +1,104 @@
-const allCards = document.querySelectorAll(".flipper");
-const arrOfCards = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-let unopenedCards = arrOfCards.length;
+let arrOfUnits = ["img/cartman.jpg",
+    "img/chief.jpg",
+    "img/sten.jpg",
+    "img/kyle.jpg",
+    "img/kenny.jpg"
+];
+let imgCover = `img/cover.jpg`;
+let gameField = document.querySelector("#gameField");
+let isLocked = false;
 let firstCard;
 let secondCard;
-let clikCounter = 0;
+let isSecondCard = false;
+let unopenedCards;
 
-const checkCard = function (event) {
-    const checkMatch = function () {
-        if (clikCounter == 0) {
-            firstCard = event.path[1];
-            clikCounter++;
-        } else if (clikCounter == 1) {
-            secondCard = event.path[1];
-            //The same card must not be removed
-            if (firstCard.style.order != secondCard.style.order) {
-                if (firstCard.dataset.hero == secondCard.dataset.hero) {
-                    firstCard.children[1].classList.add("hiddenSide");
-                    secondCard.children[1].classList.add("hiddenSide");
-                    firstCard.classList.add("hiddenFlipper");
-                    secondCard.classList.add("hiddenFlipper");
-                    unopenedCards -= 2;
-                } else {
-                    firstCard.classList.remove("rotateCard");
-                    secondCard.classList.remove("rotateCard");
-                };
-                clikCounter--;
-                if (unopenedCards == 0) {
-                    setTimeout(() => {
-                        alert("You win!");
-                        window.location.reload();
-                    }, 1500);
-                };
-            };
-        };
-    };
-    event.path[1].classList.toggle("rotateCard");
-    setTimeout(checkMatch, 600);
-}
+//feed him array & any even number
+let duplicateElements = function(array, times) {
+    return array.reduce((res, current) => {
+        return res.concat(Array(times).fill(current));
+    }, []);
+};
 
-const shuffle = function (arr) {
+let shuffle = function(arr) {
     const result = [];
-    const getRandomInt = function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
     while (arr.length > 0) {
-        let random = getRandomInt(0, arr.length - 1);
+        let random = Math.floor(Math.random() * arr.length);
         let elem = arr.splice(random, 1)[0];
         result.push(elem);
     }
     return result;
+};
+
+const checkCard = function({ target }) {
+    if (!isLocked) {
+        if (isSecondCard) {
+            isLocked = true;
+            secondCard = target.closest("div");
+            secondCard.classList.toggle("rotateCard");
+            //The same card must not be removed
+            if (firstCard.id !== secondCard.id) {
+                if (firstCard.dataset.hero === secondCard.dataset.hero) {
+                    unopenedCards -= 2;
+                    setTimeout(() => {
+                        firstCard.children[1].classList.add("hiddenSide");
+                        secondCard.children[1].classList.add("hiddenSide");
+                        firstCard.classList.add("hiddenFlipper");
+                        secondCard.classList.add("hiddenFlipper");
+                    }, 600);
+                } else {
+                    setTimeout(() => {
+                        firstCard.classList.remove("rotateCard");
+                        secondCard.classList.remove("rotateCard");
+                    }, 600);
+                };
+            };
+            setTimeout(() => {
+                isLocked = false;
+                isSecondCard = false;
+            }, 1000);
+            if (unopenedCards === 0) {
+                setTimeout(() => {
+                    alert("You win!");
+                    window.location.reload();
+                }, 1500);
+            };
+        } else {
+            firstCard = target.closest("div");
+            firstCard.classList.toggle("rotateCard");
+            isSecondCard = true;
+        };
+    };
+};
+
+let renderCard = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let card = document.createElement("div");
+        card.setAttribute("id", i);
+        card.classList.add("flipper");
+        card.setAttribute("data-hero", arr[i].slice(4, -4));
+        card.setAttribute("draggable", false);
+        card.addEventListener("mousedown", checkCard);
+
+        let frontSide = document.createElement("img");
+        frontSide.setAttribute("src", imgCover);
+        frontSide.setAttribute("draggable", false);
+        frontSide.classList.add("front");
+
+        let backSide = document.createElement("img");
+        backSide.setAttribute("src", arr[i]);
+        backSide.setAttribute("draggable", false);
+        backSide.classList.add("back");
+
+        card.appendChild(frontSide);
+        card.appendChild(backSide);
+        gameField.appendChild(card);
+    };
+};
+
+const init = function() {
+    arrOfUnits = shuffle(duplicateElements(arrOfUnits, 4));
+    unopenedCards = arrOfUnits.length;
+    renderCard(arrOfUnits);
 }
 
-const setOrder = function (order) {
-    for (let i = 0; i < allCards.length; i++) {
-        allCards[i].style.order = order[i];
-    }
-}
-
-const onStart = function () {
-    setOrder(shuffle(arrOfCards));
-    allCards.forEach(card => card.addEventListener('click', checkCard));
-}
-
-document.addEventListener("DOMContentLoaded", onStart);
+document.addEventListener("DOMContentLoaded", init);
