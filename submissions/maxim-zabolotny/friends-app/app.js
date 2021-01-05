e6257920a7ab6744a820e1eff369a88fa326dfcb
Maxim Zabolotny <maxim.zabolotny20121776@gmail.com>
2021-01-05 16:29:26 +0200

    fix the mentor's comments

--- submissions/maxim-zabolotny/friends-app/app.js
+++ submissions/maxim-zabolotny/friends-app/app.js
@@ -16,38 +16,38 @@ const DOM_NODES = {
     btnWrapper: document.querySelector('.btn-wrapper'),
     resetBtn: document.querySelector('.filter .reset_button'),
 }
-
 const state = {
-    allUsers : [],
+    allUsers: [],
     filteredUsers: [],
     sortedUsers: []
 }
-
-window.state = state
 
 const SORT = {
     all: () => renderCards(state.sortedUsers = [...state.filteredUsers]),
     female: () => renderCards(state.sortedUsers = state.filteredUsers.filter(user => user.gender.toLowerCase() == 'female')),
     male: () => renderCards(state.sortedUsers = state.filteredUsers.filter(user => user.gender.toLowerCase() == 'male')),
     nameAsc: () => renderCards(state.sortedUsers = state.filteredUsers.sort((a, b) => {
-        if(a.name.first.toLowerCase() > b.name.first.toLowerCase()) return 1
+        if (a.name.first.toLowerCase() > b.name.first.toLowerCase()) return 1
         else return -1
     })),
     nameDesc: () => renderCards(state.sortedUsers = state.filteredUsers.sort((a, b) => {
-        if(a.name.first.toLowerCase() < b.name.first.toLowerCase()) return 1
+        if (a.name.first.toLowerCase() < b.name.first.toLowerCase()) return 1
         else return -1
     })),
     ageAsc: () => renderCards(state.sortedUsers = state.filteredUsers.sort((a, b) => {
-        if(a.age > b.age) return 1
+        if (a.age > b.age) return 1
         else return -1
     })),
     ageDesc: () => renderCards(state.sortedUsers = state.filteredUsers.sort((a, b) => {
-        if(a.age < b.age) return 1
+        if (a.age < b.age) return 1
         else return -1
     })),
 }
 
-const capitalized = ({first, last}) => {
+const capitalized = ({
+    first,
+    last
+}) => {
     let str = `${first} ${last}`
     return str.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
 }
@@ -74,59 +74,74 @@ class Card {
     render() {
         let div = document.createElement('div')
         div.classList.add('card')
-        div.id=this.id
-        const markup = `<div class="details">
-                            <p class="user_title">${capitalized(this.name)}</p>
-                            <div class="user_photo horizontal_center">
-                                <img src="${this.photo}" alt="${this.name}">
-                            </div>
-                            <p class="user_value">${this.email}</p>
-                        </div>
-                        <ul class="values_list horizontal_center">
-                            <li data-title="Hi, My name is" data-value="${capitalized(this.name)}" data-label="name" class=""></li>
-                            <li data-title="My email address is" data-value="${this.email}" data-label="email" class="active"></li>
-                            <li data-title="My birthday is" data-value="${formatDate(this.date)}" data-label="birthday" class=""></li>
-                            <li data-title="My address is" data-value="${this.location}" data-label="location" class=""></li>
-                            <li data-title="My phone number is" data-value="${this.cell}" data-label="phone" class=""></li>
-                            <li data-title="My password is" data-value="${this.password}" data-label="pass" class=""></li>
-                        </ul>`;
+        div.id = this.id
+        const markup = generateMarkupForCard(
+            this.name,
+            this.photo,
+            this.email,
+            this.date,
+            this.location,
+            this.cell,
+            this.password
+            )
         div.innerHTML = markup
+
         const list = Array.from(div.getElementsByTagName("li"));
-        list.forEach((e) => {
-            e.addEventListener("mouseover", function() {
-                list.forEach(item => item.classList.remove('active'));
-                this.classList.add('active');
-                div.querySelector('.user_title').innerHTML = this.getAttribute('data-title');
-                div.querySelector('.user_value').innerHTML = this.getAttribute('data-value');
-            })
-        });
+        div.addEventListener('mouseover', function (e) {
+            let li = e.target.closest('li')
+            if (!li) return
+            list.forEach(item => item.classList.remove('active'));
+            li.classList.add('active');
+            this.querySelector('.details').querySelector('.user_title').innerHTML = li.getAttribute('data-title');
+            this.querySelector('.details').querySelector('.user_value').innerHTML = li.getAttribute('data-value');
+        })
         return div
     }
 }
 
+const generateMarkupForCard = (name, photo, email, date, location, cell, password) => {
+    return `<div class="details">
+    <p class="user_title">${capitalized(name)}</p>
+    <div class="user_photo horizontal_center">
+        <img src="${photo}" alt="${name}">
+    </div>
+    <p class="user_value">${email}</p>
+</div>
+<ul class="values_list horizontal_center">
+    <li data-title="Hi, My name is" data-value="${capitalized(name)}" data-label="name" class=""></li>
+    <li data-title="My email address is" data-value="${email}" data-label="email" class="active"></li>
+    <li data-title="My birthday is" data-value="${formatDate(date)}" data-label="birthday" class=""></li>
+    <li data-title="My address is" data-value="${location}" data-label="location" class=""></li>
+    <li data-title="My phone number is" data-value="${cell}" data-label="phone" class=""></li>
+    <li data-title="My password is" data-value="${password}" data-label="pass" class=""></li>
+</ul>`;
+}
+
 const buildCardList = () => {
     fetch(APP_CONFIG.url)
-    .then(data => data.json())
-    .then(data => state.allUsers = data.results.map(buildCard))
-    .then(() => state.sortedUsers = [...state.allUsers])
-    .then(() => state.filteredUsers = [...state.allUsers])
-    .then(() => renderCards(state.filteredUsers))
-    .catch (e => {console.log(e)})
-
-    function buildCard (user) {
-        return new Card({
-            id : user.id,
-            photo : user.picture.large,
-            name : user.name,
-            age : user.age,
-            date : user.dob.date,
-            email : user.email,
-            location : user.location.city,
-            password : user.login.password,
-            gender : user.gender,
-            cell : user.cell
+        .then(data => data.json())
+        .then(data => state.allUsers = data.results.map(buildCard))
+        .then(() => state.sortedUsers = [...state.allUsers])
+        .then(() => state.filteredUsers = [...state.allUsers])
+        .then(() => renderCards(state.filteredUsers))
+        .catch(e => {
+            console.log(e)
         })
-    }
+}
+
+function buildCard(user) {
+    return new Card({
+        id: user.id,
+        photo: user.picture.large,
+        name: user.name,
+        age: user.age,
+        date: user.dob.date,
+        email: user.email,
+        location: user.location.city,
+        password: user.login.password,
+        gender: user.gender,
+        cell: user.cell
+    })
 }
 
 const createBtn = (page, type) => `<button class="btn-inline results__btn--${ type }" data-goto=${type === 'prev' ? page - 1 : page + 1}>
@@ -136,33 +151,33 @@ const createBtn = (page, type) => `<button class="btn-inline results__btn--${ ty
                                         </svg>
                                     </button>`;
 const renderBtn = (page, sumResults, resPerPage) => {
-    const pages = Math.ceil(sumResults/resPerPage);
+    const pages = Math.ceil(sumResults / resPerPage);
     let btn;
-    if(page === 1 && pages > 1){
+    if (page === 1 && pages > 1) {
         btn = createBtn(page, 'next');
-    }else if (page === pages && pages > 1) {
+    } else if (page === pages && pages > 1) {
         btn = createBtn(page, 'prev');
-    }else if (page < pages){
+    } else if (page < pages) {
         btn = `${createBtn(page, 'next')} ${createBtn(page, 'prev')}`;
-    }else{
+    } else {
         btn = null;
     }
-    if( sumResults > 0 ){
-        if(DOM_NODES.btnWrapper) {
-            if(btn) DOM_NODES.btnWrapper.insertAdjacentHTML('beforeend', btn);
-        } else{
+    if (sumResults > 0) {
+        if (DOM_NODES.btnWrapper) {
+            if (btn) DOM_NODES.btnWrapper.insertAdjacentHTML('beforeend', btn);
+        } else {
             const btnWrapper = document.createElement('DIV');
             btnWrapper.className = 'btn-wrapper';
-            if(btn) btnWrapper.insertAdjacentHTML('beforeend', btn);
+            if (btn) btnWrapper.insertAdjacentHTML('beforeend', btn);
             DOM_NODES.target.appendChild(btnWrapper);
         }
     }
 };
 
 const renderCards = (cardsArray, currentPage = 1, resPerPage = 12) => {
-    const start = (currentPage - 1)*resPerPage;
-    const end = currentPage*resPerPage;
-    if(cardsArray && cardsArray.length !== 0){
+    const start = (currentPage - 1) * resPerPage;
+    const end = currentPage * resPerPage;
+    if (cardsArray && cardsArray.length !== 0) {
         const fragment = document.createDocumentFragment();
         cardsArray.slice(start, end).forEach(item => fragment.appendChild(item.render()));
         DOM_NODES.target.innerHTML = '';
@@ -172,7 +187,9 @@ const renderCards = (cardsArray, currentPage = 1, resPerPage = 12) => {
 };
 
 const filter = (value) => {
-    renderCards(state.filteredUsers = state.allUsers.filter(({name}) => `${name.first} ${name.last}`.toLowerCase().includes(value.toLowerCase())))
+    renderCards(state.filteredUsers = state.allUsers.filter(({
+        name
+    }) => `${name.first} ${name.last}`.toLowerCase().includes(value.toLowerCase())))
 };
 
 const reset = () => {
@@ -187,17 +204,16 @@ const resetRadioButtons = () => {
 
 const sortingCards = (e) => {
     const target = e.target.type;
-    if(!state.sortedUsers.length) state.sortedUsers = [...state.filteredUsers];
-    
+    if (!state.sortedUsers.length) state.sortedUsers = [...state.filteredUsers];
+
     if (target == 'radio') SORT[e.target.value]();
 };
 
 DOM_NODES.inputText.addEventListener("change", function (e) {
     filter(e.target.value)
 });
-// DOM_NODES.inputText.addEventListener("focus", resetRadioButtons);
 
-DOM_NODES.form.addEventListener('click', function(e) {
+DOM_NODES.form.addEventListener('click', function (e) {
     let filteredUsers = [...state.filteredUsers];
     sortingCards(e, filteredUsers);
 });
@@ -206,7 +222,7 @@ DOM_NODES.resetBtn.addEventListener('click', reset);
 
 DOM_NODES.target.addEventListener('click', e => {
     const btn = e.target.closest(`.btn-inline`);
-    if(btn){
+    if (btn) {
         const goToPage = parseInt(btn.dataset.goto);
         renderCards(state.sortedUsers, goToPage);
     }
