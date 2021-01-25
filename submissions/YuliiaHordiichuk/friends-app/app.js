f92f2a2104d046f622688f6a8d18031bf780942f
YuliiaHordiichuk <gordiychuk98@gmail.com>
2021-01-25 22:16:33 +0200

    Changes after review

--- submissions/YuliiaHordiichuk/friends-app/app.js
+++ submissions/YuliiaHordiichuk/friends-app/app.js
@@ -1,33 +1,44 @@
-const randomUsersUrl = 'https://randomuser.me/api/?results=36&inc=gender,name,email,phone,picture,dob';  
-
 let users; 
 let searchBy = ''; 
 let sortBy = ''; 
 let filterBy = 'all'; 
  
-function getData() {
+function fetchUsers() {
+    const randomUsersUrl = 'https://randomuser.me/api/?results=36&inc=gender,name,email,phone,picture,dob'; 
+
     fetch(randomUsersUrl)
-        .then(answer => answer.json())
-        .then(( { results } ) => {
-            users = results;
-            createCards(results); 
-        })
-        .catch(() => {
+        .then(handleErrors)
+        .then((response) => response.json())
+        .then(({ results }) => handleFetchedUsers(results))
+        .catch((errorText) => {
+            console.warn(errorText);
             const textContainer = document.querySelector('.cards'); 
-            textContainer.innerHTML = "Uh oh, something has gone wrong. Reload the page"; 
-        })
+            textContainer.innerHTML = 'Uh oh, something has gone wrong. Reload the page'; 
+        });
 }; 
 
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
 function initApp() {
-    getData(); 
+    fetchUsers();
     bindEventListeners(); 
 }
 
+function handleFetchedUsers(fetchedUsers) {
+    users = fetchedUsers;
+    createCards(fetchedUsers); 
+}
+
 function bindEventListeners () {
-    const form = document.querySelector('.form');
+    const filteringAndSortingForm = document.querySelector('.form');
     const resetButton = document.querySelector('.form__button');
 
-    form.addEventListener('input', ({ target: { name, value } }) => {
+    filteringAndSortingForm.addEventListener('input', ({ target: { name, value } }) => {
         if (name === 'sortBy') {
             sortBy = value; 
         } else if (name === 'searchBy') {
@@ -60,67 +71,49 @@ function getChosenUsers() {
     return newUsers; 
 }
 
-function createCards(dataArray) {
-    const cardsContainer = document.querySelector('.cards__items'); 
-
-    cardsContainer.innerHTML = dataArray.map(( { picture: {large}, name: { first, last }, dob: {age}, email, phone } ) => {
-        return `
-            <li class="cards__item card">
-                <img class="card__img" alt="user image" src="${large}">
-                <h2 class="card__title">${first} ${last}</h2>
-                <p class="card__detail">${age} years old </p>
-                <h3 class="card__subtitle">Contact details</h3>
-                <a class="card__detail card__detail_link" href="mailto:${email}">${email}</a>
-                <a class="card__detail card__detail_link" href="tel:${phone}">${phone}</a>
-            </li>
-        `; 
-    }).join(''); 
+function createCards(chosenUsers) {
+    const cardsContainer = document.querySelector(".cards__items");
+  
+    cardsContainer.innerHTML = chosenUsers
+        .map(
+            ({
+            picture: { large },
+            name: { first, last },
+            dob: { age },
+            email,
+            phone,
+            }) => {
+            return `
+                <li class="cards__item card">
+                    <img class="card__img" alt="user image" src="${large}">
+                    <h2 class="card__title">${first} ${last}</h2>
+                    <p class="card__detail">${age} years old </p>
+                    <h3 class="card__subtitle">Contact details</h3>
+                    <a class="card__detail card__detail_link" href="mailto:${email}">${email}</a>
+                    <a class="card__detail card__detail_link" href="tel:${phone}">${phone}</a>
+                </li>
+            `;
+            }
+        )
+        .join("");
 }
 
-function sortByAge(dataArray) {
-    if (sortBy === '0-9') {
-        sortByOrder(dataArray, 'dob', 'age', 'ascending')
-    } else if (sortBy === '9-0') {
-        sortByOrder(dataArray, 'dob', 'age', 'descending')
-    }
-
-    return dataArray;
+function sortByAge(users) {
+    users.sort((a,b) => a.dob.age - b.dob.age); 
+    return sortBy === '0-9' ? users : users.reverse(); 
 }
 
-function sortByName(dataArray) {
-    if (sortBy === 'a-z') {
-        sortByOrder (dataArray, 'name', 'first', 'ascending')
-    } else if (sortBy === 'z-a') {
-        sortByOrder (dataArray, 'name', 'first', 'descending')
-    }
-
-    return dataArray; 
+function sortByName(users) {
+    users.sort((a, b) => a.name.first.localeCompare(b.name.first))
+    return sortBy === 'a-z' ? users : users.reverse(); 
 }
 
-function sortByOrder(dataArray, keyToCompare, valueToCompare, sortType) {
-    dataArray.sort((a,b) => {
-        if (a[keyToCompare][valueToCompare] < b[keyToCompare][valueToCompare]) {
-            return -1; 
-        }
-        if (a[keyToCompare][valueToCompare] > b[keyToCompare][valueToCompare]) {
-            return 1; 
-        }
-        return 0; 
-    })
-
-    if (sortType === 'ascending') {
-        return dataArray; 
-    } else if (sortType === 'descending') {
-        return dataArray.reverse(); 
-    }
+function filterByGender(users) {
+    return users.filter(el => el.gender === filterBy)
 }
 
-function filterByGender(dataArray) {
-    return dataArray.filter(el => el.gender === filterBy)
-}
-
-function filterBySearch(dataArray) {
-    return dataArray.filter(
+function filterBySearch(users) {
+    return users.filter(
       ({ name:  { first, last } }) =>
         first.toLowerCase().includes(searchBy.toLowerCase()) ||
         last.toLowerCase().includes(searchBy.toLowerCase())
