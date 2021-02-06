20c00045be5bd2c850992b08d3c4c051f2affa79
Evgen241984 <evgeniy241984@gmail.com>
2021-02-06 14:33:25 +0200

    wip replaced with template string

--- submissions/evgeniy24/friends_app/app.js
+++ submissions/evgeniy24/friends_app/app.js
@@ -5,8 +5,8 @@ const REQUEST_URL  = 'https://randomuser.me/api/?results=20',
     SEARCH_FIELD = document.querySelector('#search'),
     SORT_BY_NAME_AZ = document.querySelector('.nameSortAZ'),
     SORT_BY_NAME_ZA = document.querySelector('.nameSortZA'),
-    SORT_BY_AGE_YO = document.querySelector('.ageSortYoungerFirst'),
-    SORT_BY_AGE_OY = document.querySelector('.ageSortOlderFirst'),
+    SORT_YOUNGER_FIRST = document.querySelector('.ageSortYoungerFirst'),
+    SORT_OLDER_FIRST = document.querySelector('.ageSortOlderFirst'),
     FILTER_MENU = document.querySelector('.filter-gender'),
     FILTER_BY_MALE = document.querySelector('.filter-gender__male'),
     FILTER_BY_FEMALE = document.querySelector('.filter-gender__female'),
@@ -56,48 +56,47 @@ function renderFriendsList(friends) {
 }
 
 function renderFriendsCard(friend) {
-    const cardWrap = document.createElement('div');
-    const mainInfo = document.createElement('div');
-    const friendsName = document.createElement('a');
-    const moreWrap = document.createElement('div');
-    const friendPhotoWrap = document.createElement('div')
-    const friendPhoto = document.createElement('img');
-    const friendAge = document.createElement('p');
-    const friendEmail = document.createElement('a');
-    const friendPhone = document.createElement('a');
-    const friendCity = document.createElement('a');
-
-    cardWrap.classList.add('cardWrap');
-    (friend.gender === 'male') ? cardWrap.classList.add('malegradient') : cardWrap.classList.add('femalegradient') 
-    mainInfo.classList.add('mainInfo');
-    moreWrap.classList.add('moreWrap');
-    friendPhotoWrap.classList.add('cardWrap__photo-wrap');
-    friendPhoto.classList.add('cardWrap__photo-img');
-    friendsName.classList.add('mainInfo__friendsName');
-    friendAge.classList.add('mainInfo__age');
-    friendEmail.classList.add('moreWrap__email');
-    friendPhone.classList.add('moreWrap__phone');
-    friendCity.classList.add('mainInfo__city');
-    
-    friendsName.textContent = `${friend.name.first} ${friend.name.last}`;
-    friendAge.textContent = `Age: ${friend.dob.age}`;
-    friendEmail.textContent = `email: ${friend.email}`;
-    friendPhone.textContent = `phone: ${friend.phone}`;
-    friendCity.textContent = `City: ${friend.location.city}`;
-
-    friendPhoto.setAttribute('src', friend.picture.medium);
-    friendsName.setAttribute('href', `https://www.facebook.com/search/top?q=${friend.name.first}%20${friend.name.last}`);
-    friendsName.setAttribute('target', '_blank');
-    friendEmail.setAttribute('href', `mailto:${friend.email}`);
-    friendPhone.setAttribute('href', `tel:>${friend.phone}`);
-    friendCity.setAttribute('href', `https://www.google.com/maps/place/${friend.location.city}$`);
-    friendCity.setAttribute('target', '_blank');
-
-    moreWrap.append(friendPhone, friendEmail);
-    mainInfo.append(friendsName, friendAge, friendCity);
-    friendPhotoWrap.append(friendPhoto);
-    cardWrap.append(friendPhotoWrap, mainInfo, moreWrap);
-    FRIENDS_LIST_WRAP.append(cardWrap);
+    const FRIEND_CARD = document.createElement('div');
+    FRIEND_CARD.innerHTML = 
+    `
+    <div class="cardWrap ${friend.gender === 'male' ? 'malegradient' : 'femalegradient'}">
+        <div class="cardWrap__photo-wrap">
+            <img
+                class="cardWrap__photo-img"
+                src= "${friend.picture.medium}"
+                alt="${friend.name.first} ${friend.name.last}"
+            />
+        </div>
+        <div class="mainInfo">
+            <a
+                class="mainInfo__friendsName"
+                href="https://www.facebook.com/search/top?q=${friend.name.first}%20${friend.name.last}"
+                target="_blank">
+                ${friend.name.first} ${friend.name.last}
+            </a>
+            <p class="mainInfo__age">
+                Age: ${friend.dob.age}
+            </p>
+            <a
+                class="mainInfo__city"
+                href="https://www.google.com/maps/place/${friend.location.city}$"
+                target="_blank">
+                    City: ${friend.location.city}
+            </a>
+        </div>
+        <div class="moreWrap">
+            <a class="moreWrap__phone" 
+                href="tel:>${friend.phone}">
+                phone: ${friend.phone}
+            </a>
+            <a class="moreWrap__email" 
+                href="mailto:${friend.email}">
+                email: ${friend.email}
+            </a>
+        </div>
+    </div>
+    `
+    FRIENDS_LIST_WRAP.append(FRIEND_CARD);
 }
 
 SEARCH_FIELD.addEventListener('input', function() {
@@ -108,6 +107,7 @@ SEARCH_FIELD.addEventListener('input', function() {
         })
         return renderFriendsList(searchResultList);
     }  else {
+        searchResultList = [...friendsList]
         renderFriendsList(friendsList);
     }
 })
@@ -128,22 +128,22 @@ SORTING_MENU.addEventListener('click', function(event) {
     }
 
     const sortByAge = function(a, b) {
-        return (a.dob.age - b.dob.age);
+        return a.dob.age - b.dob.age;
     }
     
     if (event.target === SORT_BY_NAME_AZ) {
-        sortedList.sort((a, b) => sortByName(a, b));
+        sortedList.sort(sortByName);
         renderFriendsList(sortedList);
 
     } else if (event.target === SORT_BY_NAME_ZA) {
         sortedList.sort((a, b) => sortByName(b, a));
         renderFriendsList(sortedList);
 
-    } else if (event.target === SORT_BY_AGE_YO) {
-        sortedList.sort((a, b) => sortByAge(a, b));
+    } else if (event.target === SORT_YOUNGER_FIRST) {
+        sortedList.sort(sortByAge);
         renderFriendsList(sortedList);
 
-    } else if (event.target === SORT_BY_AGE_OY) {
+    } else if (event.target === SORT_OLDER_FIRST) {
         sortedList.sort((a, b) => sortByAge(b, a));
         renderFriendsList(sortedList);
     } 
@@ -169,7 +169,7 @@ FILTER_MENU.addEventListener('click', function(event) {
         renderFriendsList(filteredByGenderList);
 
     } else if (event.target === FILTER_BY_ALL) {
-        filteredByGenderList = [...friendsList];
+        filteredByGenderList = genderNeutralList;
         renderFriendsList(filteredByGenderList);
         }
         
