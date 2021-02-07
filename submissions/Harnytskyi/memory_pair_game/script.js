876581884faf51c9e6141d6fac623ed042d3bfd1
Harnytskyi <andrij.garnytskiy@gmail.com>
2021-02-07 22:54:05 +0200

    changes after review

--- submissions/Harnytskyi/memory_pair_game/script.js
+++ submissions/Harnytskyi/memory_pair_game/script.js
@@ -1,51 +1,40 @@
-const CARDS = ["Kyiv", "Kharkiv", "Lviv", "Odesa", "Dnipro", "Poltava", "Zhytomyr", "Khmelnytskyi", "Luhansk", "Rivne", "Cherkasy", "Mykolaiv", "Simferopol", "Zaporizhzhia"];
+const CITIES = ["Kyiv", "Kharkiv", "Lviv", "Odesa", "Dnipro", "Poltava", "Zhytomyr", "Khmelnytskyi", "Luhansk", "Rivne", "Cherkasy", "Mykolaiv", "Simferopol", "Zaporizhzhia"];
 const GRID = document.getElementById("allCards");
-const FINISH_MATCH_CARDS = 8;
+const quantityCardPairs = 8;
 const DELAY = 800;
-let matchCards = 0;
+let matchedCards = 0;
 let firstCard;
 
-function choseCards(cardArray) {
-    let randomCards = cardArray.sort(() => 0.5 - Math.random());
-    randomCards = randomCards.slice(0, 8)
+function chooseCardsRandomly(cards) {
+    let randomCards = cards.sort(() => 0.5 - Math.random());
+    randomCards = randomCards.slice(0, quantityCardPairs)
     randomCards = randomCards.concat(randomCards);
     randomCards = randomCards.sort(() => 0.5 - Math.random())
     return randomCards;
 }
 
-function displayCard() {
-    const ALL_CARDS = document.createDocumentFragment();
+function displayCards() {
     GRID.innerHTML = "";
-    let CHOSEN_CARDS = choseCards(CARDS);
+    let CHOSEN_CARDS = chooseCardsRandomly(CITIES);
     CHOSEN_CARDS.forEach(item => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.dataset.label = item;
-        const flipper = document.createElement('div');
-        flipper.classList.add('card-flipper');
-        const cardFront = document.createElement('div')
-        cardFront.classList.add('card-front');
-        flipper.append(cardFront);
-        const cardBack = document.createElement('div');
-        cardBack.classList.add('card-back');
-        const cardImg = document.createElement('img');
-        cardImg.src = `svg/${item}.svg`;
-        cardImg.classList.add('cardImg');
-        cardBack.append(cardImg);
-        flipper.append(cardBack);
-        card.append(flipper);
-        ALL_CARDS.append(card);
+        GRID.innerHTML +=   `<div class="card" data-label="${item}">
+                                <div class="card-flipper">
+                                    <div class="card-front"></div>
+                                    <div class="card-back">  
+                                        <img src="svg/${item}.svg" class="cardImg">
+                                    </div>
+                                </div>
+                            </div>`;
     });
-    GRID.append(ALL_CARDS);
 }
 
-function hideCard(card1, card2) {
+function hideCards(card1, card2) {
     setTimeout(() => {
         card1.classList.add('hidden');
         card2.classList.add('hidden');
     }, DELAY);
 }
-function backFlipCard(card1, card2) {
+function backFlipCards(card1, card2) {
     setTimeout(() => {
         card1.classList.remove('flipped');
         card2.classList.remove('flipped');
@@ -57,27 +46,27 @@ function checkCouple({ target }) {
         TARGET_CARD.classList.add('flipped');
         if (!firstCard)
             firstCard = TARGET_CARD;
-        else if (firstCard.dataset.label == TARGET_CARD.dataset.label) {
-            hideCard(firstCard, TARGET_CARD);
+        else if (firstCard.dataset.label === TARGET_CARD.dataset.label) {
+            hideCards(firstCard, TARGET_CARD);
             firstCard = null;
-            matchCards++;
+            matchedCards++;
             checkFinishGame();
         }
         else {
-            backFlipCard(firstCard, TARGET_CARD);
+            backFlipCards(firstCard, TARGET_CARD);
             firstCard = null;
         }
     }
 }
 function checkFinishGame() {
-    if (matchCards == FINISH_MATCH_CARDS) {
+    if (matchedCards == quantityCardPairs) {
         setTimeout(() => {
             alert('Finish!');
-            matchCards = 0;
-            displayCard();
+            matchedCards = 0;
+            displayCards();
         }, DELAY + 10);
     }
 }
 
-displayCard();
+displayCards();
 GRID.addEventListener('click', checkCouple);
