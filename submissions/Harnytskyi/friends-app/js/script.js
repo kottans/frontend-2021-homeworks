09bb44da8db95ba565283494aaedd307a4d3f371
Harnytskyi <andrij.garnytskiy@gmail.com>
2021-02-09 23:42:54 +0200

    reworked sorting, simplified filtering, fixed event listener, added radiobutton

--- submissions/Harnytskyi/friends-app/js/script.js
+++ submissions/Harnytskyi/friends-app/js/script.js
@@ -1,78 +1,101 @@
 const GRID = document.getElementById("allFriends");
 const CHECKBOX_MAN = document.getElementById("gender-male");
 const CHECKBOX_WOMAN = document.getElementById("gender-female");
-const SORT_AZ = document.getElementById("a-z");
-const SORT_ZA = document.getElementById("z-a");
+const SORT_ALPHABET_ASCEND = document.getElementById("a-z");
+const SORT_ALPHABET_DESCEND = document.getElementById("z-a");
 const SEARCH_INPUT = document.getElementById('search');
-const SORT_09 = document.getElementById("0-9");
-const SORT_90 = document.getElementById("9-0");
+const SORT_AGE_ASCEND = document.getElementById("0-9");
+const SORT_AGE_DESCEND = document.getElementById("9-0");
+const FILTER_SORT = document.getElementById("filter-sort");
+const FILTER_GENDER = document.getElementById("filter-gender");
 let friends = [];
+let gender = {
+    male: true,
+    female: true
+}
 
 let selectedFriends = [...friends];
-function filter() {
+function filterByGender() {
     if (gender.male == true && gender.female == true)
         selectedFriends = friends;
     else if (gender.male == true && gender.female == false)
         selectedFriends = friends.filter(item => item.gender == "male")
     else if (gender.male == false && gender.female == true)
         selectedFriends = friends.filter(item => item.gender == "female")
+    search();
 }
 function displayFriends() {
-    GRID.innerHTML = '';
-    selectedFriends.forEach(item => {
-        GRID.innerHTML +=  `<div id="${item.name}" class="card">
+    GRID.innerHTML = selectedFriends.map(item => {
+        return `<div id="${item.name}" class="card">
             <img class="photo" src="${item.photo}">
             <p class="name">${item.name}</p>
             <p class="age">Age: ${item.age}</p>
-            <a class="phone" href="${item.phone}">${item.phone}</a>
+            <a class="phone" href="tel:${item.phone}">${item.phone}</a>
             <div class="email">
-                <a href="${item.email}">
+                <a href="mailto:${item.email}">
                     <button class="button-message">SEND MESSAGE</button>
                 </a>
             </div>
-        <div>`
-    });
-    search();
-}
-let gender = {
-    male: true,
-    female: true
+        </div>`
+    }).join("");
 }
 
-function changeFilterStatus(checkbox, sex) {
+function changeFilterStatus(checkbox) {
     if (checkbox.checked) {
-        gender[sex] = true;
+        gender[checkbox.value] = true;
     } else {
-        if (sex == "male") {
+        if (checkbox.value == "male") {
             if (gender.female == false) {
                 gender.female = true;
                 CHECKBOX_WOMAN.click();
             }
         }
-        else if (sex == "female") {
+        else if (checkbox.value == "female") {
             if (gender.male == false) {
                 gender.male = true;
                 CHECKBOX_MAN.click();
             }
         }
-        gender[sex] = false;
+        gender[checkbox.value] = false;
     }
-    filter();
+    filterByGender();
+    displayFriends();
+}
+function searchEvent() {
+    filterByGender();
     displayFriends();
 }
 
 function search() {
     let searchRequest = SEARCH_INPUT.value.toUpperCase();
+    searchedFriends = [];
     selectedFriends.forEach(item => {
-        const FRIEND = document.getElementById(item.name)
-        FRIEND.classList.remove('hidden');
-        if (item.name.toUpperCase().indexOf(searchRequest) == -1) {
-            FRIEND.classList.add('hidden');
+        if (item.name.toUpperCase().includes(searchRequest)) {
+            searchedFriends.push(item)
         }
+        selectedFriends = searchedFriends;
     })
 }
 
-function sortFriends(criterion, order) {
+function sortFriends(value) {
+    let criterion;
+    let order;
+    switch (value) {
+        case 'alphabet-ascend':
+            criterion = 'name';
+            break;
+        case 'alphabet-descend':
+            criterion = 'name';
+            order = 'descending';
+            break;
+        case 'age-ascend':
+            criterion = 'age';
+            break;
+        case 'age-descend':
+            criterion = 'age';
+            order = 'descending';
+            break;
+    }
     friends.sort((a, b) => {
         if (order == "descending") {
             let c = a;
@@ -83,54 +106,43 @@ function sortFriends(criterion, order) {
         if (a[criterion] == b[criterion]) return 0;
         if (a[criterion] < b[criterion]) return -1;
     })
-    filter();
+    filterByGender();
     displayFriends();
 }
 
-CHECKBOX_MAN.addEventListener('change', function () {
-    changeFilterStatus(CHECKBOX_MAN, "male");
-});
-CHECKBOX_WOMAN.addEventListener('change', function () {
-    changeFilterStatus(CHECKBOX_WOMAN, "female");
-});
-SORT_AZ.addEventListener('click', function () {
-    sortFriends("name");
+FILTER_GENDER.addEventListener('change', (event) => {
+    let target = event.target;
+    changeFilterStatus(target);
 })
-SORT_ZA.addEventListener('click', function () {
-    sortFriends("name", "descending");
-})
-SORT_09.addEventListener('click', function () {
-    sortFriends("age");
+FILTER_SORT.addEventListener('change', (event) => {
+    let target = event.target;
+    sortFriends(target.value);
 })
 
-SORT_90.addEventListener('click', function () {
-    sortFriends("age", "descending");
-})
+function fetchUsers() {
+    let targetUrl = "https://randomuser.me/api/?results=30&nat=au,ca,ch,de,dk,es,fr,gb,ie,no,nl,nz,us"
 
-function fetchUsers(){
-    let targetUrl  = "https://randomuser.me/api/?results=30&nat=au,ca,ch,de,dk,es,fr,gb,ie,no,nl,nz,us"
-
-    fetch(targetUrl, {metod: "get"})
-    .then((response) => {
-      let json = response.json();
-      if (response.status >= 200 && response.status < 300) {
-        return json;
-      }
-      else {
-        return error;
-      }
-    }).then((json) => {
-      saveUsers(json.results);
-    })
-    .catch(function(error) {
-      showErrorMessage(error);
-    });
+    fetch(targetUrl, { metod: "get" })
+        .then((response) => {
+            let json = response.json();
+            if (response.ok) {
+                return json;
+            }
+            else {
+                return error;
+            }
+        }).then((json) => {
+            saveUsers(json.results);
+        })
+        .catch(function (error) {
+            showErrorMessage(error);
+        });
 }
 
-function saveUsers(usersArray){
+function saveUsers(usersArray) {
     usersArray.forEach(user => {
-        let friend ={
-            name: user.name.first +" "+ user.name.last,
+        let friend = {
+            name: `${user.name.first} ${user.name.last}`,
             age: user.dob.age,
             phone: user.cell,
             email: user.email,
@@ -142,7 +154,7 @@ function saveUsers(usersArray){
     selectedFriends = [...friends];
     displayFriends();
 }
-function showErrorMessage(error){
+function showErrorMessage(error) {
     const field = document.getElementById("contentField");
     field.classList.add('error-text');
     field.innerHTML = `something wrong: <br>${error} <br> please reload the page`;
