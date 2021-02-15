a242caa3e8b4176fdcc43209da6d0a3d3ca76936
Roophee <alex.babaew@gmail.com>
2021-02-15 22:15:32 +0200

    fix data structure, change prepare cards for play algorithm

--- submissions/Roophee/pair-game/index.js
+++ submissions/Roophee/pair-game/index.js
@@ -1,8 +1,40 @@
 "use strict";
 
-const heroesArray = ['luke', 'vader', 'solo', 'ob1', 'palpatine', 'leia', 'r2d2-c3po', 'chewbacca', 'bad1', 'bad2', 'bad3', 'bad4', 'good1', 'good3', 'good4', 'robot1', 'yoda'];
-const indexInUse = [];
-const imagesCountForTwo = {};
+const heroesArray = [
+    {alt:'luke',
+    src:'img/luke.jpg'},
+    {alt:'vader',
+    src:'img/vader.jpg'},
+    {alt:'solo',
+    src:'img/solo.jpg'},
+    {alt:'ob1',
+    src:'img/ob1.jpg'},
+    {alt:'palpatine',
+    src:'img/palpatine.jpg'},
+    {alt:'leia',
+    src:'img/leia.jpg'},
+    {alt:'r2d2-c3po',
+    src:'img/r2d2-c3po.jpg'},
+    {alt:'chewbacca',
+    src:'img/chewbacca.jpg'},
+    {alt:'bad1',
+    src:'img/bad1.jpg'},
+    {alt:'bad2',
+    src:'img/bad2.jpg'},
+    {alt:'bad3',
+    src:'img/bad3.jpg'},
+    {alt:'bad4',
+    src:'img/bad4.jpg'},
+    {alt:'good1',
+    src:'img/good1.jpg'},
+    {alt:'good3',
+    src:'img/good3.jpg'},
+    {alt:'good4',
+    src:'img/good4.jpg'},
+    {alt:'robot1',
+    src:'img/robot1.jpg'},
+    {alt:'yoda',
+    src:'img/yoda.jpg'}];
 const prePair = [];
 const pairs = [];
 const gameBg = document.querySelector('.game');
@@ -14,40 +46,29 @@ const quantityOfUniqueCards = 17;
 const quantityOfCardsOnTheScreen = 12;
 const maxQuantityOfPairsOnTheScreen = 6;
 
+// SHUFFLE CARDS IN CARDS_FOR_PLAY_ARRAY
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 // TAKE RANDOM CARDS FROM POOL OF CARDS
-const cardsForPlay = function () {
-    while (true) {
-        let randomIndex = Math.floor(Math.random() * quantityOfUniqueCards);
-        if (indexInUse.length === maxQuantityOfPairsOnTheScreen) {
-            break
-        } else if (indexInUse.indexOf(randomIndex) < 0) {
-            indexInUse.push(randomIndex);
-        }
-    }
-}
-
-cardsForPlay();
-
-//COUNT HOW MANY SAME IMAGES ADD IN GAME
-const imageCounter = function (indx) {
-    if (imagesCountForTwo.hasOwnProperty(indx) && imagesCountForTwo[indx] >= 2) {
-        return -2;
-    } else if (imagesCountForTwo.hasOwnProperty(indx) && imagesCountForTwo[indx] < 2) {
-        imagesCountForTwo[indx] += 1;
-        return indx;
-    } else if (!imageCounter.hasOwnProperty(indx)) {
-        imagesCountForTwo[indx] = 1;
-        return indx;
-    }
-}
-
-// TAKE RANDOM IMAGE FROM POOL OF IMAGES FOR THIS GAME
-const getRandomImage = function () {
-    while (true) {
-        let randomIndex = Math.floor(Math.random() * maxQuantityOfPairsOnTheScreen);
-        if (imageCounter(randomIndex) >= -1) {
-            return randomIndex;
+const cardsForPlay = function (heroesArray) {
+    const heroesArrCopy = heroesArray.slice();
+    let heroesInUse = [];
+    let k = 0;
+    while(k < 7) {
+        let randomIndex = Math.floor(Math.random() * heroesArrCopy.length);
+        if (heroesInUse.length === maxQuantityOfPairsOnTheScreen) {
+            heroesInUse=[...heroesInUse,...heroesInUse];
+            shuffleArray(heroesInUse);
+            return heroesInUse;
+        } else if (heroesInUse.length < maxQuantityOfPairsOnTheScreen) {
+            heroesInUse.push(heroesArrCopy[randomIndex]);
+            heroesArrCopy.splice(randomIndex,1);
+            k++
         }
     }
 }
@@ -85,22 +106,22 @@ const calculateCardSize = () => {
 
 // ADD CARDS ON DOCUMENT
 const addCardsOnPage = function () {
+    const heroesCardForPlay = cardsForPlay(heroesArray);
     const cardsContainerOnDynamicGeneration = document.createDocumentFragment();
     const cardSizeHandler = calculateCardSize();
-    for (let i = 0; i < quantityOfCardsOnTheScreen; i++) {
-        const cardNameInsertOnDynamicGeneration = heroesArray[indexInUse[getRandomImage()]],
-            cardItem = document.createElement('div');
-        cardItem.classList.add('game__card-holder');
-        cardItem.innerHTML = `<div class="game__card-item">
-                            <div class="game__card-front">
-                                <img src="img/${cardNameInsertOnDynamicGeneration}.jpg" alt="${cardNameInsertOnDynamicGeneration}"  class="game__card-img">
-                            </div>
-                            <div class="game__card-back">
-                                <img src="img/cardback.jpg" alt="cardback" class="game__card-cardback">
-                            </div>
-                        </div>`;
-        cardsContainerOnDynamicGeneration.appendChild(cardItem);
-    }
+    for (let i = 0; i < heroesCardForPlay.length; i++) {
+            const cardItem = document.createElement('div');
+            cardItem.classList.add('game__card-holder');
+            cardItem.innerHTML = `<div class="game__card-item">
+                                <div class="game__card-front">
+                                    <img src="${heroesCardForPlay[i].src}" alt="${heroesCardForPlay[i].alt}"  class="game__card-img">
+                                </div>
+                                <div class="game__card-back">
+                                    <img src="img/cardback.jpg" alt="cardback" class="game__card-cardback">
+                                </div>
+                            </div>`;
+            cardsContainerOnDynamicGeneration.appendChild(cardItem);
+        }
     [].forEach.call(cardsContainerOnDynamicGeneration.querySelectorAll('.game__card-holder'), cardSizeHandler);
     [].forEach.call(cardsContainerOnDynamicGeneration.querySelectorAll('.game__card-img'), cardSizeHandler);
     [].forEach.call(cardsContainerOnDynamicGeneration.querySelectorAll('.game__card-cardback'), cardSizeHandler);
@@ -109,19 +130,14 @@ const addCardsOnPage = function () {
 
 addCardsOnPage();
 
-//  CLEAR ARRAYS AND OBJECTS, PICK NEW CARDS FOR THE GAME
+//  CLEAR OPERATION OBJECTS, PICK NEW CARDS FOR THE GAME
 const clearAllOperationObjectsAndCreateNewGame = function() {
     if (pairs.length === maxQuantityOfPairsOnTheScreen) {
         const playAgain = window.confirm('Congratulations. You have matched all pairs and won the game. Want to play again?');
                 if (playAgain) {
                     cardPlace.innerHTML = "";
-                    for (const prop of Object.keys(imagesCountForTwo)) {
-                        delete imagesCountForTwo[prop];
-                        };
-                    indexInUse.length = 0;
                     prePair.length = 0;
                     pairs.length = 0;
-                    cardsForPlay();
                     addCardsOnPage();
                 }
             }
@@ -135,34 +151,38 @@ const checkGameIsOverOrNot = function () {
 }
 
 // ADD HIDE CLASS ON CARD-HOLDER ITEM IF TWO CARD ARE PAIR
-let hidePair = function(){
-    if (prePair[0] && prePair[1]){
-    prePair[0][1].classList.add('hide');
-    prePair[1][1].classList.add('hide');
-    prePair.splice(0, 2);
+let hidePair = function(currentPair){
+    if (currentPair[0] && currentPair[1]){
+        currentPair[0][1].classList.add('hide');
+        currentPair[1][1].classList.add('hide');
+        currentPair.splice(0, 2);
     }
 }
 
 // REMOVE ACTIVE CLASS FROM CARD-HOLDER ITEM IF TWO CARD ARE NOT A PAIR
-let clearPair = function() {
-    prePair[0][1].classList.remove('active');
-    prePair[1][1].classList.remove('active');
-    prePair.splice(0, 2);
+let clearPair = function(currentPair) {
+    currentPair[0][1].classList.remove('active');
+    currentPair[1][1].classList.remove('active');
+    currentPair.splice(0, 2);
 }
 
 // CHECK IS TWO CARDS A PAIR OR NOT
 let checkPairOrNot = function () {
     if (prePair.length === 2 && prePair[0][0] === prePair[1][0]) {
+        let currentPair = prePair.slice();
+        prePair.splice(0, 2);
         pairs.push('1');
-        setTimeout(function () {
-            hidePair();
-        }, 500);
+        setTimeout(function (currentPair) {
+            hidePair(currentPair);
+        }, 500, currentPair);
         checkGameIsOverOrNot();
 
     } else if (prePair.length === 2 && prePair[0][0] !== prePair[1][0]) {
-        setTimeout(function () {
-            clearPair();
-        }, 800);
+        let currentPair = prePair.slice();
+        prePair.splice(0, 2);
+        setTimeout(function (currentPair) {
+            clearPair(currentPair);
+        }, 800, currentPair);
 }}
 
 // GET PICTURE NAME FROM ALT AND PARENT ELEMENT
