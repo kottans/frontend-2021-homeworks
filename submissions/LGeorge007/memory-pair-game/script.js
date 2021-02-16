4264a09f8c3f7dadaf1705088c68299fcff40d5f
George <losevgeorge007@gmail.com>
2021-02-16 18:51:25 +0200

    fixed remarks

--- submissions/LGeorge007/memory-pair-game/script.js
+++ submissions/LGeorge007/memory-pair-game/script.js
@@ -4,23 +4,22 @@ let initialArray = ["img/cartman.jpg",
     "img/kyle.jpg",
     "img/kenny.jpg"
 ];
-let arrOfUnits;
-const imgCover = `img/cover.jpg`;
+let arrOfUnits = [];
+const imgCover = "img/cover.jpg";
 const gameField = document.querySelector("#gameField");
 let isLocked = false;
 let firstCard;
 let secondCard;
 let isSecondCard = false;
-let unopenedCards;
+let unopenedCards = 0;
 
-//feed him array & any even number
-const duplicateElements = function(array, times) {
+const duplicateElements = function (array, times) {
     return array.reduce((res, current) => {
         return res.concat(Array(times).fill(current));
     }, []);
 };
 
-const shuffle = function(arr) {
+const shuffle = function (arr) {
     const result = [];
     while (arr.length > 0) {
         let random = Math.floor(Math.random() * arr.length);
@@ -30,20 +29,28 @@ const shuffle = function(arr) {
     return result;
 };
 
-const checkCard = function({ target }) {
-    if (!target.closest(".flipper")) { return };
+const checkWin = function () {
+    if (unopenedCards === 0) {
+        setTimeout(() => {
+            alert("You win!");
+            window.location.reload();
+        }, 1500);
+    };
+};
+
+const checkCard = function ({target}) {
+    if (!target.closest(".flipper")) return;
     if (!isLocked) {
         if (isSecondCard) {
             isLocked = true;
             secondCard = target.closest(".flipper");
             secondCard.classList.toggle("rotateCard");
-            //The same card must not be removed
             if (firstCard.id !== secondCard.id) {
                 if (firstCard.dataset.hero === secondCard.dataset.hero) {
                     unopenedCards -= 2;
                     setTimeout(() => {
-                        firstCard.children[1].classList.add("hiddenSide");
-                        secondCard.children[1].classList.add("hiddenSide");
+                        firstCard.querySelector(".back").classList.add("hiddenSide");
+                        secondCard.querySelector(".back").classList.add("hiddenSide");
                         firstCard.classList.add("hiddenFlipper");
                         secondCard.classList.add("hiddenFlipper");
                     }, 600);
@@ -58,12 +65,7 @@ const checkCard = function({ target }) {
                 isLocked = false;
                 isSecondCard = false;
             }, 1000);
-            if (unopenedCards === 0) {
-                setTimeout(() => {
-                    alert("You win!");
-                    window.location.reload();
-                }, 1500);
-            };
+            checkWin();
         } else {
             firstCard = target.closest(".flipper");
             firstCard.classList.toggle("rotateCard");
@@ -72,10 +74,10 @@ const checkCard = function({ target }) {
     };
 };
 
-const renderCard = function(arr) {
-    let htmlFragment = ``;
+const renderCards = function (arr) {
+    let htmlFragment = "";
     arr.forEach((el, i) => {
-        htmlFragment += `<div id=${i} class="flipper" data-hero=${el.slice(4,-4)}>
+        htmlFragment += `<div id=${i} class="flipper" data-hero=${el.slice(el.lastIndexOf("/") + 1, el.lastIndexOf("."))}>
                                 <img src=${imgCover} class="front">
                                 <img src=${el} class="back">
                          </div>`;
@@ -83,10 +85,10 @@ const renderCard = function(arr) {
     gameField.innerHTML = htmlFragment;
 };
 
-const init = function() {
+const init = function () {
     arrOfUnits = shuffle(duplicateElements(initialArray, 4));
     unopenedCards = arrOfUnits.length;
-    renderCard(arrOfUnits);
+    renderCards(arrOfUnits);
     gameField.addEventListener("click", checkCard);
 }
 
