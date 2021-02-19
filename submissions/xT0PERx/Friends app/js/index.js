43b0fef7400515a0e5351176dbba54577073e83d
Dima <t0perxxl@gmail.com>
2021-02-19 14:20:46 +0200

    fixed sorted function mechanic and names of variables

--- submissions/xT0PERx/Friends app/js/index.js
+++ submissions/xT0PERx/Friends app/js/index.js
@@ -1,33 +1,18 @@
-const friendsList = {
-  current: [],
-  changed: [],
-  count: 72,
-};
-
-const ALL_USERS_CARDS_FILTERS = friendsList.changed;
-const ALL_USERS_CARDS = friendsList.current;
-const USERS_LIST = document.getElementById("usersList");
-const FILTERS_MENU = document.getElementById("filters_menu");
-let WHAT_FILTER_CHOSEN = "";
-const SEARCH_INPUT = document.querySelector(".searcher");
-
-function initApp() {
-  getUsers().then((users) => {
-    saveUsers(users);
-    createUsersCards(ALL_USERS_CARDS);
-  });
-}
-
-initApp();
+const allSavedUsers = [];
+const countUsers = 72;
+const usersBox = document.getElementById("usersList");
+const allFilters = document.getElementById("filters_menu");
+const searchInput = document.querySelector(".searcher");
 
 function getUsers() {
-  const url = `https://randomuser.me/api/1.3/?results=${friendsList.count}`;
+  const url = `https://randomuser.me/api/1.3/?results=${countUsers}`;
 
   return fetch(url)
     .then((res) => {
       if (res.ok) {
         return res.json();
       }
+      throw new Error(res.statusText);
     })
     .then((data) => data.results)
     .catch((err) => {
@@ -35,67 +20,82 @@ function getUsers() {
     });
 }
 
+function initApp() {
+  getUsers().then((users) => {
+    saveUsers(users);
+    createUsersCards(allSavedUsers);
+  });
+}
+
+initApp();
+
 function saveUsers(users) {
   users.forEach(function (el) {
-    ALL_USERS_CARDS.push(el);
-    ALL_USERS_CARDS_FILTERS.push(el);
+    allSavedUsers.push(el);
   });
 }
 
 function createUsersCards(users) {
-  const lists = document.createDocumentFragment();
+  const listsFragment = document.createDocumentFragment();
 
   users.forEach(function (el) {
     const card = document.createElement("li");
     card.classList.add("userCard");
-    template = `<span class="name">${el.name.first} ${el.name.last}</span>
+    let template = `<span class="name">${el.name.first} ${el.name.last}</span>
      <img src="${el.picture.large}">
      <span class="age">Age: ${el.dob.age}</span>
      <span class="location">Location: ${el.location.city}</span>`;
     card.insertAdjacentHTML("beforeEnd", template);
-    lists.append(card);
+    listsFragment.append(card);
   });
 
-  USERS_LIST.appendChild(lists);
+  usersBox.appendChild(listsFragment);
 }
 
-FILTERS_MENU.addEventListener("click", function ({ target }) {
-  if (target.nodeName === "INPUT") {
-    WHAT_FILTER_CHOSEN = target.value;
-    switch (target.value) {
-      case "nameAscend":
-        sortByName();
-        offChecked();
-        break;
-      case "nameDescend":
-        sortByName();
-        offChecked();
-        break;
-      case "younger":
-        sortByAge();
-        offChecked();
-        break;
-      case "senior":
-        sortByAge();
-        offChecked();
-        break;
-      case "male":
-        sortByGender();
-        offChecked();
-        break;
-      case "female":
-        sortByGender();
-        offChecked();
-        break;
-    }
-  }
+allFilters.addEventListener("click", function ({ target }) {
+  let filteredUsers = null;
+
   if (target.value === "reset") {
     resetFilters();
+    return;
   }
+
+  if (target.nodeName !== "INPUT") {
+    return;
+  }
+
+  if (target.name === "search") {
+    return;
+  }
+
+  switch (target.value) {
+    case "nameAscend":
+      filteredUsers = sortByName(allSavedUsers);
+      break;
+    case "nameDescend":
+      filteredUsers = sortByName(allSavedUsers, true);
+      break;
+    case "younger":
+      filteredUsers = sortByAge(allSavedUsers);
+      break;
+    case "senior":
+      filteredUsers = sortByAge(allSavedUsers, true);
+      break;
+    case "male":
+      filteredUsers = sortByGender(allSavedUsers, "male");
+      break;
+    case "female":
+      filteredUsers = sortByGender(allSavedUsers, "female");
+      break;
+  }
+
+  cleanSelectedInputs();
+  deleteAllShowedUsers();
+  createUsersCards(filteredUsers);
 });
 
-function sortByName() {
-  const nameSort = ALL_USERS_CARDS_FILTERS.slice();
+function sortByName(list, isRevers) {
+  const result = [...list];
   const runSorting = (a, b) => {
     if (a.name.first > b.name.first) {
       return 1;
@@ -105,70 +105,53 @@ function sortByName() {
     }
     return 0;
   };
-  if (WHAT_FILTER_CHOSEN === "nameAscend") {
-    nameSort.sort(runSorting);
-  } else {
-    nameSort.sort((a, b) => runSorting(b, a));
-  }
-  resetCards();
-  createUsersCards(nameSort);
+  result.sort((a, b) => (isRevers ? runSorting(b, a) : runSorting(a, b)));
+  return result;
 }
 
-function sortByAge() {
-  const ageSort = ALL_USERS_CARDS_FILTERS.slice();
+function sortByAge(list, isRevers) {
+  const result = [...list];
   const runSorting = (a, b) => a.dob.age - b.dob.age;
-  if (WHAT_FILTER_CHOSEN === "younger") {
-    ageSort.sort(runSorting);
-  } else {
-    ageSort.sort((a, b) => runSorting(b, a));
-  }
-  resetCards();
-  createUsersCards(ageSort);
+  result.sort((a, b) => (isRevers ? runSorting(b, a) : runSorting(a, b)));
+  return result;
 }
 
-function sortByGender() {
-  let genderFilter = [];
-  if (WHAT_FILTER_CHOSEN === "male") {
-    genderFilter = ALL_USERS_CARDS_FILTERS.filter(
-      (elem) => elem.gender === "male"
-    );
-  } else {
-    genderFilter = ALL_USERS_CARDS_FILTERS.filter(
-      (elem) => elem.gender === "female"
-    );
-  }
-  resetCards();
-  createUsersCards(genderFilter);
+function sortByGender(list, gender) {
+  let genderFilter = [...list];
+  genderFilter = allSavedUsers.filter((elem) => elem.gender === gender);
+  return genderFilter;
 }
 
 function resetFilters() {
-  resetCards();
-  createUsersCards(ALL_USERS_CARDS);
+  deleteAllShowedUsers();
+  createUsersCards(allSavedUsers);
 }
-
-SEARCH_INPUT.addEventListener("keydown", () => {
-  search(ALL_USERS_CARDS_FILTERS);
+searchInput.addEventListener("keydown", () => {
+  if (searchInput.value === "") {
+    return;
+  }
+  deleteAllShowedUsers();
+  const featuredUsers = search(allSavedUsers);
+  createUsersCards(featuredUsers);
 });
 
 function search(people) {
-  resetCards();
-  const search = SEARCH_INPUT.value.toLowerCase();
+  const search = searchInput.value.toLowerCase();
 
-  const newPeople = people.filter(
+  const peopleSearch = people.filter(
     (elem) =>
       elem.name.first.toLowerCase().match(search) ||
       elem.name.last.toLowerCase().match(search)
   );
-  createUsersCards(newPeople);
+  return peopleSearch;
 }
 
-function resetCards() {
-  USERS_LIST.innerHTML = "";
+function deleteAllShowedUsers() {
+  usersBox.innerHTML = "";
 }
 
-function offChecked() {
-  WHAT_FILTER_CHOISEN = "";
-  let allInputs = document.querySelectorAll(".sort_input");
+function cleanSelectedInputs() {
+  const allInputs = document.querySelectorAll(".sort_input");
   allInputs.forEach((element) => {
     element.checked = false;
   });
