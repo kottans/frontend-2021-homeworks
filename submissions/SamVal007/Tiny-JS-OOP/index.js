517bbe9bdfb4859f1c4c0f6ff28542e733a7964c
SamVal007 <75952924+SamVal007@users.noreply.github.com>
2021-02-23 07:17:59 +0200

    OOP-exercise  (#407)
    * Create index.js
    
    * Update index.js
    
    * Update index.js
    
    * Made fixes by review
    
    * Update index.js
    
    * Added logic about friends-fields
    
    * Update index.js
    
    * a few fixes
    
    Made one more class Pet which would  generalize Cats and Dogs in one common class and also we could make another classes form Pet

---
+++ submissions/SamVal007/Tiny-JS-OOP/index.js
@@ -0,0 +1,76 @@
+/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+   Complete the below for code reviewers' convenience:
+
+   Code repository: _put repo URL here_
+      Web app: https://samval007.github.io/a-tiny-JS-world/
+   */
+
+// ======== OBJECTS DEFINITIONS ========
+// Define your objects here
+class Person {
+  constructor(species, name, gender, speech, legs) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.speech = speech;
+    this.legs = legs;
+  }
+
+  say() {
+    const say = [
+      this.speech,
+      `I am a ${this.species}.`,
+      `My name is ${this.name}.`,
+      `My gender is ${this.gender}.`,
+      `I have ${this.legs} legs.`
+    ];
+    return say.join(' ');
+  }
+
+}
+
+class Pet extends Person {
+  constructor(species, name, gender, speech) {
+    super(species, name, gender, speech, 4);
+
+  }
+  say() {
+    return `${super.say()} Pets are friendly to all!`;
+  }
+}
+
+
+class Dog extends Pet {
+  constructor(name, gender, speech) {
+    super("dog", name, gender, speech);
+
+  }
+
+}
+class Cat extends Pet {
+  constructor(name, gender, speech) {
+    super("cat", name, gender, speech);
+  }
+}
+
+
+class Human extends Person {
+  constructor(name, gender, speech, friends) {
+    super("human", name, gender, speech, 2); // '2' - here it's numb of legs
+    this.hands = 2;
+    this.friends = friends;
+  }
+  say() {
+    return `${super.say()} I have ${this.hands} hands. I am friendly with ${this.friends? this.friends.join(", ") : "nobody"}.`
+  }
+}
+const dog = new Dog("Sharik", "male", "Wow-Wow!");
+const cat = new Cat("Umka", "female", "Meow!");
+const man = new Human("Valerii", "male", "Bongiorno!", ['cat', 'dog']);
+const woman = new Human("Liza", "female", "Tere hommikust!");
+
+// ======== OUTPUT ========
+
+const persons = [man, woman, dog, cat];
+
+persons.forEach((person) => print(person.say()));
