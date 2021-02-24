e9037d2eb476b07d8a036a1b68c365c9c817f6db
Olha Rybchynska <olha.rybchynska@gmail.com>
2021-02-24 11:09:51 +0200

    add fixes

--- submissions/ORybka/friends-app/main.js
+++ submissions/ORybka/friends-app/main.js
@@ -1,6 +1,6 @@
-let filteredUsers = '';
+let newUsers = '';
 const cardsContainer = document.querySelector('.users-container');
-const sortInput = document.querySelectorAll('.radio-input');
+const sortInput = document.querySelectorAll('.sorting-input');
 document.addEventListener('DOMContentLoaded', () => {
   initApp();
 });
@@ -28,7 +28,7 @@ const initApp = async () => {
   fillCardContainer(usersData);
   enableInputsToSort(usersData);
   searchByUserName(usersData);
-  resetUsers(usersData);
+  resetUsers([...usersData]);
 };
 
 function fillCardContainer(data) {
@@ -36,108 +36,84 @@ function fillCardContainer(data) {
   cardsContainer.innerHTML = '';
   data.forEach((card) => {
     userCard = createCard(card);
-    cardsContainer.appendChild(userCard);
+    cardsContainer.innerHTML += userCard;
   });
 }
 
 function createCard({ picture, name, dob, email, phone, gender, location }) {
-  const card = document.createElement('div');
-  card.className = 'user-card';
-  card.innerHTML = `
-    <div class="card-image-container">
-      <img class="card-image" src="https://picsum.photos/200/300?random=${dob.age}" alt="card-image" >
-    </div>
-    <div class="user-image-container">
-      <img class="user-image" src="${picture.large}" alt="user-image" >
-    </div>
-    <div class="col user-information">
-      <h3 class="user-name">${name.first} ${name.last}</h3>
-      <p class="user-age">${dob.age} years old</p>
-      <div class="row contact-information">
-        <a class="user-email" href="mailto:${email}">
-          <span class="material-icons">email</span>
-        </a>
-        <a class="user-phone" href="tel:${phone}">
-          <span class="material-icons">phone</span>
-        </a>
-        <a class="user-location" href="https://www.google.com.ua/maps/place/${location.city}" target="_blank">
-          <span class="material-icons">location_on</span>
-        </a>
+  const card = `
+    <div class="user-card ${gender}">
+      <div class="card-image-container">
+        <img class="card-image" src="https://picsum.photos/200/300?random=${dob.age}" alt="card-image" >
+      </div>
+      <div class="user-image-container">
+        <img class="user-image" src="${picture.large}" alt="user-image" >
+      </div>
+      <div class="col user-information">
+        <h3 class="user-name">${name.first} ${name.last}</h3>
+        <p class="user-age">${dob.age} years old</p>
+        <div class="row contact-information">
+          <a class="user-email" href="mailto:${email}">
+            <span class="material-icons">email</span>
+          </a>
+          <a class="user-phone" href="tel:${phone}">
+            <span class="material-icons">phone</span>
+          </a>
+          <a class="user-location" href="https://www.google.com.ua/maps/place/${location.city}" target="_blank">
+            <span class="material-icons">location_on</span>
+          </a>
+        </div>
       </div>
     </div>
   `;
-  setCardBackground(card, gender);
   return card;
-}
-
-function setCardBackground(card, gender) {
-  return (card.className = `user-card ${gender}`);
 }
 
 const enableInputsToSort = (data) => {
   sortInput.forEach((el) =>
     el.addEventListener('click', () => {
-      if (el.name === 'byAgeDescending' || el.name === 'byAgeAscending' || el.name === 'byNameAZ' || el.name === 'byNameZA') {
-        sortUsers(data, el.name);
-        fillCardContainer(filteredUsers);
-      } else {
-        filterByGender(data, el.name);
-        fillCardContainer(filteredUsers);
+      newUsers = sortUsers(data, el.id);
+      if (el.checked && el.classList.contains('gender')) {
+        newUsers = filterByGender(newUsers, el.id);
       }
+      fillCardContainer(newUsers);
     })
   );
 };
 
-function sortUsers(data, name) {
-  const sortFunctions = {
-    byAgeAscending: (a, b) => sortByAge(a, b),
-    byAgeDescending: (a, b) => sortByAge(b, a),
-    byNameAZ: (a, b) => sortByName(a, b),
-    byNameZA: (a, b) => sortByName(b, a),
-  };
-  if (sortFunctions[name]) {
-    filteredUsers = [...data].sort(sortFunctions[name]);
-    return filteredUsers;
+function sortUsers(data, id) {
+  if (id === 'byAgeAscending' || id === 'byAgeDescending') {
+    data.sort((a, b) => a.dob.age - b.dob.age);
+    return id === 'byAgeAscending' ? data : data.reverse();
   }
+  if (id === 'byNameAZ' || id === 'byNameZA') {
+    data.sort((a, b) => a.name.first.localeCompare(b.name.first));
+    return id === 'byNameAZ' ? data : data.reverse();
+  }
+  return data;
 }
 
-function sortByAge(a, b) {
-  return a.dob.age - b.dob.age;
-}
-
-function sortByName(a, b) {
-  const userA = a.name.first.toLowerCase();
-  const userB = b.name.first.toLowerCase();
-  if (userA > userB) {
-    return 1;
+function filterByGender(data, id) {
+  if (id === 'byGenderFemale') {
+    return data.filter((user) => user.gender === 'female');
   }
-  if (userA < userB) {
-    return -1;
+  if (id === 'byGenderMale') {
+    return data.filter((user) => user.gender === 'male');
   }
-  return 0;
-}
-
-function filterByGender(data, name) {
-  filteredUsers = [...data];
-  if (name === 'byGenderFemale') {
-    filteredUsers = data.filter((user) => user.gender === 'female');
-  } else if (name === 'byGenderMale') {
-    filteredUsers = data.filter((user) => user.gender === 'male');
-  }
-  return filteredUsers;
+  return data;
 }
 
 const searchInput = document.querySelector('#user-names');
 const searchByUserName = (data) => {
-  filteredUsers = [...data];
+  newUsers = [...data];
   searchInput.addEventListener('input', () => {
-    filteredUsers = data.filter((user) => user.name.first.toLowerCase().includes(searchInput.value.toLowerCase()) || user.name.last.toLowerCase().includes(searchInput.value.toLowerCase()));
-    if (filteredUsers.length === 0) {
+    newUsers = data.filter((user) => user.name.first.toLowerCase().includes(searchInput.value.toLowerCase()) || user.name.last.toLowerCase().includes(searchInput.value.toLowerCase()));
+    if (newUsers.length === 0) {
       document.querySelector('.users-container').innerHTML = `
         <h2 class="no-matches-title">No matches...</h2>
       `;
     } else {
-      fillCardContainer(filteredUsers);
+      fillCardContainer(newUsers);
     }
   });
 };
