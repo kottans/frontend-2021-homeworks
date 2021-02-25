c09634a7d1f678c89781730613d746a7f93eeb2a
smillims <mrdenkil777@gmail.com>
2021-02-25 16:05:31 +0200

    Imrove code for pr

--- submissions/smillims/oop-exercise/index.js
+++ submissions/smillims/oop-exercise/index.js
@@ -3,83 +3,93 @@
    Complete the below for code reviewers' convenience:
 */
 
-class Inhabitats {
-	constructor(gender, legs, hands){
-		this.gender = 'female';
-		this.legs = '4';
-		this.hands = '0';
+class Inhabitant {
+	constructor(species, gender, name, legs){
+		this.species = species;
+		this.gender = gender;
+		this.name = name;
+		this.legs = legs;
+	}
+	properties(){
+		return ['species', 'gender', 'name', 'legs'];
 	}
 	saying(){
-		return `Hi, I\'m ` + this.species + `. My name is ` + this.name + `. My surname is ` + this.surname + `. I\'m ` + this.gender + `. I have ` + this.legs + ` legs and ` + this.hands + ` hands.`;
-	};
-};
+		return [`Hi, I'm ` + this.species,
+		`My name is ` + this.name,
+		`My gender is ` + this.gender,
+		`I have ` + this.legs + ` legs`, ``].join('. ');
+	}
+}
 
-class Human extends Inhabitats{
-	constructor(species, legs, hands){
-		super(species, legs, hands);
-		this.species = 'human';
-		this.legs = '2';
-		this.hands = '2';
-	};
-};
-
-class Cat extends Inhabitats {
-	constructor (species, name, surname){
-		super(species, name, surname);
-		this.species = 'cat';
-		this.name = 'Murka';
-		this.surname = 'Murkovna';
-	};
-	itsSaying(){
-		return super.saying();
-	};
-};
+class Human extends Inhabitant {
+	constructor(surname, name, gender, hands = 2) {
+		super('human', gender, name, 2);
+		this.surname = surname;
+		this.hands = hands;
+	}
+}
 
 class Man extends Human {
-	constructor (name, surname, gender){
-		super(name, surname, gender);
-		this.name = 'Jorj';
-		this.surname = 'Kluni';
-		this.gender = 'male';
-	};
-	itsSaying(){
-		return super.saying();
-	};
+	constructor(name, surname) {
+		super(surname, name, 'male')
+	}
+	itsSaying() {
+		return super.saying() + [`I have ` + this.hands + ` hands`, 
+										`My surname is ` + this.surname].join('. ');
+	}
 }
-
-class Dog extends Inhabitats {
-	constructor(species, name, surname){
-		super(species, name, surname);
-		this.species = 'dog';
-		this.name = 'Arrow';
-		this.surname = 'Space';
-	};
-	itsSaying(){
-		return super.saying();
-	};
-};
 
 class Woman extends Human {
-	constructor(name, surname, gender){
-		super(name, surname, gender);
-		this.name = 'Enn';
-		this.surname = 'Hatuey';
-		this.gender = 'female';
-	};
-	itsSaying(){
-		return super.saying();
-	};
-};
+	constructor(name, surname) {
+		super(surname, name, 'female')
+	}
+	itsSaying() {
+		return super.saying() + [`I have ` + this.hands + ` hands`, 
+										`My surname is ` + this.surname].join('. ');
+	}
+}
 
-const cat = new Cat();
-const man = new Man();
-const dog = new Dog();
-const woman = new Woman();
+class Cat extends Inhabitant {
+	constructor(surname, name, gender) {
+		super('cat', gender, name, '4')
+		this.surname = surname;
+	}
+}
 
-const allInhabitats = [cat, man, dog, woman];
+class sheCat extends Cat {
+	constructor(name, surname) {
+		super(surname, name, 'female')
+	}
+	itsSaying() {
+		return super.saying() + [`My surname is ` + this.surname].join('. ');
+	}
+}
+
+class Dog extends Inhabitant {
+	constructor(surname, name, gender) {
+		super('dog', gender, name, '4')
+		this.surname = surname;
+	}
+}
+
+class sheDog extends Dog {
+	constructor(name, surname){
+		super(surname, name, 'female')
+	}
+	itsSaying() {
+		return super.saying() + [`My surname is ` + this.surname].join('. ');
+	}
+}
+
+const dog = new sheDog('Arrow', 'Space');
+const cat = new sheCat('Murka', 'Murkovna');
+const woman = new Woman('Enn', 'Hatuey');
+const man = new Man('Jorj', 'Kluni');
+
+const allInhabitants = [dog, cat, woman, man];
 
 const printAll = function() {
-	allInhabitats.map(item => print(item.itsSaying()));
-}
+	allInhabitants.map(item => print(item.itsSaying()));
+};
 
 printAll();
