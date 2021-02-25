292ecf6f870dbd346ed51887b56b94876e2aad78
Yakov <ponomarykov@gmail.com>
2021-02-26 00:02:38 +0100

    fixed all the comments of the reviewers

--- submissions/iakow/memory-pair-game/js/game.js
+++ submissions/iakow/memory-pair-game/js/game.js
@@ -1,171 +1,123 @@
-import { handler } from "./controller.js";
-import { GAME_SIZE, IMAGES } from "./config.js";
+import * as Render from "./render.js";
+import { IMAGES, GAME_SIZE } from "./config.js";
 
-function preloadImages() {
+Render.preloadImages().then(Render.mountGame).then(Render.greeting);
+
+let gameCards;
+const openedCardsIDs = [];
+let bestScore = 0;
+let matchMoves, matchStartTime;
+
+setMatch();
+
+export function pickCard(cardID) {
+  const pickIsDisabled =
+    openedCardsIDs.some((openedCardID) => openedCardID === cardID) ||
+    gameCards[cardID] === null;
+
+  if (pickIsDisabled) return;
+
+  switch (openedCardsIDs.length) {
+    case 0:
+      openCard(cardID);
+      if (checkIfLastPair())
+        autoOpenLastCard()
+          .then(discardMatchedPair)
+          .then(completeMatch)
+          .then(newMatch);
+      break;
+    case 1:
+      openCard(cardID);
+      checkIfMatched() ? discardMatchedPair() : closeMismatchedPair();
+      break;
+    case 2:
+      return;
+    default:
+      throw new Error(
+        "Something went wrong: openedCardsIDs array should`t be longer than 2"
+      );
+  }
+}
+
+function setMatch() {
+  matchMoves = 0;
+  matchStartTime = Date.now();
+
+  gameCards = getNewCardsSet();
+
+  function getNewCardsSet() {
+    const randomize = () => 0.5 - Math.random();
+    const randomImgSet = [...IMAGES].sort(randomize).slice(0, GAME_SIZE / 2);
+
+    return [...randomImgSet, ...randomImgSet].sort(randomize);
+  }
+}
+
+function completeMatch() {
   return new Promise((resolve) => {
-    let uploadedImages = 0;
-    const fragment = document.createDocumentFragment();
+    const awaitDiscardingLastCards = 1400;
 
-    IMAGES.forEach((image) => addLink(`img/${image}`));
-
-    document.head.appendChild(fragment);
-
-    function addLink(path) {
-      const link = document.createElement("link");
-      link.rel = "preload";
-      link.href = path;
-      link.as = "image";
-      link.addEventListener("load", onLoadHandler);
-      fragment.appendChild(link);
-
-      function onLoadHandler({ target }) {
-        uploadedImages++;
-        target.removeEventListener("load", onLoadHandler);
-
-        if (uploadedImages === IMAGES.length) {
-          removeSpinner();
-          resolve();
-        }
-
-        function removeSpinner() {
-          document.querySelector(".lds-grid").remove();
-        }
-      }
-    }
-  });
-}
-
-const timeToCheckCards = 600;
-
-function greeting() {
-  const mountAwaiting = 500;
-
-  setTimeout(() => {
-    alert(
-      "It's time to learn to distinguish between masks!\n\n " +
-        "Well you know what to do... :)"
-    );
-  }, mountAwaiting);
-}
-
-function mountGame() {
-  const gameContainer = document.querySelector(".container");
-  const fragment = document.createDocumentFragment();
-
-  for (let i = 0; i < GAME_SIZE; i++) {
-    fragment.appendChild(createCard(i));
-  }
-
-  gameContainer.appendChild(fragment);
-  gameContainer.addEventListener("click", handler);
-
-  function createCard(id) {
-    const cardPlace = document.createElement("div");
-    cardPlace.classList.add("cardPlace");
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.id = id;
-    cardPlace.appendChild(card);
-
-    return cardPlace;
-  }
-}
-
-function animateOpen(cardID, image) {
-  const card = document.getElementById(cardID);
-  card.classList.add("open");
-  card.style.backgroundImage = `none, url("./img/${image}")`;
-}
-
-function showResults(time, moves, score, bestScore) {
-  let comment;
-
-  if (bestScore === 0) {
-    comment = "Congratulations! Now try to improve your result!";
-  } else {
-    if (bestScore > score) {
-      comment = "You can better...";
-    } else {
-      comment = "This is your new record!!!";
-    }
-  }
-
-  alert(
-    [
-      `Best result: ${bestScore}`,
-      "",
-      comment,
-      `Moves: ${moves}`,
-      `Time: ${time.getMinutes()}min ${time.getSeconds()}sec`,
-      `Score: ${score}`,
-    ].join("\n")
-  );
-}
-
-function showNewCards() {
-  document
-    .querySelectorAll(".card")
-    .forEach((card) => card.removeAttribute("style"));
-}
-
-function animateClose(...cardsID) {
-  return new Promise((resolve) => {
     setTimeout(() => {
+      const normalizeFactor = 100000000;
+      const time = new Date(Date.now() - matchStartTime);
+      const score = Math.round(normalizeFactor / (matchMoves * time));
+
+      if (score >= bestScore) bestScore = score;
+
+      Render.showResults(time, matchMoves, score, bestScore);
+
       resolve();
-
-      cardsID.forEach((cardID) => {
-        const card = document.getElementById(cardID);
-
-        card.classList.add("closing");
-        card.classList.remove("open");
-        card.addEventListener("transitionend", clear);
-      });
-
-      function clear(e) {
-        if (e.propertyName === "transform") {
-          e.target.classList.remove("closing");
-          e.target.removeAttribute("style");
-          e.target.removeEventListener("transitionend", clear);
-        }
-      }
-    }, timeToCheckCards);
+    }, awaitDiscardingLastCards);
   });
 }
 
-function animateDiscard(...cardsID) {
+function newMatch() {
+  setMatch();
+  Render.showNewCards();
+}
+
+function openCard(pickedCardID) {
+  openedCardsIDs.push(pickedCardID);
+  Render.animateOpen(pickedCardID, gameCards[pickedCardID]);
+}
+
+function checkIfMatched() {
+  return gameCards[openedCardsIDs[0]] === gameCards[openedCardsIDs[1]];
+}
+
+function closeMismatchedPair() {
+  matchMoves++;
+  Render.animateClose(...openedCardsIDs).then(clearOpenedCards);
+}
+
+function discardMatchedPair() {
+  const [firstID, secondID] = openedCardsIDs;
+
+  matchMoves++;
+  gameCards[firstID] = gameCards[secondID] = null;
+  Render.animateDiscard(...openedCardsIDs).then(clearOpenedCards);
+}
+
+function checkIfLastPair() {
+  return gameCards.filter((card) => card).length === 2;
+}
+
+function autoOpenLastCard() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      cardsID.forEach((cardID) => {
-        resolve();
-
-        window.navigator.vibrate(100);
-
-        const openedCard = document.getElementById(cardID);
-
-        openedCard.classList.add("hiding");
-        openedCard.classList.remove("open");
-        openedCard.addEventListener("transitionend", removeCard);
-
-        function removeCard(e) {
-          if (e.propertyName === "opacity") {
-            e.target.style.display = "none";
-            e.target.classList.remove("hiding");
-            e.target.removeEventListener("transitionend", removeCard);
-          }
-        }
-      });
-    }, timeToCheckCards);
+      openCard(getLastCardID());
+      resolve();
+    }, 300);
   });
+
+  function getLastCardID() {
+    return gameCards.findIndex(
+      (card, i) =>
+        card === gameCards[openedCardsIDs[0]] && i !== openedCardsIDs[0]
+    );
+  }
 }
 
-export {
-  animateOpen,
-  mountGame,
-  animateClose,
-  animateDiscard,
-  showNewCards,
-  greeting,
-  showResults,
-  preloadImages,
-};
+function clearOpenedCards() {
+  openedCardsIDs.length = 0;
+}
