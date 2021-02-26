114e81058bc570938972525e4d496882b1f70651
smillims <mrdenkil777@gmail.com>
2021-02-26 13:23:32 +0200

    Upload new changes for pr

--- submissions/smillims/oop-exercise/index.js
+++ submissions/smillims/oop-exercise/index.js
@@ -1,92 +1,85 @@
 /* 
-	Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
+  Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 */
 
 class Inhabitant {
-	constructor(species, gender, name, legs){
-		this.species = species;
-		this.gender = gender;
-		this.name = name;
-		this.legs = legs;
-	}
-	saying(){
-		return [`Hi, I'm ` + this.species,
-		`My name is ` + this.name,
-		`My gender is ` + this.gender,
-		`I have ` + this.legs + ` legs`, ``].join('. ');
-	}
+  constructor(surname, species, gender, name, legs){
+    this.surname = surname;
+    this.species = species;
+    this.gender = gender;
+    this.name = name;
+    this.legs = legs;
+  }
+  toString(){
+    return [`Hi, I'm ` + this.species,
+    `My name is ` + this.name,
+    `My gender is ` + this.gender,
+    `I have ` + this.legs + ` legs`, ``].join(', ')
+  }
 }
 
 class Human extends Inhabitant {
-	constructor(surname, name, gender, hands = 2) {
-		super('human', gender, name, 2);
-		this.surname = surname;
-		this.hands = hands;
-	}
+  constructor(surname, name, gender, hands = 2) {
+    super(surname, 'human', gender, name, 2);
+    this.hands = hands;
+  }
 }
 
 class Man extends Human {
-	constructor(name, surname) {
-		super(surname, name, 'male')
-	}
-	itsSaying() {
-		return super.saying() + [`I have ` + this.hands + ` hands`, 
-										`My surname is ` + this.surname].join('. ');
-	}
+  constructor(name, surname) {
+    super(surname, name, 'male')
+  }
+  toString() {
+    return super.toString() + [`I have ` + this.hands + ` hands`, 
+                            ` My surname is ` + this.surname]
+  }
 }
 
 class Woman extends Human {
-	constructor(name, surname) {
-		super(surname, name, 'female')
-	}
-	itsSaying() {
-		return super.saying() + [`I have ` + this.hands + ` hands`, 
-										`My surname is ` + this.surname].join('. ');
-	}
+  constructor(name, surname) {
+    super(surname, name, 'female')
+  }
+  toString() {
+    return super.toString() + [`I have ` + this.hands + ` hands`, 
+                            ` My surname is ` + this.surname]
+  }
 }
 
 class Cat extends Inhabitant {
-	constructor(surname, name, gender) {
-		super('cat', gender, name, '4')
-		this.surname = surname;
-	}
+  constructor(surname, name, gender) {
+    super(surname, 'cat', gender, name, '4')
+  }
 }
 
-class sheCat extends Cat {
-	constructor(name, surname) {
-		super(surname, name, 'female')
-	}
-	itsSaying() {
-		return super.saying() + [`My surname is ` + this.surname].join('. ');
-	}
+class SheCat extends Cat {
+  constructor(name, surname) {
+    super(surname, name, 'female')
+  }
+  toString() {
+    return super.toString() + [`My surname is ` + this.surname]
+  }
 }
 
 class Dog extends Inhabitant {
-	constructor(surname, name, gender) {
-		super('dog', gender, name, '4')
-		this.surname = surname;
-	}
+  constructor(surname, name, gender) {
+    super(surname, 'dog', gender, name, '4')
+  }
 }
 
-class sheDog extends Dog {
-	constructor(name, surname){
-		super(surname, name, 'female')
-	}
-	itsSaying() {
-		return super.saying() + [`My surname is ` + this.surname].join('. ');
-	}
+class SheDog extends Dog {
+  constructor(name, surname){
+    super(surname, name, 'female')
+  }
+  toString() {
+    return super.toString() + [`My surname is ` + this.surname]
+  }
 }
 
-const dog = new sheDog('Arrow', 'Space');
-const cat = new sheCat('Murka', 'Murkovna');
+const dog = new SheDog('Arrow', 'Space');
+const cat = new SheCat('Murka', 'Murkovna');
 const woman = new Woman('Enn', 'Hatuey');
 const man = new Man('Jorj', 'Kluni');
 
 const allInhabitants = [dog, cat, woman, man];
-
-const printAll = function() {
-	allInhabitants.map(item => print(item.itsSaying()));
-};
-
-printAll();
+allInhabitants.forEach(inhabitant => print(inhabitant));
